1740a26e34256ee91690646cb6b08056e7d34281
saiashish9 <saiashish3760@gmail.com>
2021-07-30 00:06:57 +0530

    avatar updated

--- src/layout/header/components/drawer/index.js
+++ src/layout/header/components/drawer/index.js
@@ -1,8 +1,21 @@
 import React from "react";
 
-import { StyledDrawer, Container, CloseIcon, Label } from "./styles";
+import {
+  StyledDrawer,
+  Container,
+  CloseIcon,
+  Label,
+  Content,
+  LanguageContainer,
+  Icon,
+  Title,
+} from "./styles";
+
+import { useHistory } from "react-router-dom";
 
 const Modal = ({ visible, setVisible, languages, selected, setSelected }) => {
+  const history = useHistory();
+
   return (
     <StyledDrawer
       placement="bottom"
@@ -13,9 +26,21 @@ const Modal = ({ visible, setVisible, languages, selected, setSelected }) => {
       <Container>
         <CloseIcon onClick={() => setVisible(false)} />
         <Label>Select Language</Label>
-        {languages.map((i) => (
-          <>{i.icon}</>
-        ))}
+        <LanguageContainer>
+          {languages.map((i, k) => (
+            <Content
+              onClick={() => {
+                setSelected(i.id);
+                history.push(i.code);
+              }}
+              key={k}
+              active={selected === i.id ? 1 : 0}
+            >
+              <Icon>{i.icon}</Icon>
+              <Title>{i.title}</Title>
+            </Content>
+          ))}
+        </LanguageContainer>
       </Container>
     </StyledDrawer>
   );
--- src/layout/header/components/drawer/styles.js
+++ src/layout/header/components/drawer/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import { Drawer } from "antd";
 
@@ -10,7 +10,7 @@ const { veryDarkGray, white, darkGrey } = Colors;
 
 export const StyledDrawer = styled(Drawer)`
   .ant-drawer-content-wrapper {
-    height: 69vh !important;
+    height: 72vh !important;
   }
   .ant-drawer-content {
     background: ${({ theme: { current } }) =>
@@ -35,4 +35,33 @@ export const Label = styled.p`
   font-size: 1rem;
   font-weight: ${FontWeight.medium};
   margin: 1rem 0;
+`;
+
+export const Content = styled.div`
+  padding: 1rem 0;
+  width: 47%;
+  ${Styles.CCC}
+  margin: 0.5rem 0;
+  ${({ active }) =>
+    active &&
+    css`
+      border: 2px solid #fff;
+      border-radius: 5px;
+    `}
+`;
+
+export const LanguageContainer = styled.div`
+  ${Styles.RBC}
+  flex-wrap: wrap;
+`;
+
+export const Icon = styled.p`
+  font-size: 2.1rem;
+`;
+
+export const Title = styled.p`
+  color: ${({ theme: { current } }) =>
+    current !== Theme.dark ? veryDarkGray : white};
+  font-size: 0.8rem;
+  font-weight: ${FontWeight.medium};
 `;
