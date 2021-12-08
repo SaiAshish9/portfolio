3353b3e780224b5d7d3a9193916db46fea51c4ba
saiashish9 <saiashish3760@gmail.com>
2021-12-09 03:37:02 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -3789,10 +3789,10 @@ pop_back() :
                   from O(n) to O(logn)
                 </Span>
                 <Span>The height of an AVL tree is always O(Logn)</Span>
-                Balance factor of a node: (height of left subtree) - (height
-                of right tree). It should be between -1 and 1. If it's greater
-                than 1 then we should consider LL (Left Left) or LR case. Otherwise RR or RL
-                case.
+                Balance factor of a node: (height of left subtree) - (height of
+                right tree). It should be between -1 and 1. If it's greater than
+                1 then we should consider LL (Left Left) or LR case. Otherwise
+                RR or RL case.
                 <CodeEditor
                   options={{
                     output: null,
@@ -4055,7 +4055,7 @@ pop_back() :
                       tree.remove(40)
                       tree.preOrder()                      
                         `,
-                        output:`
+                        output: `
                         30 
                         20 
                         10 
@@ -4068,18 +4068,257 @@ pop_back() :
                         10 
                         25 
                         50 
-                        `
+                        `,
                       },
                     },
                   }}
                 />
-                <b>5. Red-Black Tree</b>
+                <Span>
+                  <b>5. Red-Black Tree</b>
+                </Span>
+                <Span>
+                  It's a kind of self balancing binary search tree which uses
+                  one extra bit for the color of each node instead of its
+                  balancing height to ensure that tree remains balanced during
+                  insertion and deletion. We make uses of various flags for
+                  rotation.
+                </Span>
+                <Span>
+                  Rules: Root will always be black.There are no two adjacent red
+                  nodes. Number of black nodes from root to the leaf node will
+                  always be the same through any path. Newly inserted node will
+                  always be given
+                </Span>
+                <Span>
+                  We need to perform rotation when the parent of current node is
+                  red.
+                </Span>
+                <Span>
+                  In the insert operation, we check the color of the uncle to
+                  decide the appropriate case. In the delete operation, we check
+                  the color of the sibling to decide the appropriate case after bst deletion.
+                </Span>
+                <p>
+                  Red-Black Tree of height h has black-height greater than or
+                  equal to h/2. Height less than or equal to 2log2(n+1) <br />
+                  Cost Of Operations: <br />
+                  Insertion: O(log n) <br />
+                  Search: O(log n) <br />
+                  Deletion: O(log n)
+                </p>
                 <CodeEditor
                   options={{
                     output: null,
                     codes: {
-                      Javascript: {
-                        code: ``,
+                      Java: {
+                        code: `
+                        import java.io.*;
+
+                        public class Main
+                        {
+                          public Node root;
+                          public Main()
+                          {
+                            super();
+                            root = null;
+                          }
+                          class Node
+                          {
+                            int data;
+                            Node left;
+                            Node right;
+                            char colour;
+                            Node parent;
+                        
+                            Node(int data)
+                            {
+                              super();
+                              this.data = data; 
+                              this.left = null; 
+                              this.right = null; 
+                              this.colour = 'R'; 
+                              this.parent = null; 
+                            }
+                          }
+                          Node rotateLeft(Node node)
+                          {
+                            Node x = node.right;
+                            Node y = x.left;
+                            x.left = node;
+                            node.right = y;
+                            node.parent = x; 
+                            if(y!=null)
+                              y.parent = node;
+                            return(x);
+                          }
+                        
+                          Node rotateRight(Node node)
+                          {
+                            Node x = node.left;
+                            Node y = x.right;
+                            x.right = node;
+                            node.left = y;
+                            node.parent = x;
+                            if(y!=null)
+                              y.parent = node;
+                            return(x);
+                          }
+                        
+                        
+                          boolean ll = false;
+                          boolean rr = false;
+                          boolean lr = false;
+                          boolean rl = false;
+                        
+                        
+                          Node insertHelp(Node root, int data)
+                          {
+                            // f is true when RED RED conflict is there.
+                            boolean f=false;
+                            
+                        
+                            if(root==null)
+                              return(new Node(data));
+                            else if(data<root.data)
+                            {
+                              // find the location and mark f 
+                              // true if desired condition 
+                              // satisfies
+                              // root.left -> root is parent at last
+                              root.left = insertHelp(root.left, data);
+                              root.left.parent = root;
+                              if(root!=this.root)
+                              {
+                                if(root.colour=='R' && root.left.colour=='R')
+                                  f = true;
+                              }
+                            }
+                            else
+                            {
+                              root.right = insertHelp(root.right,data);
+                              root.right.parent = root;
+                              if(root!=this.root)
+                              {
+                                if(root.colour=='R' && root.right.colour=='R')
+                                  f = true;
+                              }
+                            }
+                        
+                            if(this.ll) 
+                            {
+                              root = rotateLeft(root);
+                              root.colour = 'B';
+                              root.left.colour = 'R';
+                              this.ll = false;
+                            }
+                            else if(this.rr) 
+                            {
+                              root = rotateRight(root);
+                              root.colour = 'B';
+                              root.right.colour = 'R';
+                              this.rr = false;
+                            }
+                            else if(this.rl) 
+                            {
+                              root.right = rotateRight(root.right);
+                              root.right.parent = root;
+                              root = rotateLeft(root);
+                              root.colour = 'B';
+                              root.left.colour = 'R';
+                        
+                              this.rl = false;
+                            }
+                            else if(this.lr) 
+                            {
+                              root.left = rotateLeft(root.left);
+                              root.left.parent = root;
+                              root = rotateRight(root);
+                              root.colour = 'B';
+                              root.right.colour = 'R';
+                              this.lr = false;
+                            }
+                        
+                        // red red case
+                        // is uncle is black,perform rotation
+                            if(f)
+                            {
+                              if(root.parent.right == root) 
+                              {
+                                //  uncle -> root.parent.left
+                                if(root.parent.left==null || root.parent.left.colour=='B') 
+                                {
+                                  if(root.left!=null && root.left.colour=='R')
+                                    this.rl = true;
+                                  else if(root.right!=null && root.right.colour=='R')
+                                    this.ll = true;
+                                }
+                                else 
+                                {
+                                  root.parent.left.colour = 'B';
+                                  root.colour = 'B';
+                                  // root cannot be red
+                                  if(root.parent!=this.root)
+                                    root.parent.colour = 'R';
+                                }
+                              }
+                              else
+                              {
+                                if(root.parent.right==null || root.parent.right.colour=='B')
+                                {
+                                  if(root.left!=null && root.left.colour=='R')
+                                    this.rr = true;
+                                  else if(root.right!=null && root.right.colour=='R')
+                                    this.lr = true;
+                                }
+                                else
+                                {
+                                  root.parent.right.colour = 'B';
+                                  root.colour = 'B';
+                                  if(root.parent!=this.root)
+                                    root.parent.colour = 'R';
+                                }
+                              }
+                              f = false;
+                            }
+                            return(root);
+                          }
+                        
+                          public void insert(int data)
+                          {
+                            if(this.root==null)
+                            {
+                              this.root = new Node(data);
+                              this.root.colour = 'B';
+                            }
+                            else
+                              this.root = insertHelp(this.root,data);
+                          }
+                          
+                          public void inorderTraversal(Node node)
+                          {
+                            if(node!=null)
+                            {
+                              inorderTraversal(node.left);
+                              System.out.printf("%d ", node.data);
+                              inorderTraversal(node.right);
+                            }	}
+                        
+                          public static void main(String[] args)
+                          {
+                            Main t = new Main();
+                            int[] arr = {1,4,6,3,5,7,8,2,9};
+                            for(int i=0;i<9;i++)
+                            {
+                              t.insert(arr[i]);
+                          }
+                            t.inorderTraversal(t.root);
+                        }
+                        }
+                        Deletion -> https://www.geeksforgeeks.org/red-black-tree-set-3-delete-2/
+                        `,
+                        output: `
+ 1 2 3 4 5 6 7 8 9
+ `,
                       },
                     },
                   }}
