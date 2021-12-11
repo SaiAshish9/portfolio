7a24ef9d27cca6ecf88e16dd08744a8f0fe7660a
saiashish9 <saiashish3760@gmail.com>
2021-12-11 22:18:10 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -5285,6 +5285,142 @@ if __name__=='__main__':
           },
           {
             title: "Tabular",
+            content: (
+              <>
+                <Span>
+                  <b>Hash Table</b>
+                </Span>
+                <Span>
+                  A Hash Table transforms a key into an integer index using a
+                  hash function, and the index will decide where to store the
+                  key/value pair in memory:
+                </Span>
+                <b>Implementation</b>
+                <CodeEditor
+                  options={{
+                    output: null,
+                    codes: {
+                      Javascript: {
+                        code: `
+                        class HashTable {
+                          constructor() {
+                            this.table = new Array(127);
+                            this.size = 0;
+                          }
+                        
+                          _hash(key) {
+                            let hash = 0;
+                            for (let i = 0; i < key.length; i++) {
+                              hash += key.charCodeAt(i);
+                            }
+                            return hash % this.table.length;
+                          }
+                        
+                          set(key, value) {
+                            const index = this._hash(key);
+                            this.table[index] = [key, value];
+                            this.size++;
+                          }
+                        
+                          get(key) {
+                            const target = this._hash(key);
+                            return this.table[target];
+                          }
+                        
+                          remove(key) {
+                            const index = this._hash(key);
+                        
+                            if (this.table[index] && this.table[index].length) {
+                              this.table[index] = [];
+                              this.size--;
+                              return true;
+                            } else {
+                              return false;
+                            }
+                          }
+                        }
+                        
+                        const ht = new HashTable();
+                        
+                        ht.set("a", 1);
+                        ht.set("b", 2);
+                        console.log(ht.get("a")); 
+                        console.log(ht.get("b"));
+                        `,
+                        output: `
+                        [ 'a', 1 ]
+[ 'b', 2 ]
+                        `,
+                      },
+                    },
+                  }}
+                />
+                <Span>Map</Span>
+                <Span>
+                  A map stores a collection of (key,value) pairs, such that each
+                  possible key appears at most once in the collection.
+                </Span>
+                <CodeEditor
+                  options={{
+                    output: null,
+                    codes: {
+                      Javascript: {
+                        code: ``,
+                        output: `o`,
+                      },
+                      Java: {
+                        code: ``,
+                        output: `o`,
+                      },
+                      Kotlin: {
+                        code: ``,
+                        output: `o`,
+                      },
+                      Python: {
+                        code: ``,
+                        output: `o`,
+                      },
+                      "C++": {
+                        code: ``,
+                        output: `o`,
+                      },
+                    },
+                  }}
+                />
+                <Span>Set</Span>
+                <Span>
+                  A set is an abstract data type that can store unique values,
+                  without any particular order.
+                </Span>
+                <CodeEditor
+                  options={{
+                    output: null,
+                    codes: {
+                      Javascript: {
+                        code: ``,
+                        output: `o`,
+                      },
+                      Java: {
+                        code: ``,
+                        output: `o`,
+                      },
+                      Kotlin: {
+                        code: ``,
+                        output: `o`,
+                      },
+                      Python: {
+                        code: ``,
+                        output: `o`,
+                      },
+                      "C++": {
+                        code: ``,
+                        output: `o`,
+                      },
+                    },
+                  }}
+                />
+              </>
+            ),
           },
         ],
         content: (
