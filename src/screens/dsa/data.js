0ab9796dedfab4afc012c8aed43e21dd83611f19
saiashish9 <saiashish3760@gmail.com>
2021-12-15 23:59:57 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -8864,6 +8864,24 @@ printMaxActivities(s,f)`,
             title: "Job Sequencing With Deadlines",
             content: (
               <>
+                <Span>
+                  <b>Problem Statement</b>
+                </Span>
+                <Span>
+                  Given an array of jobs where every job has a deadline and
+                  associated profit if the job is finished before the deadline.
+                  It is also given that every job takes single unit of time, so
+                  the minimum possible deadline of any job is 1. How to maximize
+                  the total profit if only one job can be scheduled at a time
+                </Span>
+                <Span>
+                  <b>Example</b>
+                </Span>
+                <p>
+                  JobId: A , B , C , D , E <br />
+                  Deadline (Units/time): 2 , 1 , 2 ,1 ,3 <br />
+                  Profit 100, 19, 27, 25, 15
+                </p>
                 <CodeEditor
                   options={{
                     output: null,
