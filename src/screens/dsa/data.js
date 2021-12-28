fc00d80a40b3be6973c8875fbf47ea8d0f3435d3
saiashish9 <saiashish3760@gmail.com>
2021-12-28 17:33:35 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -355,11 +355,12 @@ export const DATA = {
                         //         return fullname;
                         //     }
                         // };
-                        // var pokemonName = function() {
-                        //     console.log(this.getPokeName() + 'I choose you!');
-                        // };
+                        var pokemonName = function(snack, hobby) {
+                          console.log(this.getPokeName() + ' loves ' + snack + ' and ' + hobby);
+                        }                      
                         // var logPokemon = pokemonName.bind(pokemon); // creates new object and binds pokemon.
                         // 'this' of pokemon === pokemon now
+                        // logPokemon('sushi', 'algorithms'); // Pika Chu  loves sushi and algorithms
                         // pokemonName.call(pokemon,'sushi', 'algorithms');
                         // Pika Chu  loves sushi and algorithms
                         // pokemonName.apply(pokemon,['sushi', 'algorithms']) 
