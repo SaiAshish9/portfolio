09d02151ef014bc1f38fb7d174b0843dfa949596
saiashish9 <saiashish3760@gmail.com>
2021-12-31 00:54:50 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -16886,163 +16886,163 @@ if __name__ == '__main__':
         title: "Q2. Add Two Numbers",
       },
       q3: {
-        title: "Q3: Longest Substring Without Repeating Characters",
+        title: "Q3. Longest Substring Without Repeating Characters",
       },
       q4: {
-        title: "Q4: Median Of Two Sorted Arrays",
+        title: "Q4. Median Of Two Sorted Arrays",
       },
       q5: {
-        title: "Q5: LPS",
+        title: "Q5. LPS",
       },
       q6: {
-        title: "Q6: ZigZag Conversion",
+        title: "Q6. ZigZag Conversion",
       },
       q7: {
-        title: "Q7: Reverse Integer",
+        title: "Q7. Reverse Integer",
       },
       q8: {
-        title: "Q8: ATOI",
+        title: "Q8. ATOI",
       },
       q9: {
-        title: "Q9: Palindrome",
+        title: "Q9. Palindrome",
       },
       q10: {
         title: "Q10. Regular Expression Matching",
       },
       q11: {
-        title: "Q11: Container With Most Medium Water",
+        title: "Q11. Container With Most Medium Water",
       },
       q12: {
-        title: "Q12: Integer To Roman",
+        title: "Q12. Integer To Roman",
       },
       q13: {
-        title: "Q13: Roman To Integer",
+        title: "Q13. Roman To Integer",
       },
       q14: {
-        title: "Q14: Longest Common Prefix",
+        title: "Q14. Longest Common Prefix",
       },
       q15: {
-        title: "Q15: 3Sum",
+        title: "Q15. 3Sum",
       },
       q16: {
-        title: "Q16 : 3SumClosest",
+        title: "Q16. 3SumClosest",
       },
       q17: {
-        title: "Q17: Letter Combinations Of A PhoneNumber",
+        title: "Q17. Letter Combinations Of A PhoneNumber",
       },
       q18: {
-        title: "Q18: 4Sum",
+        title: "Q18. 4Sum",
       },
       q19: {
-        title: "Q19: Remove Nth Node From End of List",
+        title: "Q19. Remove Nth Node From End of List",
       },
       q20: {
-        title: "Q20: Valid Parenthesis",
+        title: "Q20. Valid Parenthesis",
       },
       q21: {
-        title: "Q21: Merge Two Sorted Lists",
+        title: "Q21. Merge Two Sorted Lists",
       },
       q22: {
-        title: "Q22: Generate Parentheses",
+        title: "Q22. Generate Parentheses",
       },
       q23: {
-        title: "Q23: Merge K Sorted Lists",
+        title: "Q23. Merge K Sorted Lists",
       },
       q24: {
-        title: "Q24: Swap Nodes In Pairs",
+        title: "Q24. Swap Nodes In Pairs",
       },
       q25: {
-        title: "Q25: Reverse Nodes in k-Group",
+        title: "Q25. Reverse Nodes in k-Group",
       },
       q26: {
-        title: "Q26: Remove Duplicates from Sorted Array.",
+        title: "Q26. Remove Duplicates from Sorted Array.",
       },
       q27: {
-        title: "Q27: Remove Element",
+        title: "Q27. Remove Element",
       },
       q28: {
-        title: "Q28: Implement strStr()",
+        title: "Q28. Implement strStr()",
       },
       q29: {
-        title: "Q29: Divide Two Integers",
+        title: "Q29. Divide Two Integers",
       },
       q30: {
-        title: "Q30: Substring with Concatenation of All Words",
+        title: "Q30. Substring with Concatenation of All Words",
       },
       q31: {
-        title: "Q31: Next Permutation",
+        title: "Q31. Next Permutation",
       },
       q32: {
-        title: "Q32: Longest Valid Parentheses",
+        title: "Q32. Longest Valid Parentheses",
       },
       q33: {
-        title: "Q33: Search in Rotated Sorted Array",
+        title: "Q33. Search in Rotated Sorted Array",
       },
       q34: {
-        title: "Q34: Find First and Last Position of Element in Sorted Array",
+        title: "Q34. Find First and Last Position of Element in Sorted Array",
       },
       q35: {
-        title: "Q35: Search Insert Position",
+        title: "Q35. Search Insert Position",
       },
       q36: {
-        title: "Q36: Valid Sudoku",
+        title: "Q36. Valid Sudoku",
       },
       q37: {
-        title: "Q37: Sudoku Solver",
+        title: "Q37. Sudoku Solver",
       },
       q38: {
         title: "Q38: Count And Say",
       },
       q39: {
-        title: "Q39: Combination Sum",
+        title: "Q39. Combination Sum",
       },
       q40: {
-        title: "Q40: Combination Sum 2",
+        title: "Q40. Combination Sum 2",
       },
       q41: {
-        title: "Q41: Missing Positive",
+        title: "Q41. Missing Positive",
       },
       q42: {
-        title: "Q42: Tapping Rain Water",
+        title: "Q42. Tapping Rain Water",
       },
       q43: {
-        title: "Q43: Multiply Strings",
+        title: "Q43. Multiply Strings",
       },
       q44: {
-        title: "Q44: Wildcard Matching",
+        title: "Q44. Wildcard Matching",
       },
       q45: {
-        title: "Q45: Jump Game 2",
+        title: "Q45. Jump Game 2",
       },
       q46: {
-        title: "Q46: Permutations",
+        title: "Q46. Permutations",
       },
       q47: {
-        title: "Q47: Permutations 2",
+        title: "Q47. Permutations 2",
       },
       q48: {
-        title: "Q48: Rotate Image",
+        title: "Q48. Rotate Image",
       },
       q49: {
-        title: "Q49: Group Anagrams",
+        title: "Q49. Group Anagrams",
       },
       q50: {
-        title: "Q50: Pow(x,n)",
+        title: "Q50. Pow(x,n)",
       },
       q51: {
-        title: "Q51: N-Queens",
+        title: "Q51. N-Queens",
       },
       q52: {
-        title: "Q52: N-Queen’s 2",
+        title: "Q52. N-Queen’s 2",
       },
       q53: {
-        title: "Q53: Maximum Subarray",
+        title: "Q53. Maximum Subarray",
       },
       q54: {
-        title: "Q54: Spiral Matrix",
+        title: "Q54. Spiral Matrix",
       },
       q55: {
-        title: "Q55: Jump Game",
+        title: "Q55. Jump Game",
       },
     },
   },
