4e41cc7cd749f873609e7ca7cfb9633d5374f8f7
saiashish9 <saiashish3760@gmail.com>
2022-01-02 03:50:16 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -21853,9 +21853,131 @@ while ans is not None:
             <CodeEditor
               options={{
                 title: "Q22. Generate Parentheses",
+                output: `[((())), (()()), (())(), ()(()), ()()()]`,
                 codes: {
                   Javascript: {
                     code: ``,
+                  },
+                  Java: {
+                    code: `import java.util.*;
+
+                    class Main {
+                      public List<String> generateParenthesis(int n){
+                            List<String> result = new LinkedList<String>();
+                            process("", n, n, result);
+                            return result;
+                      }
+                      private void process(String prefix, int left, int right, List<String> result) {
+                            if (left == 0 && right == 0) {
+                                result.add(prefix);
+                                return;
+                            }
+                            if (left > 0) {
+                                process(prefix + '(', left - 1, right, result);
+                            }
+                            if (left < right) {
+                                process(prefix + ')', left, right - 1, result);
+                            }
+                      } 
+                      public static void main(String ...s){
+                        Main sol = new Main();
+                        System.out.println(sol.generateParenthesis(3));
+                      }
+                    }`,
+                  },
+                  Python: {
+                    code: `
+class Main:
+
+  def generateParenthesis(self,n:int):
+    result = []
+    self.process("",n,n,result)
+    return result
+  
+  def process(self,prefix,left,right,result):
+    if left == 0 and right == 0:
+      result.append(prefix)
+      return
+    if left > 0 :
+      self.process(f"{prefix}(",left-1,right,result)
+    if left < right:
+      self.process(f"{prefix})",left,right-1,result)
+
+sol = Main()
+print(sol.generateParenthesis(3))`,
+                  },
+                  "C++": {
+                    code: `#include<iostream>
+                    #include<string>
+                    #include<vector>
+                    
+                    using namespace std;
+                    
+                    class Main{
+                       
+                      public: 
+                      vector<string> generateParenthesis(int n){
+                         vector<string> result;
+                         process("",n,n,&result);
+                         return result;
+                      }
+                    
+                      void process(string prefix,int left,int right,vector<string> *result)
+                      {
+                        if(left==0&&right==0){
+                          result->push_back(prefix);
+                          return; 
+                        }
+                        if(left>0){
+                          process(prefix+"(",left-1,right,result);
+                        }
+                        if(left<right){
+                          process(prefix+")",left,right-1,result);
+                        }
+                      }
+                    };
+                    
+                    int main(){
+                      Main m;
+                      vector<string> vec;
+                      vec = m.generateParenthesis(3);
+                      for(auto i:vec){
+                        cout << i << " ";
+                      }
+                      return 0;
+                    }`,
+                  },
+                  Kotlin: {
+                    code: `import java.util.*
+
+                    internal class Main {
+                        fun generateParenthesis(n: Int): List<String> {
+                            val result: MutableList<String> = LinkedList()
+                            process("", n, n, result)
+                            return result
+                        }
+                    
+                        private fun process(prefix: String, left: Int, right: Int, result: MutableList<String>) {
+                            if (left == 0 && right == 0) {
+                                result.add(prefix)
+                                return
+                            }
+                            if (left > 0) {
+                                process("$prefix(", left - 1, right, result)
+                            }
+                            if (left < right) {
+                                process("$prefix)", left, right - 1, result)
+                            }
+                        }
+                    
+                        companion object {
+                            @JvmStatic
+                            fun main(s: Array<String>) {
+                                val sol = Main()
+                                println(sol.generateParenthesis(3))
+                            }
+                        }
+                    }`,
                   },
                 },
               }}
