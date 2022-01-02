ee1e3a21a57beacb4555b8f4478514a58b29e9ad
saiashish9 <saiashish3760@gmail.com>
2022-01-02 18:20:06 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -23741,10 +23741,107 @@ print(removeDuplicates([0,0,1,2,2,3,4]))
       },
       q29: {
         title: "Q29. Divide Two Integers",
-        content: <></>,
+        content: (
+          <>
+            <Span>
+              <b>Q29. Divide Two Integers</b>
+            </Span>
+            <Span>
+              Given two integers dividend and divisor, divide two integers
+              without using multiplication, division, and mod operator.
+            </Span>
+            <Span>
+              The integer division should truncate toward zero, which means
+              losing its fractional part. For example, 8.345 would be truncated
+              to 8, and -2.7335 would be truncated to -2.
+            </Span>
+            <Span>Return the quotient after dividing dividend by divisor.</Span>
+            <Span>
+              {" "}
+              Note: Assume we are dealing with an environment that could only
+              store integers within the 32-bit signed integer range: [−231, 231
+              − 1]. For this problem, if the quotient is strictly greater than
+              231 - 1, then return 231 - 1, and if the quotient is strictly less
+              than -231, then return -231.
+            </Span>
+            <Span>
+              <b>Example 1:</b>
+            </Span>
+            <Span>
+              Input: dividend = 10, divisor = 3 <br />
+              Output: 3 <br />
+              Explanation: 10/3 = 3.33333.. which is truncated to 3.
+            </Span>
+            <Span>
+              <b>Example 2:</b>
+            </Span>
+            <Span>
+              Input: dividend = 7, divisor = -3 <br />
+              Output: -2 <br />
+              Explanation: 7/-3 = -2.33333.. which is truncated to -2.
+            </Span>
+            <Span>
+              <b>Constraints:</b>
+            </Span>
+            <p>
+              -231 &lt;= dividend, divisor &lt;= 231 - 1 <br />
+              divisor != 0
+            </p>
+            <CodeEditor
+              options={{
+                title: "Q29. Divide Two Integers",
+                codes: {
+                  Javascript: {
+                    output:`3`,
+                    code: `import java.io.*;
+
+                    class Main {
+                       
+                       static PrintStream ps = System.out;
+                       
+                       public int divide(int dividend, int divisor) {
+                            int dd = dividend;
+                            int dr = divisor;
+                            if(dr == 0){
+                                return Integer.MAX_VALUE;
+                            }
+                            if(dr == -1 && dd == Integer.MIN_VALUE){
+                                return Integer.MAX_VALUE;
+                            }
+                            int flag = 1;
+                            if((dd < 0 && dr > 0) || (dd > 0 && dr < 0)){ 
+                                flag = -1; 
+                            } 
+                            long ldd = Math.abs((long)dd); 
+                            long ldr = Math.abs((long)dr); 
+                            int result = 0; 
+                            while(ldd >= ldr){
+                                int shift = 0;
+                                while(ldd >= (ldr << shift)){
+                                    shift++;
+                                }
+                                shift --;
+                                result += 1 << shift;
+                                ldd -= ldr << shift;
+                            }
+                            return result*flag;
+                        }
+                    
+                        public static void main(String ...s){
+                        Main m = new Main();
+                        ps.println(m.divide(10,3));   
+                        }
+                    }`,
+                  },
+                },
+              }}
+            />
+          </>
+        ),
       },
       q30: {
         title: "Q30. Substring with Concatenation of All Words",
+        content: <></>,
       },
       q31: {
         title: "Q31. Next Permutation",
