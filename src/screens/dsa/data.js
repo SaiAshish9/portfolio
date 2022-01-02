761e82ff2de4ba0808f3a832b92f09ded31218b3
saiashish9 <saiashish3760@gmail.com>
2022-01-02 16:37:00 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -22887,10 +22887,10 @@ linebreak()
               The number of nodes in the list is n. <br />
               1 &lt;= k &lt;= n &lt;= 5000 <br />0 &lt;= Node.val &lt;= 1000
             </Span>
-            <Span>
+            <p>
               <b>Follow-up</b>: Can you solve the problem in O(1) extra memory
               space?
-            </Span>
+            </p>
             <CodeEditor
               options={{
                 title: "Q25. Reverse Nodes in k-Group",
@@ -23325,6 +23325,159 @@ linebreak()
       },
       q26: {
         title: "Q26. Remove Duplicates from Sorted Array.",
+        content: (
+          <>
+            <Span>
+              <b>Q26. Remove Duplicates from Sorted Array.</b>
+            </Span>
+            <Span>
+              Given an integer array nums sorted in non-decreasing order, remove
+              the duplicates in-place such that each unique element appears only
+              once. The relative order of the elements should be kept the same.
+              <br />
+              Since it is impossible to change the length of the array in some
+              languages, you must instead have the result be placed in the first
+              part of the array nums. More formally, if there are k elements
+              after removing the duplicates, then the first k elements of nums
+              should hold the final result. It does not matter what you leave
+              beyond the first k elements.
+              <br />
+              Return k after placing the final result in the first k slots of
+              nums.
+              <br />
+              Do not allocate extra space for another array. You must do this by
+              modifying the input array in-place with O(1) extra memory.
+            </Span>
+            <Span>
+              Custom Judge: The judge will test your solution with the following
+              code: int[] nums = [...]; // Input array int[] expectedNums =
+              [...]; // The expected answer with correct length int k =
+              removeDuplicates(nums); // Calls your implementation
+              <br />
+              assert k == expectedNums.length; for (int i = 0; i &lt; k; i++){" "}
+              {"{"} <br />
+              assert nums[i] == expectedNums[i];
+              {"}"} <br />
+              If all assertions pass, then your solution will be accepted.
+            </Span>
+            <Span>
+              <b>Example 1</b>
+            </Span>
+            <Span>
+              Input: nums = [1,1,2] <br />
+              Output: 2, nums = [1,2,_] <br />
+              Explanation: Your function should return k = 2, with the first two
+              elements of nums being 1 and 2 respectively. It does not matter
+              what you leave beyond the returned k (hence they are underscores).
+            </Span>
+            <Span>
+              <b>Example 2</b>
+            </Span>
+            <Span>
+              Input: nums = [0,0,1,1,1,2,2,3,3,4] <br />
+              Output: 5, nums = [0,1,2,3,4,_,_,_,_,_] <br />
+              Explanation: Your function should return k = 5, with the first
+              five elements of nums being 0, 1, 2, 3, and 4 respectively. It
+              does not matter what you leave beyond the returned k (hence they
+              are underscores).
+            </Span>
+            <Span>
+              <b>Constraints</b>
+            </Span>
+            <p>
+              0 &lt;= nums.length &lt;= 3 * 104 <br />
+              -100 &lt;= nums[i] &lt;= 100 <br />
+              nums is sorted in non-decreasing order.
+            </p>
+            <CodeEditor
+              options={{
+                title: "Q26. Remove Duplicates from Sorted Array.",
+                codes: {
+                  Java: {
+                    code: `import java.util.*;
+
+                    class Solution {
+                        public int removeDuplicates(int[] nums) {
+                                TreeMap<Integer,Integer> map= new TreeMap<Integer,Integer>();
+                        for(int i=0;i<nums.length;i++){
+                          if(!map.containsValue(nums[i])){
+                            map.put(i,nums[i]);
+                          }
+                        }
+                        int i=0;
+                        for(Map.Entry m:map.entrySet()){
+                          nums[i]=(int)m.getValue();
+                          i++;
+                        }
+                        return map.size();
+                        }
+                    }`,
+                  },
+                  Python: {
+                    code: `
+def removeDuplicates(nums):
+  m = {}
+  for i in range(len(nums)):
+    if not nums[i] in m.values():
+      m[i]=nums[i]
+  for i in range(len(m.values())):
+    nums[i] = list(m.values())[i]
+  return len(m)
+
+print(removeDuplicates([0,0,1,2,2,3,4]))
+                    `,
+                  },
+                  "C++": {
+                    code: `#include <iostream>
+                    #include <bits/stdc++.h>
+                    
+                    using namespace std;
+                    
+                    int removeDuplicates(int nums[]){
+                      map<int,int> m;
+                      int len = *(&nums+1)-nums;
+                      for(int i=0;i<len;i++){
+                        if(m.find(nums[i])==m.end()){
+                           m[nums[i]]=i;
+                        }
+                      }
+                      for(int i=0;i<m.size();i++){
+                        nums[i]=m[i];
+                      }
+                      return m.size();
+                    }
+                    
+                    int main(){
+                      int r[]={0,0,1,2,2,3,4};
+                      cout << removeDuplicates(r) << endl;
+                      return 0;
+                    }`,
+                  },
+                  Kotlin: {
+                    code: `import java.util.*
+
+                    internal class Solution {
+                        fun removeDuplicates(nums: IntArray): Int {
+                            val map = TreeMap<Int, Int>()
+                            for (i in nums.indices) {
+                                if (!map.containsValue(nums[i])) {
+                                    map[i] = nums[i]
+                                }
+                            }
+                            var i = 0
+                            for ((_, value) in map) {
+                                nums[i] = value
+                                i++
+                            }
+                            return map.size
+                        }
+                    }`,
+                  },
+                },
+              }}
+            />
+          </>
+        ),
       },
       q27: {
         title: "Q27. Remove Element",
