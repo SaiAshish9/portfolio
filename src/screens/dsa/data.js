fc85c0126058015448cc79b0de1bbb23e1115e55
saiashish9 <saiashish3760@gmail.com>
2022-01-02 19:34:54 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -24128,7 +24128,90 @@ print(removeDuplicates([0,0,1,2,2,3,4]))
       },
       q33: {
         title: "Q33. Search in Rotated Sorted Array",
-        content: <></>,
+        content: (
+          <>
+            <Span>
+              <b>33. Search in Rotated Sorted Array</b>
+            </Span>
+            <Span>
+              There is an integer array nums sorted in ascending order (with
+              distinct values).
+              <br />
+              Prior to being passed to your function, nums is possibly rotated
+              at an unknown pivot index k (1 &lt;= k &lt; nums.length) such that
+              the resulting array is [nums[k], nums[k+1], ..., nums[n-1],
+              nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example,
+              [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become
+              [4,5,6,7,0,1,2].
+              <br />
+              Given the array nums after the possible rotation and an integer
+              target, return the index of target if it is in nums, or -1 if it
+              is not in nums.
+              <br />
+              You must write an algorithm with O(log n) runtime complexity.
+            </Span>
+            <Span>
+              <b>Example 1:</b>
+            </Span>
+            <Span>
+              <b>Example 2:</b>
+            </Span>
+            <Span>
+              <b>Example 3:</b>
+            </Span>
+            <Span>
+              <b>Constraints:</b>
+            </Span>
+            <p>
+              1 &lt;= nums.length &lt;= 5000 <br />
+              -104 &lt;= nums[i] &lt;= 104 <br />
+              All values of nums are unique. <br />
+              nums is an ascending array that is possibly rotated. <br />
+              -104 &lt;= target &lt;= 104
+            </p>
+            <CodeEditor
+              options={{
+                title: "Q33. Search in Rotated Sorted Array",
+                codes: {
+                  Javascript: {
+                    code: `/**
+                    * @param {number[]} nums
+                    * @param {number} target
+                    * @return {number}
+                    */
+                   const search = function (nums, target) {
+                     let start = 0;
+                     let end = nums.length - 1;
+                     while (start <= end) {
+                       let mid = parseInt((start + end) / 2);
+                       if (nums[mid] == target) {
+                         return mid;
+                       }
+                       if (nums[start] <= nums[mid]) {
+                         if (nums[start] <= target && target < nums[mid]) {
+                           end = mid - 1;
+                         } else {
+                           start = mid + 1;
+                         }
+                       } else {
+                         if (nums[mid] < target && target <= nums[end]) {
+                           start = mid + 1;
+                         } else {
+                           end = mid - 1;
+                         }
+                       }
+                     }
+                     return -1;
+                   };
+                   
+                   console.log(search([4, 5, 6, 0, 1, 2], 0));`,
+                    output: `3`,
+                  },
+                },
+              }}
+            />
+          </>
+        ),
       },
       q34: {
         title: "Q34. Find First and Last Position of Element in Sorted Array",
