28f653654cc120de46c09dcbd6482c5df21060bb
saiashish9 <saiashish3760@gmail.com>
2022-01-02 20:26:04 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -24370,7 +24370,161 @@ print(removeDuplicates([0,0,1,2,2,3,4]))
       },
       q36: {
         title: "Q36. Valid Sudoku",
-        content: <></>,
+        content: (
+          <>
+            <Span>
+              <b>36. Valid Sudoku</b>
+            </Span>
+            <Span>
+              Determine if a 9 x 9 Sudoku board is valid. Only the filled cells
+              need to be validated according to the following rules:
+            </Span>
+            <Span>
+              1. Each row must contain the digits 1-9 without repetition. <br />
+              2. Each column must contain the digits 1-9 without repetition.{" "}
+              <br />
+              3. Each of the nine 3 x 3 sub-boxes of the grid must contain the
+              digits 1-9 without repetition.
+            </Span>
+            <Span>
+              <b>Note:</b>
+            </Span>
+            <Span>
+              A Sudoku board (partially filled) could be valid but is not
+              necessarily solvable.
+            </Span>
+            <Span>
+              Only the filled cells need to be validated according to the
+              mentioned rules.
+            </Span>
+            <Span>
+              <b>Example 1:</b>
+            </Span>
+            <Span>
+              Input: board = <br />
+              [["5","3",".",".","7",".",".",".","."] <br />
+              ,["6",".",".","1","9","5",".",".","."] <br />
+              ,[".","9","8",".",".",".",".","6","."]
+              <br />
+              ,["8",".",".",".","6",".",".",".","3"]
+              <br />
+              ,["4",".",".","8",".","3",".",".","1"]
+              <br />
+              ,["7",".",".",".","2",".",".",".","6"]
+              <br />
+              ,[".","6",".",".",".",".","2","8","."]
+              <br />
+              ,[".",".",".","4","1","9",".",".","5"]
+              <br />
+              ,[".",".",".",".","8",".",".","7","9"]]
+              <br />
+              Output: true
+              <br />
+            </Span>
+            <Span>
+              <b>Example 2:</b>
+            </Span>
+            <Span>
+              Input: board = [["8","3",".",".","7",".",".",".","."] <br />
+              ,["6",".",".","1","9","5",".",".","."]
+              <br />
+              ,[".","9","8",".",".",".",".","6","."] <br />
+              ,["8",".",".",".","6",".",".",".","3"]
+              <br />
+              ,["4",".",".","8",".","3",".",".","1"]
+              <br />
+              ,["7",".",".",".","2",".",".",".","6"]
+              <br />
+              ,[".","6",".",".",".",".","2","8","."]
+              <br />
+              ,[".",".",".","4","1","9",".",".","5"]
+              <br />
+              ,[".",".",".",".","8",".",".","7","9"]]
+              <br />
+              Output: false Explanation: Same as Example 1, except with the 5 in
+              the top left corner being modified to 8. Since there are two 8's
+              in the top left 3x3 sub-box, it is invalid.
+            </Span>
+            <Span>
+              <b>Constraints:</b>
+            </Span>
+            <p>
+              board.length == 9 <br />
+              board[i].length == 9 <br />
+              board[i][j] is a digit 1-9 or '.'.
+            </p>
+            <CodeEditor
+              options={{
+                title: "Q36. Valid Sudoku",
+                codes: {
+                  Javascript: {
+                    output: `false`,
+                    code: `
+/**
+* @param {character[][]} board
+* @return {boolean}
+*/
+                    var isValidSudoku = function(board){
+                    let helper = new Set()
+                    for(let i in board){
+                      helper.clear()
+                      for(let j in board){
+                        if(!check(helper,board[i][j])){
+                          return false
+                        }
+                      }
+                      console.log('\n')
+                    }
+                    for(let j in board){
+                      helper.clear()
+                      for(let i in board){
+                        if(!check(helper,board[i][j])){
+                          return false
+                        }
+                      }
+                    }
+                    for(let i=0;i<9;i+=3){
+                      for(let j=0;j<9;j+=3){
+                        helper.clear()
+                        for(let k=0;k<9;k+=3){
+                          if(!check(helper,board[parseInt(i+k/3)][parseInt(j+k%3)])){
+                            return false
+                          }
+                        }
+                      }
+                    }
+                    return true
+                  }
+                  
+                  
+                  var check = function(helper,c){
+                    if(c=='.'){
+                      return true
+                    }
+                    if(helper.has(c)){
+                      return false
+                    }
+                    helper.add(c)
+                    return true
+                  }
+                  
+                  console.log(isValidSudoku([
+                    ['5','3','.','.','7','.','7','.','.'],
+                    ['6','.','.','1','9','5','.','.','.'],
+                    ['.','9','8','.','.','.','.','6','.'],
+                    ['8','.','.','.','6','.','.','.','3'],
+                    ['4','.','.','8','.','3','.','.','1'],
+                    ['7','.','.','.','2','.','.','.','6'],
+                    ['.','6','.','.','.','.','2','8','.'],
+                    ['.','.','.','4','1','9','.','.','5'],
+                    ['.','.','.','.','8','.','.','7','9']
+                  ]))`,
+                  },
+                },
+              }}
+            />
+          </>
+        ),
       },
       q37: {
         title: "Q37. Sudoku Solver",
