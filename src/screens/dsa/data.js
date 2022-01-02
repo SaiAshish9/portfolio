6359aee61fca78e5da516b70d3ba31a84bcb4b49
saiashish9 <saiashish3760@gmail.com>
2022-01-02 19:40:10 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -24246,6 +24246,61 @@ print(removeDuplicates([0,0,1,2,2,3,4]))
               nums is a non-decreasing array. <br />
               -109 &lt;= target &lt;= 109
             </p>
+            <CodeEditor
+              options={{
+                title:
+                  "Q34. Find First and Last Position of Element in Sorted Array",
+                codes: {
+                  Javascript: {
+                    output: `[3,4]`,
+                    code: `/**
+                    * @param {number[]} nums
+                    * @param {number} target
+                    * @return {number[]}
+                    */
+                   var searchRange = function(nums,target){
+                       var left = 0
+                       var right = nums.length -1
+                       var res = []
+                       res[0] = -1
+                       var has = false
+                       while(left <= right){
+                         var mid = left + parseInt((right-left)/2)
+                         if(nums[mid]<target){
+                           left++
+                         }else if(nums[mid]==target){
+                           has = true
+                           right--
+                         }else{
+                           right--
+                         }
+                       }
+                       if(has){
+                         res[0] = left
+                       }
+                       left = 0
+                       right = nums.length - 1
+                       while (left <= right) {
+                         let mid = left + parseInt((right - left) / 2)
+                         if (nums[mid] <= target) {
+                           left++
+                         } else {
+                           right--
+                         }
+                         }
+                         if (has) {
+                           res[1] = right
+                         }else{
+                             res[1] = -1
+                         }
+                       return res
+                     }
+                     
+                     console.log(searchRange([5,7,7,8,8,10],8))`,
+                  },
+                },
+              }}
+            />
           </>
         ),
       },
