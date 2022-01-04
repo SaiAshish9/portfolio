c4410cb544125ce0ee3ce87ace9f84dbb46ed204
saiashish9 <saiashish3760@gmail.com>
2022-01-05 03:26:14 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -6942,6 +6942,56 @@ if __name__=='__main__':
                     },
                   }}
                 />
+                <p>
+                  <b>Array Permutations Using Dfs</b>
+                </p>
+                <CodeEditor
+                  options={{
+                    title: "Array Permutations",
+                    codes: {
+                      Javascript: {
+                        output: `[
+                      [ 1, 2, 3 ],
+                      [ 1, 3, 2 ],
+                      [ 2, 1, 3 ],
+                      [ 2, 3, 1 ],
+                      [ 3, 1, 2 ],
+                      [ 3, 2, 1 ]
+                    ]`,
+                        code: `/**
+                    * @param {number[]} nums
+                    * @return {number[][]}
+                    */
+                   var permute = function (nums) {
+                     let res = [];
+                     let visited = Array(nums.length).fill(false);
+                     dfs(nums, res, [], visited);
+                     return res;
+                   };
+                   
+                   var dfs = function (nums, res = [], curr = [], visited = []) {
+                     if (curr.length == nums.length) {
+                       res.push(curr.slice());
+                       return;
+                     }
+                     for (let i in nums) {
+                       if (visited[i] === false) {
+                         visited[i] = true;
+                         curr.push(nums[i]);
+                         dfs(nums, res, curr, visited);
+                         curr.pop();
+                         visited[i] = false;
+                       }
+                     }
+                   };
+                   
+                   console.log(permute([1, 2, 3]));
+                   
+                   `,
+                      },
+                    },
+                  }}
+                />
                 <Span>
                   <b>Trie</b>
                 </Span>
