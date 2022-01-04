aca33c31bbe5d39947e79347ac0514250bd64ad1
saiashish9 <saiashish3760@gmail.com>
2022-01-04 21:36:05 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -17830,10 +17830,13 @@ console.log(isPairSum(arr, arrSize, val));
             </Span>
             <Span>
               I got the call from razorpay . They asked me questions regarding
-              ci/cd , graphql and various important libraries,etc. I was able to
-              answer all of them.
+              ci/cd , graphql and various important libraries and
+              frameworks,etc. I was able to answer all of them.
             </Span>
             <Span>They're ready to provide me full time employment.</Span>
+            <Span>
+              At the same time, I got the internship opportunity from paytm.{" "}
+            </Span>
           </>
         ),
       },
