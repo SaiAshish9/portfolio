a633d2b7cb16f58d9284a24b43e4cb4b7ed5a468
saiashish9 <saiashish3760@gmail.com>
2022-01-06 03:49:23 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -28794,6 +28794,70 @@ print(removeDuplicates([0,0,1,2,2,3,4]))
             <Span>
               <b>Q65. Valid Number</b>
             </Span>
+            <Span>
+              A valid number can be split up into these components (in order):
+            </Span>
+            <Span>
+              1. A decimal number or an integer. <br />
+              (2. Optional) An 'e' or 'E', followed by an integer.
+            </Span>
+            <Span>
+              A decimal number can be split up into these components (in order):
+            </Span>
+            <Span>
+              1. (Optional) A sign character (either '+' or '-'). <br />
+              2. One of the following formats:
+              <br />
+              1. One or more digits, followed by a dot '.'. <br />
+              2. One or more digits, followed by a dot '.', followed by one or
+              more digits.
+              <br />
+              3. A dot '.', followed by one or more digits.
+            </Span>
+            <Span>
+              An integer can be split up into these components (in order):
+            </Span>
+            <Span>
+              1. (Optional) A sign character (either '+' or '-'). <br />
+              2. One or more digits.
+            </Span>
+            <Span>
+              For example, all the following are valid numbers: ["2", "0089",
+              "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1",
+              "53.5e93", "-123.456e789"], while the following are not valid
+              numbers: ["abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3",
+              "95a54e53"].
+            </Span>
+            <Span>Given a string s, return true if s is a valid number.</Span>
+            <Span>
+              <b>Example 1:</b>
+            </Span>
+            <Span>
+              Input: s = "0" <br />
+              Output: true
+            </Span>
+            <Span>
+              <b>Example 2:</b>
+            </Span>
+            <Span>
+              Input: s = "e" <br />
+              Output: false
+            </Span>
+            <Span>
+              <b>Example 3:</b>
+            </Span>
+            <Span>
+              Input: s = "." <br />
+              Output: false
+            </Span>
+            <Span>
+              <b>Constraints:</b>
+            </Span>
+            <p>
+              1 &lt;= s.length &lt;= 20 <br />s consists of only English letters
+              (both uppercase and lowercase), digits (0-9), plus '+', minus '-',
+              or dot '.'.
+            </p>
             <CodeEditor
               options={{
                 title: "Q65. Valid Number",
