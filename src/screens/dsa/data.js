a3ba700951512bc5ed490dabfda75256a845a3a7
saiashish9 <saiashish3760@gmail.com>
2022-01-06 21:27:46 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -29403,12 +29403,96 @@ print(removeDuplicates([0,0,1,2,2,3,4]))
             <Span>
               <b>Q71. Simplify Path</b>
             </Span>
+            <Span>
+              Given a string path, which is an absolute path (starting with a
+              slash '/') to a file or directory in a Unix-style file system,
+              convert it to the simplified canonical path.
+            </Span>
+            <Span>
+              In a Unix-style file system, a period '.' refers to the current
+              directory, a double period '..' refers to the directory up a
+              level, and any multiple consecutive slashes (i.e. '//') are
+              treated as a single slash '/'. For this problem, any other format
+              of periods such as '...' are treated as file/directory names.
+            </Span>
+            <Span>The canonical path should have the following format:</Span>
+            <Span>
+              1. The path starts with a single slash '/'. <br />
+              2. Any two directories are separated by a single slash '/'. <br />
+              3. The path does not end with a trailing '/'.
+              <br />
+              4. The path only contains the directories on the path from the
+              root directory to the target file or directory (i.e., no period
+              '.' or double period '..')
+            </Span>
+            <Span>Return the simplified canonical path.</Span>
+            <Span>
+              <b>Example 1:</b>
+            </Span>
+            <Span>
+              Input: path = "/home/"
+              <br />
+              Output: "/home"
+              <br />
+              Explanation: Note that there is no trailing slash after the last
+              directory name.
+            </Span>
+            <Span>
+              <b>Example 2:</b>
+            </Span>
+            <Span>
+              Input: path = "/../"
+              <br />
+              Output: "/"
+              <br />
+              Explanation: Going one level up from the root directory is a
+              no-op, as the root level is the highest level you can go.
+            </Span>
+            <Span>
+              <b>Example 3:</b>
+            </Span>
+            <Span>
+              Input: path = "/home//foo/"
+              <br />
+              Output: "/home/foo"
+              <br />
+              Explanation: In the canonical path, multiple consecutive slashes
+              are replaced by a single one.
+            </Span>
+            <Span>
+              <b>Constraints:</b>
+            </Span>
+            <p>
+              1 &lt;= path.length &lt;= 3000 <br />
+              path consists of English letters, digits, period '.', slash '/' or
+              '_'.
+              <br />
+              path is a valid absolute Unix path.
+            </p>
             <CodeEditor
               options={{
                 title: "Q71. Simplify Path",
                 codes: {
                   Javascript: {
-                    code: ``,
+                    code: `/**
+                    * @param {string} path
+                    * @return {string}
+                    */
+                   var simplifyPath = function(path) {
+                     const stack = []
+                     const paths = path.split('/')
+                     for(let s of paths){
+                       if(['', '.'].includes(s)) continue
+                       if(s === '..') 
+                       {if(stack) stack.pop()}
+                       else
+                         stack.push(s)
+                     }
+                     return '/' + stack.join('/')
+                   };
+                   
+                   simplifyPath("/home/")`,
+                    output:`/home`
                   },
                 },
               }}
