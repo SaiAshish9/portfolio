73d469c542d48a9d6920a88cf18289209d471246
saiashish9 <saiashish3760@gmail.com>
2022-01-07 22:10:58 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -30363,12 +30363,87 @@ print(removeDuplicates([0,0,1,2,2,3,4]))
             <Span>
               <b>Q81. Search in Rotated Sorted Array II</b>
             </Span>
+            <Span>
+              There is an integer array nums sorted in non-decreasing order (not
+              necessarily with distinct values).
+            </Span>
+            <Span>
+              Before being passed to your function, nums is rotated at an
+              unknown pivot index k (0 &lt;= k &lt; nums.length) such that the
+              resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0],
+              nums[1], ..., nums[k-1]] (0-indexed). For example,
+              [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become
+              [4,5,6,6,7,0,1,2,4,4].
+            </Span>
+            <Span>
+              Given the array nums after the rotation and an integer target,
+              return true if target is in nums, or false if it is not in nums.
+            </Span>
+            <Span>
+              You must decrease the overall operation steps as much as possible.
+            </Span>
+            <Span>
+              <b>Example 1:</b>
+            </Span>
+            <Span>
+              Input: nums = [2,5,6,0,0,1,2], <br />
+              target = 0 <br />
+              Output: true
+            </Span>
+            <Span>
+              <b>Example 2:</b>
+            </Span>
+            <Span>
+              Input: nums = [2,5,6,0,0,1,2],
+              <br /> target = 3<br />
+              Output: false
+            </Span>
+            <Span>
+              <b>Constraints:</b>
+            </Span>
+            <p>
+              <b>Follow up:</b>
+              This problem is similar to Search in Rotated Sorted Array, but
+              nums may contain duplicates. Would this affect the runtime
+              complexity? How and why?
+            </p>
             <CodeEditor
               options={{
                 title: "Q81. Search in Rotated Sorted Array II",
                 codes: {
                   Javascript: {
-                    code: ``,
+                    code: `/**
+                    * @param {number[]} nums
+                    * @return {number}
+                    */
+                   var search = function(nums,target) {
+                       let l = 0;
+                       let r = nums.length - 1;
+                       while (l <= r) {
+                         const m = l + parseInt((r - l) / 2);
+                         if (nums[m] == target)
+                           return true;
+                         if (nums[l] == nums[m] && nums[m] == nums[r]) {
+                           ++l;
+                           --r;
+                         } else if (nums[l] <= nums[m]) {  
+                           if (nums[l] <= target && target < nums[m])
+                             r = m - 1;
+                           else
+                             l = m + 1;
+                         } else {  
+                           if (nums[m] < target && target <= nums[r])
+                             l = m + 1;
+                           else
+                             r = m - 1;
+                         }
+                       }
+                   
+                       return false;
+                   };
+                   
+                   search([2,5,6,0,0,1,2],0)`,
+                    output: `true`,
                   },
                 },
               }}
