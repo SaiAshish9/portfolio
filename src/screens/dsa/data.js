8ffd197b02db67f6a94c57f644de4bf97567a590
saiashish9 <saiashish3760@gmail.com>
2022-01-07 23:19:25 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -132,6 +132,8 @@ import Leetcode79b from "assets/leetcode/79b.png";
 import Leetcode79c from "assets/leetcode/79c.png";
 import Leetcode82a from "assets/leetcode/q82a.png";
 import Leetcode82b from "assets/leetcode/q82b.png";
+import Leetcode83a from "assets/leetcode/83a.png";
+import Leetcode83b from "assets/leetcode/83b.png";
 
 export const DATA = {
   ds: {
@@ -30543,13 +30545,42 @@ print(removeDuplicates([0,0,1,2,2,3,4]))
         content: (
           <>
             <Span>
-              <b></b>
+              <b>Q83. Remove Duplicates from Sorted List</b>
             </Span>
+            <Span>
+              Given the head of a sorted linked list, delete all duplicates such
+              that each element appears only once. Return the linked list sorted
+              as well.
+            </Span>
+            <Span>
+              <b>Example 1:</b>
+            </Span>
+            <Span>
+              Input: head = [1,1,2] <br />
+              Output: [1,2]
+            </Span>
+            <Span>
+              <b>Example 2:</b>
+            </Span>
+            <Span>
+              Input: head = [1,1,2,3,3] <br />
+              Output: [1,2,3]
+            </Span>
+            <Span>
+              <b>Constraints:</b>
+            </Span>
+            <p>
+              The number of nodes in the list is in the range [0, 300]. <br />
+              -100 &lt;= Node.val &lt;= 100 <br />
+              The list is guaranteed to be sorted in ascending order.
+            </p>
             <CodeEditor
               options={{
+                title: "Q83. Remove Duplicates from Sorted List",
                 codes: {
                   Javascript: {
                     code: ``,
+                    output: ``,
                   },
                 },
               }}
