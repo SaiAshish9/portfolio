f7650563216bd75e227212c6b95dff513e96f64c
saiashish9 <saiashish3760@gmail.com>
2022-01-08 18:04:05 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -31020,7 +31020,7 @@ print(removeDuplicates([0,0,1,2,2,3,4]))
                     };
                     
                     isScramble("great","rgeat")`,
-                   output:`true`
+                    output: `true`,
                   },
                 },
               }}
@@ -31035,6 +31035,65 @@ print(removeDuplicates([0,0,1,2,2,3,4]))
             <Span>
               <b>Q88. Merge Sorted Array</b>
             </Span>
+            <Span>
+              You are given two integer arrays nums1 and nums2, sorted in
+              non-decreasing order, and two integers m and n, representing the
+              number of elements in nums1 and nums2 respectively.
+            </Span>
+            <Span>
+              Merge nums1 and nums2 into a single array sorted in non-decreasing
+              order.
+            </Span>
+            <Span>
+              The final sorted array should not be returned by the function, but
+              instead be stored inside the array nums1. To accommodate this,
+              nums1 has a length of m + n, where the first m elements denote the
+              elements that should be merged, and the last n elements are set to
+              0 and should be ignored. nums2 has a length of n.
+            </Span>
+            <Span>
+              <b>Example 1:</b>
+            </Span>
+            <SPan>
+              Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3 <br />
+              Output: [1,2,2,3,5,6]
+              <br />
+              Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+              <br />
+              The result of the merge is [1,2,2,3,5,6] with the underlined
+              elements coming from nums1.
+            </SPan>
+            <Span>
+              <b>Example 2:</b>
+            </Span>
+            <Span>
+              Input: nums1 = [1], m = 1, nums2 = [], n = 0 <br />
+              Output: [1]
+              <br />
+              Explanation: The arrays we are merging are [1] and []. <br />
+              The result of the merge is [1].
+            </Span>
+            <Span>
+              <b>Example 3:</b>
+            </Span>
+            <Span>
+              Input: nums1 = [0], m = 0, nums2 = [1], n = 1 <br />
+              Output: [1] <br />
+              Explanation: The arrays we are merging are [] and [1]. <br />
+              The result of the merge is [1]. <br />
+              Note that because m = 0, there are no elements in nums1. The 0 is
+              only there to ensure the merge result can fit in nums1.
+            </Span>
+            <Span>
+              <b>Constraints:</b>
+            </Span>
+            <p>
+              nums1.length == m + n <br />
+              nums2.length == n <br />
+              0 &lt;= m, n &lt;= 200 <br />
+              1 &lt;= m + n &lt;= 200 <br />
+              -109 &lt;= nums1[i], nums2[j] &lt;= 109
+            </p>
             <CodeEditor
               options={{
                 codes: {
