4730614095cb3ee97f62e6fd17353949beafea0f
saiashish9 <saiashish3760@gmail.com>
2022-01-10 02:23:05 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -33677,7 +33677,30 @@ print(removeDuplicates([0,0,1,2,2,3,4]))
                 title: "Q115. Distinct Subsequences",
                 codes: {
                   Javascript: {
-                    code: ``,
+                    code: `/**
+                    * @param {string} s
+                    * @param {string} t
+                    * @return {number}
+                    */
+                   var numDistinct = function(s, t) {
+                     const m = s.length
+                     const n = t.length
+                     const dp = Array(n+1).fill(0)
+                     dp[0] = 1
+                     for (let i = 1; i <= m; ++i)
+                     for (let j = n; j >= 1; --j)
+                       if (s[i - 1] == t[j - 1])
+                         dp[j] += dp[j - 1];
+                     console.log(dp)
+                     return dp[n];
+                   };
+                   
+                   numDistinct("rabbbit", "rabbit")`,
+                   output:`[
+                    1, 1, 1, 3,
+                    3, 3, 3
+                  ]
+                  3`
                   },
                 },
               }}
