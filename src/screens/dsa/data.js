acfd08765bd008cf6bec965f627956672da11b89
saiashish9 <saiashish3760@gmail.com>
2022-01-12 03:23:36 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -35871,8 +35871,25 @@ class Solution:
               options={{
                 title: "Q138. Copy List with Random Pointer",
                 codes: {
-                  Javascript: {
-                    code: ``,
+                  Python: {
+                    code: `
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random        
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if head is None:
+            return None
+        if head in self.dict:
+            return self.dict[head]
+        self.dict[head] = Node(head.val)
+        self.dict[head].next = self.copyRandomList(head.next)
+        self.dict[head].random = self.copyRandomList(head.random)
+        return self.dict[head]
+    dict = {}
+                        `,
                   },
                 },
               }}
