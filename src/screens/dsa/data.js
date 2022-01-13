e67fe9d5ef0703a2c91ce6ae167ef2fd86025dea
saiashish9 <saiashish3760@gmail.com>
2022-01-13 20:53:18 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -38203,9 +38203,41 @@ class LRUCache:
               options={{
                 title: "Q166. Fraction to Recurring Decimal (Q160)",
                 codes: {
-                  Javascript: {
-                    code: ``,
-                    output: ``,
+                  Java: {
+                    code: `import java.util.*;
+
+                    class Solution {
+                        public String fractionToDecimal(int numerator, int denominator) {
+                       if (numerator == 0)
+                          return "0";
+                        StringBuilder sb = new StringBuilder();
+                        if (numerator < 0 ^ denominator < 0)
+                          sb.append("-");
+                        long n = Math.abs((long) numerator);
+                        long d = Math.abs((long) denominator);
+                        sb.append(n / d);
+                        if (n % d == 0)
+                          return sb.toString();
+                        sb.append(".");
+                        Map<Long, Integer> seen = new HashMap<>();
+                        for (long r = n % d; r > 0; r %= d) {
+                          if (seen.containsKey(r)) {
+                            sb.insert(seen.get(r), "(");
+                            sb.append(")");
+                            break;
+                          }
+                          seen.put(r, sb.length());
+                          r *= 10;
+                          sb.append(r / d);
+                        }
+                        return sb.toString();
+                        }
+                    
+                        public static void Solution(String ...s){
+                          System.out.println(new Solution().fractionToDecimal(2,1));
+                        }
+                    }`,
+                    output: `1`,
                   },
                 },
               }}
