43cfd5609271f293b1d125519c7cd87f3c02c21a
saiashish9 <saiashish3760@gmail.com>
2022-01-18 13:54:45 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -42258,9 +42258,48 @@ class LRUCache:
                 title:
                   "Q211. Design Add and Search Words Data Structure (Q203)",
                 codes: {
-                  Javascript: {
-                    code: ``,
-                    output: ``,
+                  Java: {
+                    code: `class TrieNode {
+                      public TrieNode[] children = new TrieNode[26];
+                      public boolean isWord = false;
+                    }
+                    
+                    class WordDictionary {
+                      public void addWord(String word) {
+                        TrieNode node = root;
+                        for (final char c : word.toCharArray()) {
+                          final int i = c - 'a';
+                          if (node.children[i] == null)
+                            node.children[i] = new TrieNode();
+                          node = node.children[i];
+                        }
+                        node.isWord = true;
+                      }
+                    
+                      public boolean search(String word) {
+                        return dfs(word, 0, root);
+                      }
+                    
+                      private TrieNode root = new TrieNode();
+                    
+                      private boolean dfs(String word, int s, TrieNode node) {
+                        if (s == word.length())
+                          return node.isWord;
+                        if (word.charAt(s) != '.') {
+                          TrieNode next = node.children[word.charAt(s) - 'a'];
+                          return next == null ? false : dfs(word, s + 1, next);
+                        }
+                    
+                        // word.charAt(s) == '.' -> search all 26 children
+                        for (int i = 0; i < 26; ++i)
+                          if (node.children[i] != null && dfs(word, s + 1, node.children[i]))
+                            return true;
+                    
+                        return false;
+                      }
+                    }
+                    `,
+                    output: `[null,null,null,null,false,true,true,true]`,
                   },
                 },
               }}
