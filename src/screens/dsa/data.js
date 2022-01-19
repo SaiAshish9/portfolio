d6512c3ac3c0cfeeae799b36821c84b3a2948ec1
saiashish9 <saiashish3760@gmail.com>
2022-01-19 16:07:52 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -45048,8 +45048,28 @@ Window position                Max
                 title: "Q240. Search a 2D Matrix II (Q232)",
                 codes: {
                   Javascript: {
-                    code: ``,
-                    output: ``,
+                    code: `/**
+                    * @param {number[][]} matrix
+                    * @param {number} target
+                    * @return {boolean}
+                    */
+                   var searchMatrix = function(matrix, target) {
+                     if(!matrix.length) return false;
+                     let r = 0;
+                     let c = matrix[0].length - 1;
+                     while (r < matrix.length && c >= 0) {
+                       if (matrix[r][c] == target)
+                       return true;
+                       if (matrix[r][c] > target)
+                       --c;
+                       else
+                       ++r;
+                       }
+                     return false;
+                   };
+                   
+                   searchMatrix([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]],20)`,
+                    output: `false`,
                   },
                 },
               }}
