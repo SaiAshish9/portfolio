5b55afd0c6c70b8ad86c7263d8c8bb0a37efd09d
saiashish9 <saiashish3760@gmail.com>
2022-01-19 15:04:40 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -44806,32 +44806,74 @@ class LRUCache:
             <Span>
               <b>Q238. Product of Array Except Self (Q230)</b>
             </Span>
-            <Span></Span>
+            <Span>
+              Given an integer array nums, return an array answer such that
+              answer[i] is equal to the product of all the elements of nums
+              except nums[i].
+              <br />
+              The product of any prefix or suffix of nums is guaranteed to fit
+              in a 32-bit integer.
+              <br />
+              You must write an algorithm that runs in O(n) time and without
+              using the division operation.
+            </Span>
             <Span>
               <b>Example 1:</b>
+            </Span>
+            <Span>
+              Input: nums = [1,2,3,4] <br />
+              Output: [24,12,8,6]
             </Span>
             <Span>
               <b>Example 2:</b>
             </Span>
             <Span>
-              <b>Example 3:</b>
+              Input: nums = [-1,1,0,-3,3]
+              <br />
+              Output: [0,0,9,0,0]
             </Span>
             <Span>
               <b>Constraints:</b>
             </Span>
-            <Span></Span>
-            <Span></Span>
+            <Span>
+              2 &lt;= nums.length &lt;= 105 <br />
+              -30 &lt;= nums[i] &lt;= 30 <br />
+              The product of any prefix or suffix of nums is guaranteed to fit
+              in a 32-bit integer.
+            </Span>
             <Span>
               <b>Complexity:</b>
             </Span>
-            <p></p>
+            <p>
+              Follow up: Can you solve the problem in O(1) extra space
+              complexity? (The output array does not count as extra space for
+              space complexity analysis.)
+            </p>
             <CodeEditor
               options={{
                 title: "Q238. Product of Array Except Self (Q230)",
                 codes: {
                   Javascript: {
-                    code: ``,
-                    output: ``,
+                    code: `/**
+                    * @param {number[]} nums
+                    * @return {number[]}
+                    */
+                   var productExceptSelf = function(nums) {
+                     const n = nums.length;
+                     const ans = Array(n);
+                     ans[0] = 1;
+                     for (let i = 1; i < n; ++i)
+                       ans[i] = ans[i - 1] * nums[i - 1];
+                     let suffix = 1; 
+                     for (let i = n - 1; i >= 0; --i) {
+                       ans[i] *= suffix;
+                       suffix *= nums[i];
+                     }
+                     return ans;   
+                   };
+                   
+                   productExceptSelf([1,2,3,4])`,
+                    output: `[ 24, 12, 8, 6 ]`,
                   },
                 },
               }}
