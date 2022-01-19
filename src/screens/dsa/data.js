60e7000de94ddbe98c7dd6ca5a6ac23653f16d66
saiashish9 <saiashish3760@gmail.com>
2022-01-20 01:22:57 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -193,6 +193,7 @@ import Leetcode235 from "assets/leetcode/235.png";
 import Leetcode236 from "assets/leetcode/236.png";
 import Leetcode240 from "assets/leetcode/240.png";
 import Leetcode257 from "assets/leetcode/257.png";
+import Leetcode289 from "assets/leetcode/289.png";
 
 export const DATA = {
   ds: {
@@ -46757,24 +46758,73 @@ Window position                Max
             <Span>
               <b>Q289. Game of Life (Q251)</b>
             </Span>
-            <Span></Span>
+            <Span>
+              According to Wikipedia's article: "The Game of Life, also known
+              simply as Life, is a cellular automaton devised by the British
+              mathematician John Horton Conway in 1970."
+              <br />
+              The board is made up of an m x n grid of cells, where each cell
+              has an initial state: live (represented by a 1) or dead
+              (represented by a 0). Each cell interacts with its eight neighbors
+              (horizontal, vertical, diagonal) using the following four rules
+              (taken from the above Wikipedia article):
+              <br />
+              Any live cell with fewer than two live neighbors dies as if caused
+              by under-population.
+              <br />
+              Any live cell with two or three live neighbors lives on to the
+              next generation.
+              <br />
+              Any live cell with more than three live neighbors dies, as if by
+              over-population.
+              <br />
+              Any dead cell with exactly three live neighbors becomes a live
+              cell, as if by reproduction.
+              <br />
+              The next state is created by applying the above rules
+              simultaneously to every cell in the current state, where births
+              and deaths occur simultaneously. Given the current state of the m
+              x n grid board, return the next state.
+            </Span>
             <Span>
               <b>Example 1:</b>
             </Span>
-            <Span></Span>
+            <Img src={Leetcode289} left />
+            <Span>
+              Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]
+              <br />
+              Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]
+            </Span>
             <Span>
               <b>Example 2:</b>
             </Span>
-            <Span></Span>
             <Span>
-              <b>Example 3:</b>
+              Input: board = [[1,1],[1,0]]
+              <br />
+              Output: [[1,1],[1,1]]
             </Span>
-            <Span></Span>
             <Span>
               <b>Constraints:</b>
             </Span>
-            <Span></Span>
-            <Span></Span>
+            <Span>
+              m == board.length <br />
+              n == board[i].length <br />
+              1 &lt;= m, n &lt;= 25 <br />
+              board[i][j] is 0 or 1.
+            </Span>
+            <Span>
+              Follow up:
+              <br />
+              Could you solve it in-place? Remember that the board needs to be
+              updated simultaneously: You cannot update some cells first and
+              then use their updated values to update other cells.
+              <br />
+              In this question, we represent the board using a 2D array. In
+              principle, the board is infinite, which would cause problems when
+              the active area encroaches upon the border of the array (i.e.,
+              live cells reach the border). How would you address these
+              problems?
+            </Span>
             <Span>
               <b>Complexity:</b>
             </Span>
