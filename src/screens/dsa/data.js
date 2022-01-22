6a6c816c699f3940266eac11725b736d127ab803
saiashish9 <saiashish3760@gmail.com>
2022-01-22 21:38:34 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -52731,8 +52731,32 @@ Window position                Max
                 title: "Q350. Intersection of Two Arrays II (Q293)",
                 codes: {
                   Javacript: {
-                    code: ``,
-                    output: ``,
+                    code: `/**
+                    * @param {number[]} nums1
+                    * @param {number[]} nums2
+                    * @return {number[]}
+                    */
+                   var intersect = function(nums1, nums2) {
+                     nums1.sort((a,b)=>a-b)
+                     nums2.sort((a,b)=>a-b)
+                     const ans= []; 
+                     let i = 0; 
+                     let j = 0;  
+                     while (i < nums1.length && j < nums2.length)
+                       if (nums1[i] < nums2[j]) {
+                         ++i;
+                       } else if (nums1[i] > nums2[j]) {
+                         ++j;
+                       } else {
+                         ans.push(nums1[i]);
+                         ++i;
+                         ++j;
+                       }
+                     return ans;
+                   };
+                   
+                   intersect([1,2,2,1],[2,2])`,
+                    output: `[ 2, 2 ]`,
                   },
                 },
               }}
