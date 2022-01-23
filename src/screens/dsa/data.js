82278f0b4d6e2eb0937ea1048156238aa5ddb44e
saiashish9 <saiashish3760@gmail.com>
2022-01-23 17:54:09 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -54413,16 +54413,60 @@ Window position                Max
                 getRandom is called.
               </>
             }
-            tc="n"
+            tc="1"
             sc="n"
-            codes={{ Javascript: { code: ``, output: `` } }}
+            codes={{ Java: { code: `class RandomizedSet {
+              public boolean insert(int val) {
+                if (valToIndex.containsKey(val))
+                  return false;
+            
+                valToIndex.put(val, vals.size());
+                vals.add(val);
+            
+                return true;
+              }
+            
+              public boolean remove(int val) {
+                if (!valToIndex.containsKey(val))
+                  return false;
+                final int index = valToIndex.get(val);
+                valToIndex.put(last(vals), index);
+                valToIndex.remove(val);
+                vals.set(index, last(vals));
+                vals.remove(vals.size() - 1);
+                return true;
+              }
+            
+              public int getRandom() {
+                final int index = rand.nextInt(vals.size());
+                return vals.get(index);
+              }
+            
+              private Map<Integer, Integer> valToIndex = new HashMap<>();
+              private List<Integer> vals = new ArrayList<>();
+              private Random rand = new Random();
+            
+              private int last(List<Integer> vals) {
+                return vals.get(vals.size() - 1);
+              }
+            }
+            
+            
+            /**
+             * Your RandomizedSet object will be instantiated and called as such:
+             * RandomizedSet obj = new RandomizedSet();
+             * boolean param_1 = obj.insert(val);
+             * boolean param_2 = obj.remove(val);
+             * int param_3 = obj.getRandom();
+             */`, output: `[null,true,false,true,2,true,false,2]` } }}
           />
         ),
       },
       q311: {
-        title: "Q (Q311)",
+        title: "Q381. Insert Delete GetRandom O(1) - Duplicates allowed (Q312)",
         content: (
           <Comp
+            title="Q381. Insert Delete GetRandom O(1) - Duplicates allowed (Q312)"
             content1={<></>}
             img={null}
             content2={null}
@@ -54450,7 +54494,7 @@ Window position                Max
         ),
       },
       q312: {
-        title: "Q (Q312)",
+        title: "Q (Q311)",
         content: (
           <Comp
             content1={<></>}
