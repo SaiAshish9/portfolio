77fbdfef0c56a0d4fe7abbb5fde62f4f42bf3bf6
saiashish9 <saiashish3760@gmail.com>
2022-01-23 17:07:03 +0530

    1

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -204,6 +204,7 @@ import Leetcode332 from "assets/leetcode/332.png";
 import Leetcode335 from "assets/leetcode/335.png";
 import Leetcode337 from "assets/leetcode/337.png";
 import Leetcode363 from "assets/leetcode/363.png";
+import Leetcode375 from "assets/leetcode/375.png";
 import Comp from "./comp";
 
 export const DATA = {
@@ -53870,7 +53871,9 @@ Window position                Max
             }
             tc="log n"
             sc="1"
-            codes={{ Javascript: { code: `/**
+            codes={{
+              Javascript: {
+                code: `/**
             * Forward declaration of guess API.
             * (The problem description is not clear, so I translate it into follows.)
             *
@@ -53899,36 +53902,123 @@ Window position                Max
                return l;
              }
            }
-           `, output: `6` } }}
+           `,
+                output: `6`,
+              },
+            }}
           />
         ),
       },
       q306: {
-        title: "Q (Q306)",
+        title: "Q375. Guess Number Higher or Lower II (Q306)",
         content: (
           <Comp
-            content1={<></>}
+            title="Q375. Guess Number Higher or Lower II (Q306)"
+            content1={
+              <>
+                We are playing the Guessing Game. The game will work as follows:
+                <br />
+                I pick a number between 1 and n.
+                <br />
+                You guess a number.
+                <br />
+                If you guess the right number, you win the game.
+                <br />
+                If you guess the wrong number, then I will tell you whether the
+                number I picked is higher or lower, and you will continue
+                guessing.
+                <br />
+                Every time you guess a wrong number x, you will pay x dollars.
+                If you run out of money, you lose the game.
+                <br />
+                Given a particular n, return the minimum amount of money you
+                need to guarantee a win regardless of what number I pick.
+              </>
+            }
             img={null}
             content2={null}
             examples={[
               {
-                content: <></>,
+                img: Leetcode375,
+                content: (
+                  <>
+                    Input: n = 10 <br />
+                    Output: 16
+                    <br />
+                    Explanation: The winning strategy is as follows:
+                    <br />
+                    - The range is [1,10]. Guess 7.
+                    <br />
+                    - If this is my number, your total is $0. Otherwise, you pay
+                    $7.
+                    <br />
+                    - If my number is higher, the range is [8,10]. Guess 9.
+                    <br />
+                    - If this is my number, your total is $7. Otherwise, you pay
+                    $9.
+                    <br />
+                    - If my number is higher, it must be 10. Guess 10. Your
+                    total is $7 + $9 = $16.
+                    <br />
+                    - If my number is lower, it must be 8. Guess 8. Your total
+                    is $7 + $9 = $16.
+                    <br />
+                    - If my number is lower, the range is [1,6]. Guess 3.
+                    <br />
+                    - If this is my number, your total is $7. Otherwise, you pay
+                    $3.
+                    <br />
+                    - If my number is higher, the range is [4,6]. Guess 5.
+                    <br />
+                    - If this is my number, your total is $7 + $3 = $10.
+                    Otherwise, you pay $5.
+                    <br />
+                    - If my number is higher, it must be 6. Guess 6. Your total
+                    is $7 + $3 + $5 = $15.
+                    <br />
+                    - If my number is lower, it must be 4. Guess 4. Your total
+                    is $7 + $3 + $5 = $15.
+                    <br />
+                    - If my number is lower, the range is [1,2]. Guess 1.
+                    <br />
+                    - If this is my number, your total is $7 + $3 = $10.
+                    Otherwise, you pay $1.
+                    <br />
+                    - If my number is higher, it must be 2. Guess 2. Your total
+                    is $7 + $3 + $1 = $11.
+                    <br />
+                    The worst case in all these scenarios is that you pay $16.
+                    Hence, you only need $16 to guarantee a win.
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: n = 1
+                    <br />
+                    Output: 0
+                    <br />
+                    Explanation: There is only one possible number, so you can
+                    guess 1 and not have to pay anything.
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: n = 2 Output: 1 Explanation: There are two possible
+                    numbers, 1 and 2. - Guess 1. - If this is my number, your
+                    total is $0. Otherwise, you pay $1. - If my number is
+                    higher, it must be 2. Guess 2. Your total is $1. The worst
+                    case is that you pay $1.
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
-              <>
-                <b>Follow up: </b>
-              </>
-            }
-            tc="n"
-            sc="n"
+            constraints={<>1 &lt;= n &lt;= 200</>}
+            tc="n^3"
+            sc="n^2"
             codes={{ Javascript: { code: ``, output: `` } }}
           />
         ),
