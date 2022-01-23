4ab691c813f2d8e626ec0af1dc2f18f1c11a5be0
saiashish9 <saiashish3760@gmail.com>
2022-01-24 00:56:31 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -56110,7 +56110,23 @@ dir
             constraints={<>1 &lt;= n &lt;= 2^31 - 1</>}
             tc="log n"
             sc="n"
-            codes={{ Javascript: { code: ``, output: `` } }}
+            codes={{ Javascript: { code: `/**
+            * @param {number} n
+            * @return {number}
+            */
+           var integerReplacement = function(n){
+             let ans = 0;
+             for (; n > 1; ++ans)
+               if ((n & 1) == 0) 
+                 n >>= 1;
+               else if (n == 3 || ((n >> 1) & 1) == 0) 
+                 --n;
+               else
+                 ++n;
+             return ans;
+           };
+           
+           integerReplacement(8)`, output: `3` } }}
           />
         ),
       },
