b21e1f304a2a3c17cd51e351ee7959ddfa43c296
saiashish9 <saiashish3760@gmail.com>
2022-01-24 01:10:57 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -56195,7 +56195,31 @@ dir
             }
             tc="n"
             sc="n"
-            codes={{ Javascript: { code: ``, output: `` } }}
+            codes={{ Java: { code: `class Solution {
+              public Solution(int[] nums) {
+                this.nums = nums;
+              }
+            
+              public int pick(int target) {
+                int ans = -1;
+                int range = 0;
+            
+                for (int i = 0; i < nums.length; ++i)
+                  if (nums[i] == target && rand.nextInt(++range) == 0)
+                    ans = i;
+            
+                return ans;
+              }
+            
+              private int[] nums;
+              private Random rand = new Random();
+            }
+            
+            /**
+             * Your Solution object will be instantiated and called as such:
+             * Solution obj = new Solution(nums);
+             * int param_1 = obj.pick(target);
+             */`, output: `[null,2,0,2]` } }}
           />
         ),
       },
