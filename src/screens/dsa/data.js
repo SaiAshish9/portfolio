0328cbbe0531ea0d33dd99cfe691541a96963e0c
saiashish9 <saiashish3760@gmail.com>
2022-01-25 01:13:54 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -27551,43 +27551,39 @@ removeElement([3,2,2,3],3)`,
                 title: "Q40. Combination Sum 2",
                 output: `[[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]]`,
                 codes: {
-                  Java: {
-                    code: `import java.util.*;
-
-              class Solution {
-                  public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-                      List<List<Integer>> result = new ArrayList<List<Integer>>();
-                      if(candidates == null || candidates.length == 0) return result;
-                      ArrayList<Integer> current = new ArrayList<Integer>();
-                      Arrays.sort(candidates);
-                      combinationSum(candidates, target, 0, current, result);
-                      return result;
+                  Javascript:{
+                    code:`/**
+                    * @param {number[]} candidates
+                    * @param {number} target
+                    * @return {number[][]}
+                    */
+                   
+                   function dfs(s, candidates, target, res, curr = []) {
+                     if (target < 0)
+                       return;
+                     if (target == 0) {
+                       res.push(curr.slice());
+                       return;
+                     }
+                     for (let i = s; i < candidates.length; ++i) {
+                       if (i > s && candidates[i] == candidates[i - 1])
+                           continue;
+                       curr.push(candidates[i]);
+                       dfs(i+1, candidates, target - candidates[i], res, curr);
+                       curr.pop();
+                     }
+                   }
+                   
+                   
+                   var combinationSum2 = function(candidates, target){
+                     const res = []
+                     candidates.sort((a,b)=>a-b)
+                     dfs(0, candidates, target, res)
+                     return res  
+                   };
+                   
+                   combinationSum2([10,1,2,7,6,1,5],8)`
                   }
-                  public void combinationSum(int[] candidates, int target, int j, ArrayList<Integer> curr, List<List<Integer>> result){
-                      if(target == 0){
-                          ArrayList<Integer> temp = new ArrayList<Integer>(curr);
-                          result.add(temp);
-                          return;
-                      }
-                      for(int i=j; i<candidates.length; i++){
-                          if (target < candidates[i]){
-                              return;
-                          }
-                          if (i == j || candidates[i] != candidates[i-1]){
-                              curr.add(candidates[i]);
-                              combinationSum(candidates, target - candidates[i], i + 1, curr, result);
-                              curr.remove(curr.size()-1);
-                          }
-                      }
-                  }
-              
-                  public static void Solution(String ...st){
-                       Solution m = new Solution();
-                       System.out.println(m.combinationSum2(new int[]{10,1,2,7,6,1,5},8));
-                  }
-              
-              }`,
-                  },
                 },
               }}
             />
