d5ce21f50ae1007f5ee85bf3c3b4f10696f8307f
saiashish9 <saiashish3760@gmail.com>
2022-01-25 03:56:25 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -57538,25 +57538,51 @@ class Solution {
         title: "Q412. Fizz Buzz (Q350)",
         content: (
           <Comp
-            content1={<></>}
+            content1={
+              <>
+                Given an integer n, return a string array answer (1-indexed)
+                where:
+                <br />
+                answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+                <br />
+                answer[i] == "Fizz" if i is divisible by 3.
+                <br />
+                answer[i] == "Buzz" if i is divisible by 5.
+                <br />
+                answer[i] == i (as a string) if none of the above conditions are
+                true.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: n = 3 <br />
+                    Output: ["1","2","Fizz"]
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: n = 5<br />
+                    Output: ["1","2","Fizz","4","Buzz"]
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: n = 15
+                    <br />
+                    Output:
+                    ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
-              <>
-                <b>Follow up :</b>{" "}
-              </>
-            }
+            constraints={<>1 &lt;= n &lt;= 104</>}
             tc="n"
             sc="n"
             codes={{
