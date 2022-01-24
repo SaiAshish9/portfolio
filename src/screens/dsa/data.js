103122def99dbac6ed8b6738d5da843ec8960b19
saiashish9 <saiashish3760@gmail.com>
2022-01-25 01:20:50 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -27680,6 +27680,13 @@ removeElement([3,2,2,3],3)`,
                           return i
                         }
                        }
+                   //  for (let i = 0; i < n; ++i)
+                   //  while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1])
+                   //  swap(nums, i, nums[i] - 1);
+                   //  for (let i = 0; i < n; ++i)
+                   //   if (nums[i] != i + 1)
+                   //   return i + 1;
+                   
                        return n+1
                     }
                     
