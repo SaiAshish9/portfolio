9705d4bbd77381292c3e981adb9051036d453d9a
saiashish9 <saiashish3760@gmail.com>
2022-01-25 22:11:23 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -59520,27 +59520,66 @@ class Node {
         title: "Q438. Find All Anagrams in a String (Q370)",
         content: (
           <Comp
-            content1={<></>}
+            title="Q438. Find All Anagrams in a String (Q370)"
+            content1={
+              <>
+                Given two strings s and p, return an array of all the start
+                indices of p's anagrams in s. You may return the answer in any
+                order.
+                <br />
+                An Anagram is a word or phrase formed by rearranging the letters
+                of a different word or phrase, typically using all the original
+                letters exactly once.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: s = "cbaebabacd", p = "abc"
+                    <br />
+                    Output: [0,6]
+                    <br />
+                    Explanation:
+                    <br />
+                    The substring with start index = 0 is "cba", which is an
+                    anagram of "abc".
+                    <br />
+                    The substring with start index = 6 is "bac", which is an
+                    anagram of "abc".
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: s = "abab", p = "ab"
+                    <br />
+                    Output: [0,1,2]
+                    <br />
+                    Explanation:
+                    <br />
+                    The substring with start index = 0 is "ab", which is an
+                    anagram of "ab".
+                    <br />
+                    The substring with start index = 1 is "ba", which is an
+                    anagram of "ab".
+                    <br />
+                    The substring with start index = 2 is "ab", which is an
+                    anagram of "ab".
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>{" "}
+                1 &lt;= s.length, p.length &lt;= 3 * 10^4 <br />s and p consist
+                of lowercase English letters.
               </>
             }
             tc="n"
-            sc="n"
+            sc="1"
             codes={{
               Javascript: {
                 code: ``,
