95c05037d253957c73cd5e53f3500886f379c046
saiashish9 <saiashish3760@gmail.com>
2022-01-25 22:56:33 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -59620,31 +59620,73 @@ class Node {
         title: "Q440. K-th Smallest in Lexicographical Order (Q371)",
         content: (
           <Comp
-            content1={<></>}
+            title="Q440. K-th Smallest in Lexicographical Order (Q371)"
+            content1={
+              <>
+                Given two integers n and k, return the kth lexicographically
+                smallest integer in the range [1, n].
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: n = 13, k = 2 <br />
+                    Output: 10 <br />
+                    Explanation: The lexicographical order is [1, 10, 11, 12,
+                    13, 2, 3, 4, 5, 6, 7, 8, 9], so the second smallest number
+                    is 10.
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: n = 1, k = 1<br />
+                    Output: 1
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
-              <>
-                <b>Follow up :</b>{" "}
-              </>
-            }
-            tc="n"
-            sc="n"
+            constraints={<>1 &lt;= k &lt;= n &lt;= 10^9</>}
+            tc="log^3 n"
+            sc="1"
             codes={{
               Javascript: {
-                code: ``,
-                output: ``,
+                code: `/**
+                * @param {number} n
+                * @param {number} k
+                * @return {number}
+                */
+               
+               function getGap(a, b, n) {
+                 let gap = 0;
+                 while (a <= n) {
+                   gap += Math.min(n + 1, b) - a;
+                   a *= 10;
+                   b *= 10;
+                 }
+                 return gap;
+               }
+               
+               var findKthNumber = function(n, k) {
+                 let curr = 1;
+                 for (let i = 1; i < k;) {
+                   let gap = getGap(curr, curr + 1, n);
+                   if (i + gap <= k) {
+                     i += gap;
+                     ++curr;
+                   } else {
+                     ++i;
+                     curr *= 10;
+                   }
+                 }
+                 return curr; 
+               };
+               
+               findKthNumber(13,2)`,
+                output: `10`,
               },
             }}
           />
