a48884994e75f03e4e44c45b23b91452f794d2be
saiashish9 <saiashish3760@gmail.com>
2022-01-25 14:48:20 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -58149,20 +58149,68 @@ class Solution {
         title: "Q420. Strong Password Checker (Q357)",
         content: (
           <Comp
-            content1={<></>}
+            content1={
+              <>
+                A password is considered strong if the below conditions are all
+                met:
+                <br />
+                It has at least 6 characters and at most 20 characters.
+                <br />
+                It contains at least one lowercase letter, at least one
+                uppercase letter, and at least one digit.
+                <br />
+                It does not contain three repeating characters in a row (i.e.,
+                "...aaa..." is weak, but "...aa...a..." is strong, assuming
+                other conditions are met).
+                <br />
+                Given a string password, return the minimum number of steps
+                required to make password strong. if password is already strong,
+                return 0.
+                <br />
+                In one step, you can:
+                <br />
+                Insert one character to password,
+                <br />
+                Delete one character from password, or
+                <br />
+                Replace one character of password with another character.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: password = "a" <br />
+                    Output: 5
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: password = "aA1" <br />
+                    Output: 3
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: password = "1337C0d3"
+                    <br />
+                    Output: 0
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
+            constraints={
+              <>
+                1 &lt;= password.length &lt;= 50 <br />
+                password consists of letters, digits, dot '.' or exclamation
+                mark '!'.
+              </>
+            }
             fp={
               <>
                 <b>Follow up :</b>{" "}
