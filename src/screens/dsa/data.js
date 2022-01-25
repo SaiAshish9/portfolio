66e3909899209df3f6b03657d484e0c6324a6641
saiashish9 <saiashish3760@gmail.com>
2022-01-25 21:39:12 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -59390,9 +59390,34 @@ class Node {
             tc="n.log n"
             sc="n"
             codes={{
-              Javascript: {
-                code: ``,
-                output: ``,
+              Java: {
+                code: `
+                // [[1,4],[2,3],[3,4]]
+                import java.util.*;
+
+                class Solution {
+                  public int[] findRightInterval(int[][] intervals) {
+                    final int n = intervals.length;
+                
+                    int[] ans = new int[n];
+                    NavigableMap<Integer, Integer> startToIndex = new TreeMap<>();
+                
+                    for (int i = 0; i < n; ++i)
+                      startToIndex.put(intervals[i][0], i);
+                
+                    for (int i = 0; i < n; ++i) {
+                      Map.Entry<Integer, Integer> entry = startToIndex.ceilingEntry(intervals[i][1]);
+                      if (entry == null)
+                        ans[i] = -1;
+                      else
+                        ans[i] = entry.getValue();
+                    }
+                
+                    return ans;
+                  }
+                }
+                `,
+                output: `[ -1, 0, 1 ]`,
               },
             }}
           />
