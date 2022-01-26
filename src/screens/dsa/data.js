3f2931264ef7eedc5871a729d39c553f6ed4db93
saiashish9 <saiashish3760@gmail.com>
2022-01-27 03:53:51 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -61737,7 +61737,22 @@ class Node {
         content: (
           <Comp
             title="Q463. Island Perimeter (Q394)"
-            content1={<></>}
+            content1={
+              <>
+                You are given row x col grid representing a map where grid[i][j]
+                = 1 represents land and grid[i][j] = 0 represents water.
+                <br />
+                Grid cells are connected horizontally/vertically (not
+                diagonally). The grid is completely surrounded by water, and
+                there is exactly one island (i.e., one or more connected land
+                cells).
+                <br />
+                The island doesn't have "lakes", meaning the water inside isn't
+                connected to the water around the island. One cell is a square
+                with side length 1. The grid is rectangular, width and height
+                don't exceed 100. Determine the perimeter of the island.
+              </>
+            }
             content2={null}
             examples={[
               {
@@ -61756,7 +61771,7 @@ class Node {
                 <b>Follow up :</b>
               </>
             }
-            tc="n"
+            tc="m.n"
             sc="n"
             codes={{
               Javascript: {
