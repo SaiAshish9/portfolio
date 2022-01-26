d4dcabebf8a27c18d992bf0e9daffef6a22f5f09
saiashish9 <saiashish3760@gmail.com>
2022-01-26 14:34:28 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -60687,27 +60687,71 @@ class Node {
         content: (
           <Comp
             title="Q452. Minimum Number of Arrows to Burst Balloons (Q383)"
-            content1={<></>}
+            content1={
+              <>
+                There are some spherical balloons taped onto a flat wall that
+                represents the XY-plane. The balloons are represented as a 2D
+                integer array points where points[i] = [xstart, xend] denotes a
+                balloon whose horizontal diameter stretches between xstart and
+                xend. You do not know the exact y-coordinates of the balloons.
+                <br />
+                Arrows can be shot up directly vertically (in the positive
+                y-direction) from different points along the x-axis. A balloon
+                with xstart and xend is burst by an arrow shot at x if xstart
+                &lt;= x &lt;= xend. There is no limit to the number of arrows
+                that can be shot. A shot arrow keeps traveling up infinitely,
+                bursting any balloons in its path.
+                <br />
+                Given the array points, return the minimum number of arrows that
+                must be shot to burst all balloons.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: points = [[10,16],[2,8],[1,6],[7,12]] <br />
+                    Output: 2 <br />
+                    Explanation: The balloons can be burst by 2 arrows: <br />
+                    - Shoot an arrow at x = 6, bursting the balloons [2,8] and
+                    [1,6]. <br />- Shoot an arrow at x = 11, bursting the
+                    balloons [10,16] and [7,12].
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: points = [[1,2],[3,4],[5,6],[7,8]] <br />
+                    Output: 4 <br />
+                    Explanation: One arrow needs to be shot for each balloon for
+                    a total of 4 arrows.
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: points = [[1,2],[2,3],[3,4],[4,5]] <br />
+                    Output: 2 <br />
+                    Explanation: The balloons can be burst by 2 arrows: <br />
+                    - Shoot an arrow at x = 2, bursting the balloons [1,2] and
+                    [2,3]. <br />- Shoot an arrow at x = 4, bursting the
+                    balloons [3,4] and [4,5].
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                1 &lt;= points.length &lt;= 10^5 <br />
+                points[i].length == 2 <br />
+                -2^31 &lt;= xstart &lt; xend &lt;= 2^31 - 1
               </>
             }
-            tc="n"
-            sc="n"
+            tc="n.log n"
+            sc="1"
             codes={{
               Javascript: {
                 code: ``,
