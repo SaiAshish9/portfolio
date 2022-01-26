f5da821f56f05b87721709c8ff16130ec31cfa07
saiashish9 <saiashish3760@gmail.com>
2022-01-26 16:42:28 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -61618,8 +61618,23 @@ class Node {
             sc="1"
             codes={{
               Javascript: {
-                code: ``,
-                output: ``,
+                code: `/**
+                * @param {number} x
+                * @param {number} y
+                * @return {number}
+                */
+               var hammingDistance = function(x, y) {
+                 let res = 0;
+                 while (x > 0 || y > 0) {
+                   res += (x & 1) ^ (y & 1);
+                   x >>= 1;
+                   y >>= 1;
+                 }
+                 return res;
+               };
+               
+               console.log(hammingDistance(1,4))`,
+                output: `2`,
               },
             }}
           />
