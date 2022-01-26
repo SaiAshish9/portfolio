712b5dc77e8f8601bdb6053b5ec914db972cdf46
saiashish9 <saiashish3760@gmail.com>
2022-01-27 03:52:54 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -61645,31 +61645,88 @@ class Node {
         content: (
           <Comp
             title="Q462. Minimum Moves to Equal Array Elements II (Q393)"
-            content1={<></>}
+            content1={
+              <>
+                Given an integer array nums of size n, return the minimum number
+                of moves required to make all array elements equal.
+                <br />
+                In one move, you can increment or decrement an element of the
+                array by 1.
+                <br />
+                Test cases are designed so that the answer will fit in a 32-bit
+                integer.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: nums = [1,2,3] <br />
+                    Output: 2 <br />
+                    Explanation: <br />
+                    Only two moves are needed (remember each move increments or
+                    decrements one element): <br />
+                    [1,2,3] =&gt; [2,2,3] =&gt; [2,2,2]
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: nums = [1,10,2,9] <br />
+                    Output: 16
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                n == nums.length <br />
+                1 &lt;= nums.length &lt;= 10^5 <br />
+                -10^9 &lt;= nums[i] &lt;= 1^09
               </>
             }
             tc="n"
-            sc="n"
+            sc="1"
             codes={{
-              Javascript: {
-                code: ``,
-                output: ``,
+              Java: {
+                code: `
+                [1,10,2,9]
+                class Solution {
+                  public int minMoves2(int[] nums) {
+                    final int n = nums.length;
+                    final int median = quickSelect(nums, 0, n - 1, (n + 1) / 2);
+                    int ans = 0;
+                    for (final int num : nums)
+                      ans += Math.abs(num - median);
+                    return ans;
+                  }
+  
+                  private int quickSelect(int[] nums, int l, int r, int k) {
+                    final int randIndex = new Random().nextInt(r - l + 1) + l;
+                    swap(nums, randIndex, r);
+                    final int pivot = nums[r];
+                    int nextSwapped = l;
+                    for (int i = l; i < r; ++i)
+                      if (nums[i] <= pivot)
+                        swap(nums, nextSwapped++, i);
+                    swap(nums, nextSwapped, r);
+                    final int count = nextSwapped - l + 1;
+                    if (count == k)
+                      return nums[nextSwapped];
+                    if (count > k)
+                      return quickSelect(nums, l, nextSwapped - 1, k);
+                    return quickSelect(nums, nextSwapped + 1, r, k - count);
+                  }
+                  private void swap(int[] nums, int i, int j) {
+                    final int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                  }
+                }
+               `,
+                output: `16`,
               },
             }}
           />
