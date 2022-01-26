2406d312a41f691e09f67287d1959ff01ecce5a8
saiashish9 <saiashish3760@gmail.com>
2022-01-26 15:45:12 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -60926,7 +60926,7 @@ class Node {
                };
                
                console.log(fourSumCount([0],[0],[0],[0]))`,
-                output: ``,
+                output: `1`,
               },
             }}
           />
@@ -60937,31 +60937,77 @@ class Node {
         content: (
           <Comp
             title="Q455. Assign Cookies (Q386)"
-            content1={<></>}
+            content1={
+              <>
+                Assume you are an awesome parent and want to give your children
+                some cookies. But, you should give each child at most one
+                cookie.
+                <br />
+                Each child i has a greed factor g[i], which is the minimum size
+                of a cookie that the child will be content with; and each cookie
+                j has a size s[j]. If s[j] &gt;= g[i], we can assign the cookie
+                j to the child i, and the child i will be content. Your goal is
+                to maximize the number of your content children and output the
+                maximum number.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: g = [1,2,3], s = [1,1] <br />
+                    Output: 1 <br />
+                    Explanation: You have 3 children and 2 cookies. The greed
+                    factors of 3 children are 1, 2, 3. And even though you have
+                    2 cookies, since their size is both 1, you could only make
+                    the child whose greed factor is 1 content. You need to
+                    output 1.
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: g = [1,2], s = [1,2,3] <br />
+                    Output: 2 <br />
+                    Explanation: You have 2 children and 3 cookies. The greed
+                    factors of 2 children are 1, 2. You have 3 cookies and their
+                    sizes are big enough to gratify all of the children, You
+                    need to output 2.
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                1 &lt;= g.length &lt;= 3 * 10^4
+                <br />
+                0 &lt;= s.length &lt;= 3 * 1^04
+                <br />1 &lt;= g[i], s[j] &lt;= 2^31 - 1
               </>
             }
-            tc="n"
-            sc="n"
+            tc="n.log n"
+            sc="1"
             codes={{
               Javascript: {
-                code: ``,
-                output: ``,
+                code: `/**
+                * @param {number[]} g
+                * @param {number[]} s
+                * @return {number}
+                */
+               var findContentChildren = function(g, s) {
+                 g.sort((a,b)=>a-b);
+                 s.sort((a,b)=>a-b);
+                 let i = 0;
+                 for (let j = 0; i < g.length && j < s.length; ++j)
+                   if (g[i] <= s[j])
+                   ++i;
+                 return i;  
+               };
+               
+               console.log(findContentChildren([1,2],[1,2,3]))`,
+                output: `2`,
               },
             }}
           />
