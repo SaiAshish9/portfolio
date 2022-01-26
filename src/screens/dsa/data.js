afeeaef5946bb65b55d2ab9dc2b9412949640bf9
saiashish9 <saiashish3760@gmail.com>
2022-01-26 16:35:38 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -61403,7 +61403,35 @@ class Node {
         content: (
           <Comp
             title="Q460. LFU Cache (Q391)"
-            content1={<></>}
+            content1={
+              <>
+                Design and implement a data structure for a Least Frequently
+                Used (LFU) cache.
+                <br />
+                Implement the LFUCache class:
+                <br />
+                LFUCache(int capacity) Initializes the object with the capacity
+                of the data structure.
+                <br /> int get(int key) Gets the value of the key if the key
+                exists in the cache. Otherwise, returns -1.
+                <br /> void put(int key, int value) Update the value of the key
+                if present, or inserts the key if not already present. When the
+                cache reaches its capacity, it should invalidate and remove the
+                least frequently used key before inserting a new item. For this
+                problem, when there is a tie (i.e., two or more keys with the
+                same frequency), the least recently used key would be
+                invalidated.
+                <br /> To determine the least frequently used key, a use counter
+                is maintained for each key in the cache. The key with the
+                smallest use counter is the least frequently used key.
+                <br /> When a key is first inserted into the cache, its use
+                counter is set to 1 (due to the put operation). The use counter
+                for a key in the cache is incremented either a get or put
+                operation is called on it.
+                <br /> The functions get and put must each run in O(1) average
+                time complexity.
+              </>
+            }
             content2={null}
             examples={[
               {
