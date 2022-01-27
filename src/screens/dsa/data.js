ee3eb9fde007c869bfca84e713b38f0b37c5a27b
saiashish9 <saiashish3760@gmail.com>
2022-01-27 23:35:06 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -64442,8 +64442,48 @@ Window position                Median
             sc="m.n"
             codes={{
               Javascript: {
-                code: ``,
-                output: ``,
+                code: `/**
+                * @param {number[][]} mat
+                * @return {number[]}
+                */
+               var findDiagonalOrder = function(mat) {
+                 const m = mat.length;
+                 const n = mat[0].length;
+                 const res = Array(m * n).fill(0);
+                 let d = 1; 
+                 let row = 0;
+                 let col = 0;
+                 for (let i = 0; i < m * n; ++i) {
+                   res[i] = mat[row][col];
+                   row -= d;
+                   col += d;
+                   if (row == m) {
+                     row = m - 1;
+                     col += 2;
+                     d = -d;
+                   }
+                   if (col == n) {
+                     col = n - 1;
+                     row += 2;
+                     d = -d;
+                   }
+                   if (row < 0) {
+                     row = 0;
+                     d = -d;
+                   }
+                   if (col < 0) {
+                     col = 0;
+                     d = -d;
+                   }
+                 }
+                 return res;
+               };
+               
+               console.log(findDiagonalOrder([[1,2,3],[4,5,6],[7,8,9]]))`,
+                output: `[
+                  1, 2, 4, 7, 5,
+                  3, 6, 8, 9
+                ]`,
               },
             }}
           />
