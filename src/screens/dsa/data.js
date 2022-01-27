391413e691867f39e6a223870f05d9739c396d0a
saiashish9 <saiashish3760@gmail.com>
2022-01-27 22:23:09 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -63995,26 +63995,62 @@ Window position                Median
         content: (
           <Comp
             title="Q494. Target Sum (Q418)"
-            content1={<></>}
+            content1={
+              <>
+                You are given an integer array nums and an integer target.
+                <br />
+                You want to build an expression out of nums by adding one of the
+                symbols '+' and '-' before each integer in nums and then
+                concatenate all the integers.
+                <br />
+                For example, if nums = [2, 1], you can add a '+' before 2 and a
+                '-' before 1 and concatenate them to build the expression
+                "+2-1". Return the number of different expressions that you can
+                build, which evaluates to target.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: nums = [1,1,1,1,1], target = 3 <br />
+                    Output: 5 <br />
+                    Explanation: There are 5 ways to assign symbols to make the
+                    sum of nums be target 3.
+                    <pre>
+                      {`
+-1 + 1 + 1 + 1 + 1 = 3
++1 - 1 + 1 + 1 + 1 = 3
++1 + 1 - 1 + 1 + 1 = 3
++1 + 1 + 1 - 1 + 1 = 3
++1 + 1 + 1 + 1 - 1 = 3     
+     `}
+                    </pre>
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: nums = [1], target = 1 <br />
+                    Output: 1
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                1 &lt;= nums.length &lt;= 20
+                <br />
+                0 &lt;= nums[i] &lt;= 1000
+                <br />
+                0 &lt;= sum(nums[i]) &lt;= 1000
+                <br />
+                -1000 v= target &lt;= 1000
               </>
             }
-            tc="n"
+            tc="n.k"
             sc="n"
             codes={{
               Javascript: {
