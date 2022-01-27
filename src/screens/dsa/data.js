401671b718a83c3cad36cffda4b93841e28934cf
saiashish9 <saiashish3760@gmail.com>
2022-01-28 00:44:31 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -64821,23 +64821,45 @@ Window position                Median
         content: (
           <Comp
             title="Q503. Next Greater Element II (Q426)"
-            content1={<></>}
+            content1={
+              <>
+                Given a circular integer array nums (i.e., the next element of
+                nums[nums.length - 1] is nums[0]), return the next greater
+                number for every element in nums.
+                <br />
+                The next greater number of a number x is the first greater
+                number to its traversing-order next in the array, which means
+                you could search circularly to find its next greater number. If
+                it doesn't exist, return -1 for this number.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: nums = [1,2,1] <br />
+                    Output: [2,-1,2] <br />
+                    Explanation: The first 1's next greater number is 2; <br />
+                    The number 2 can't find next greater number. <br />
+                    The second 1's next greater number needs to search
+                    circularly, which is also 2.
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: nums = [1,2,3,4,3] <br />
+                    Output: [2,3,4,-1,4]
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                1 &lt;= nums.length &lt;= 10^4 <br />
+                -10^9 &lt;= nums[i] &lt;= 10^9
               </>
             }
             tc="n"
