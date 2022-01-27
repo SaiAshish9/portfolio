87f11dea16b6650e3c09bd74c47b9b41df7635df
saiashish9 <saiashish3760@gmail.com>
2022-01-27 19:44:01 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -63179,23 +63179,53 @@ Window position                Median
         content: (
           <Comp
             title="Q482. License Key Formatting (Q410)"
-            content1={<></>}
+            content1={
+              <>
+                You are given a license key represented as a string s that
+                consists of only alphanumeric characters and dashes. The string
+                is separated into n + 1 groups by n dashes. You are also given
+                an integer k.
+                <br />
+                We want to reformat the string s such that each group contains
+                exactly k characters, except for the first group, which could be
+                shorter than k but still must contain at least one character.
+                Furthermore, there must be a dash inserted between two groups,
+                and you should convert all lowercase letters to uppercase.
+                <br />
+                Return the reformatted license key.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: s = "5F3Z-2e-9-w", k = 4 <br />
+                    Output: "5F3Z-2E9W" <br />
+                    Explanation: The string s has been split into two parts,
+                    each part has 4 characters. Note that the two extra dashes
+                    are not needed and can be removed.
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: s = "2-5g-3-J", k = 2<br />
+                    Output: "2-5G-3J"
+                    <br />
+                    Explanation: The string s has been split into three parts,
+                    each part has 2 characters except the first part as it could
+                    be shorter as mentioned above.
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                1 &lt;= s.length &lt;= 10^5 <br />
+                s consists of English letters, digits, and dashes '-'. <br />1
+                &lt;= k &lt;= 10^4
               </>
             }
             tc="n"
