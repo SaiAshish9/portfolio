58a1583ff0fff23c6a6645991048a15b634d8117
saiashish9 <saiashish3760@gmail.com>
2022-01-27 17:06:44 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -62991,26 +62991,70 @@ class Node {
         title: "Q480. Sliding Window Median (Q408)",
         content: (
           <Comp
-            content1={<></>}
+            content1={
+              <>
+                The median is the middle value in an ordered integer list. If
+                the size of the list is even, there is no middle value. So the
+                median is the mean of the two middle values.
+                <br />
+                For examples, if arr = [2,3,4], the median is 3.
+                <br />
+                For examples, if arr = [1,2,3,4], the median is (2 + 3) / 2 =
+                2.5.
+                <br />
+                You are given an integer array nums and an integer k. There is a
+                sliding window of size k which is moving from the very left of
+                the array to the very right. You can only see the k numbers in
+                the window. Each time the sliding window moves right by one
+                position.
+                <br />
+                Return the median array for each window in the original array.
+                Answers within 10-5 of the actual value will be accepted.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: nums = [1,3,-1,-3,5,3,6,7],
+                    <br /> k = 3<br />
+                    Output: [1.00000,-1.00000,-1.00000,3.00000,5.00000,6.00000]
+                    <br />
+                    Explanation: <br />
+                    <pre>
+                      {`
+Window position                Median
+---------------                -----
+[1  3  -1] -3  5  3  6  7        1
+ 1 [3  -1  -3] 5  3  6  7       -1
+ 1  3 [-1  -3  5] 3  6  7       -1
+ 1  3  -1 [-3  5  3] 6  7        3
+ 1  3  -1  -3 [5  3  6] 7        5
+ 1  3  -1  -3  5 [3  6  7]       6                  
+                  `}
+                    </pre>
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: nums = [1,2,3,4,2,3,1,4,2],
+                    <br /> k = 3<br />
+                    Output:
+                    [2.00000,3.00000,3.00000,3.00000,2.00000,3.00000,2.00000]
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                1 &lt;= k &lt;= nums.length &lt;= 10^5 <br />
+                -2^31 &lt;= nums[i] &lt;= 2^31 - 1
               </>
             }
-            tc="n"
+            tc="n.log n"
             sc="n"
             codes={{
               Javascript: {
