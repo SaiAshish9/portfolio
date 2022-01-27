e14ca54b5077ff06259d4b531fe0720cfa300ba7
saiashish9 <saiashish3760@gmail.com>
2022-01-27 16:11:12 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -62752,23 +62752,48 @@ class Node {
         content: (
           <Comp
             title="Q477. Total Hamming Distance (Q405)"
-            content1={<></>}
+            content1={
+              <>
+                The Hamming distance between two integers is the number of
+                positions at which the corresponding bits are different.
+                <br />
+                Given an integer array nums, return the sum of Hamming distances
+                between all the pairs of the integers in nums.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: nums = [4,14,2] <br />
+                    Output: 6 <br />
+                    Explanation: In binary representation, the 4 is 0100, 14 is
+                    1110, and 2 is 0010 (just showing the four bits relevant in
+                    this case).
+                    <br />
+                    The answer will be: HammingDistance(4, 14) +
+                    HammingDistance(4, 2) + HammingDistance(14, 2) = 2 + 2 + 2 =
+                    6.
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: nums = [4,14,4] <br />
+                    Output: 4
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                The Hamming distance between two integers is the number of
+                positions at which the corresponding bits are different.
+                <br />
+                Given an integer array nums, return the sum of Hamming distances
+                between all the pairs of the integers in nums.
               </>
             }
             tc="n"
