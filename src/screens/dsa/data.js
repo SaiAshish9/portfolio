c8989dc3f54c2429be2d3bbe99e6d1c8109872e2
saiashish9 <saiashish3760@gmail.com>
2022-01-27 20:31:56 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -63498,7 +63498,40 @@ Window position                Median
         content: (
           <Comp
             title="Q488. Zuma Game (Q414)"
-            content1={<></>}
+            content1={
+              <>
+                You are playing a variation of the game Zuma.
+                <br />
+                In this variation of Zuma, there is a single row of colored
+                balls on a board, where each ball can be colored red 'R', yellow
+                'Y', blue 'B', green 'G', or white 'W'. You also have several
+                colored balls in your hand.
+                <br />
+                Your goal is to clear all of the balls from the board. On each
+                turn:
+                <br />
+                Pick any ball from your hand and insert it in between two balls
+                in the row or on either end of the row.
+                <br />
+                If there is a group of three or more consecutive balls of the
+                same color, remove the group of balls from the board.
+                <br />
+                If this removal causes more groups of three or more of the same
+                color to form, then continue removing each group until there are
+                none left.
+                <br />
+                If there are no more balls on the board, then you win the game.
+                <br />
+                Repeat this process until you either win or do not have any more
+                balls in your hand.
+                <br />
+                Given a string board, representing the row of balls on the
+                board, and a string hand, representing the balls in your hand,
+                return the minimum number of balls you have to insert to clear
+                all the balls from the board. If you cannot clear all the balls
+                from the board using the balls in your hand, return -1.
+              </>
+            }
             content2={null}
             examples={[
               {
