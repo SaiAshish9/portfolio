aa121c75243a21151b4ef1f424b812a2489e95c5
saiashish9 <saiashish3760@gmail.com>
2022-01-27 14:47:37 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -62101,8 +62101,26 @@ class Node {
             sc="1"
             codes={{
               Javascript: {
-                code: ``,
-                output: ``,
+                code: `/**
+                * @param {string} p
+                * @return {number}
+                */
+               var findSubstringInWraproundString = function(p) {
+                 let maxLength = 1;
+                 const count = Array(26).fill(0); 
+                 for (let i = 0; i < p.length; ++i) {
+                   if (i > 0 && (p[i].charCodeAt(0) - p[i - 1].charCodeAt(0) == 1 || p[i - 1].charCodeAt(0) - p[i].charCodeAt(0) == 25))
+                     ++maxLength;
+                   else
+                     maxLength = 1;
+                   let index = p[i].charCodeAt(0) - 97;
+                   count[index] = Math.max(count[index], maxLength);
+                 }
+                 return count.reduce((a,b)=>a+b,0);
+               };
+               
+               console.log(findSubstringInWraproundString("zab"))`,
+                output: `6`,
               },
             }}
           />
@@ -62113,27 +62131,74 @@ class Node {
         content: (
           <Comp
             title="Q468. Validate IP Address (Q398)"
-            content1={<></>}
+            content1={
+              <>
+                Given a string queryIP, return "IPv4" if IP is a valid IPv4
+                address, "IPv6" if IP is a valid IPv6 address or "Neither" if IP
+                is not a correct IP of any type.
+                <br />
+                A valid IPv4 address is an IP in the form "x1.x2.x3.x4" where 0
+                &lt;= xi &lt;= 255 and xi cannot contain leading zeros. For
+                example, "192.168.1.1" and "192.168.1.0" are valid IPv4
+                addresses but "192.168.01.1", while "192.168.1.00" and
+                "192.168@1.1" are invalid IPv4 addresses.
+                <br />
+                A valid IPv6 address is an IP in the form
+                "x1:x2:x3:x4:x5:x6:x7:x8" where:
+                <br />
+                1 &lt;= xi.length &lt;= 4<br />
+                xi is a hexadecimal string which may contain digits, lower-case
+                English letter ('a' to 'f') and upper-case English letters ('A'
+                to 'F').
+                <br />
+                Leading zeros are allowed in xi.
+                <br />
+                For example, "2001:0db8:85a3:0000:0000:8a2e:0370:7334" and
+                "2001:db8:85a3:0:0:8A2E:0370:7334" are valid IPv6 addresses,
+                while "2001:0db8:85a3::8A2E:037j:7334" and
+                "02001:0db8:85a3:0000:0000:8a2e:0370:7334" are invalid IPv6
+                addresses.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: queryIP = "172.16.254.1" <br />
+                    Output: "IPv4" <br />
+                    Explanation: This is a valid IPv4 address, return "IPv4".
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: queryIP = "2001:0db8:85a3:0:0:8A2E:0370:7334" <br />
+                    Output: "IPv6" <br />
+                    Explanation: This is a valid IPv6 address, return "IPv6".
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: queryIP = "256.256.256.256" <br />
+                    Output: "Neither" <br />
+                    Explanation: This is neither a IPv4 address nor a IPv6
+                    address.
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                queryIP consists only of English letters, digits and the
+                characters '.' and ':'.
               </>
             }
             tc="n"
-            sc="n"
+            sc="1"
             codes={{
               Javascript: {
                 code: ``,
