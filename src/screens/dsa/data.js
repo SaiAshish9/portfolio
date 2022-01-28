64503eef1d2026e411dd1ab36d66ffc86a74feac
saiashish9 <saiashish3760@gmail.com>
2022-01-29 00:29:01 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -65793,8 +65793,22 @@ Window position                Median
             sc="n"
             codes={{
               Javascript: {
-                code: ``,
-                output: ``,
+                code: `/**
+                * @param {number} amount
+                * @param {number[]} coins
+                * @return {number}
+                */
+               var change = function(amount, coins) {
+                 const dp = Array(amount + 1).fill(0);
+                 dp[0] = 1;
+                 for (let coin of coins)
+                   for (let i = coin; i <= amount; ++i)
+                     dp[i] += dp[i - coin];
+                 return dp[amount];  
+               };
+               
+               console.log(change(5,[1,2,5]))`,
+                output: `4`,
               },
             }}
           />
