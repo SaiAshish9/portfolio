a8dd63ba8ce734e48022f60d87b4eb8d7c5093d8
saiashish9 <saiashish3760@gmail.com>
2022-01-28 22:27:28 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -65350,7 +65350,37 @@ Window position                Median
         content: (
           <Comp
             title="Q514. Freedom Trail (Q433)"
-            content1={<></>}
+            content1={
+              <>
+                In the video game Fallout 4, the quest "Road to Freedom"
+                requires players to reach a metal dial called the "Freedom Trail
+                Ring" and use the dial to spell a specific keyword to open the
+                door.
+                <br />
+                Given a string ring that represents the code engraved on the
+                outer ring and another string key that represents the keyword
+                that needs to be spelled, return the minimum number of steps to
+                spell all the characters in the keyword.
+                <br />
+                Initially, the first character of the ring is aligned at the
+                "12:00" direction. You should spell all the characters in key
+                one by one by rotating ring clockwise or anticlockwise to make
+                each character of the string key aligned at the "12:00"
+                direction and then by pressing the center button.
+                <br />
+                At the stage of rotating the ring to spell the key character
+                key[i]:
+                <br />
+                You can rotate the ring clockwise or anticlockwise by one place,
+                which counts as one step. The final purpose of the rotation is
+                to align one of ring's characters at the "12:00" direction,
+                where this character must equal key[i]. If the character key[i]
+                has been aligned at the "12:00" direction, press the center
+                button to spell, which also counts as one step. After the
+                pressing, you could begin to spell the next character in the key
+                (next stage). Otherwise, you have finished all the spelling.
+              </>
+            }
             content2={null}
             examples={[
               {
