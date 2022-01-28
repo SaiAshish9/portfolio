7f0d89cfe1e98ec4d305b20a8a83458cd18accbf
saiashish9 <saiashish3760@gmail.com>
2022-01-28 20:17:35 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -65199,25 +65199,49 @@ Window position                Median
         content: (
           <Comp
             title="Q509. Fibonacci Number (Q431)"
-            content1={<></>}
+            content1={
+              <>
+                The Fibonacci numbers, commonly denoted F(n) form a sequence,
+                called the Fibonacci sequence, such that each number is the sum
+                of the two preceding ones, starting from 0 and 1. That is,
+                <br />
+                F(0) = 0, F(1) = 1<br />
+                F(n) = F(n - 1) + F(n - 2), for n &gt; 1.
+                <br />
+                Given n, calculate F(n).
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: n = 2 <br />
+                    Output: 1 <br />
+                    Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: n = 3 <br />
+                    Output: 2 <br />
+                    Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: n = 4<br />
+                    Output: 3<br />
+                    Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
-              <>
-                <b>Follow up :</b>
-              </>
-            }
+            constraints={<>0 &lt;= n &lt;= 30</>}
             tc="n"
             sc="n"
             codes={{
