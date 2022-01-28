310458443acaddb7acaf3e44b16081a19a6db868
saiashish9 <saiashish3760@gmail.com>
2022-01-28 23:49:26 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -65627,27 +65627,78 @@ Window position                Median
         content: (
           <Comp
             title="Q517. Super Washing Machines (Q436)"
-            content1={<></>}
+            content1={
+              <>
+                You have n super washing machines on a line. Initially, each
+                washing machine has some dresses or is empty.
+                <br />
+                For each move, you could choose any m (1 &lt;= m &lt;= n)
+                washing machines, and pass one dress of each washing machine to
+                one of its adjacent washing machines at the same time.
+                <br />
+                Given an integer array machines representing the number of
+                dresses in each washing machine from left to right on the line,
+                return the minimum number of moves to make all the washing
+                machines have the same number of dresses. If it is not possible
+                to do it, return -1.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: machines = [1,0,5] <br />
+                    Output: 3 <br />
+                    Explanation: <br />
+                    <pre>
+                      {`
+1st move:    1     0 <-- 5    =>    1     1     4
+2nd move:    1 <-- 1 <-- 4    =>    2     1     3
+3rd move:    2     1 <-- 3    =>    2     2     2
+                    `}
+                    </pre>
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: machines = [0,3,0]
+                    <br />
+                    Output: 2<br />
+                    Explanation:
+                    <br />
+                    <pre>
+                      {`
+1st move:    0 <-- 3     0    =>    1     2     0
+2nd move:    1     2 --> 0    =>    1     1     1  
+  `}
+                    </pre>
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: machines = [0,2,0] <br />
+                    Output: -1 <br />
+                    Explanation: It's impossible to make all three washing
+                    machines have the same number of dresses.
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                n == machines.length
+                <br />
+                1 &lt;= n &lt;= 10^4
+                <br />0 &lt;= machines[i] &lt;= 10^5
               </>
             }
             tc="n"
-            sc="n"
+            sc="1"
             codes={{
               Javascript: {
                 code: ``,
