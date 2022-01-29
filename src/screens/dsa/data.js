e92286b548d572c620dff17764cb7162f1ded667
saiashish9 <saiashish3760@gmail.com>
2022-01-29 17:25:37 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -66522,7 +66522,21 @@ Window position                Median
         content: (
           <Comp
             title="Q528. Random Pick with Weight (Q446)"
-            content1={<></>}
+            content1={
+              <>
+                You are given a 0-indexed array of positive integers w where
+                w[i] describes the weight of the ith index.
+                <br />
+                You need to implement the function pickIndex(), which randomly
+                picks an index in the range [0, w.length - 1] (inclusive) and
+                returns it. The probability of picking an index i is w[i] /
+                sum(w).
+                <br />
+                For example, if w = [1, 3], the probability of picking index 0
+                is 1 / (1 + 3) = 0.25 (i.e., 25%), and the probability of
+                picking index 1 is 3 / (1 + 3) = 0.75 (i.e., 75%).
+              </>
+            }
             content2={null}
             examples={[
               {
