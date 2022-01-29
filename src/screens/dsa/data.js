d9f8d4bc2abefe2b57a23c14c1e23cdb567efd6d
saiashish9 <saiashish3760@gmail.com>
2022-01-29 23:02:47 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -67398,27 +67398,46 @@ Window position                Median
         content: (
           <Comp
             title="Q541. Reverse String II (Q455)"
-            content1={<></>}
+            content1={
+              <>
+                Given a string s and an integer k, reverse the first k
+                characters for every 2k characters counting from the start of
+                the string.
+                <br />
+                If there are fewer than k characters left, reverse all of them.
+                If there are less than 2k but greater than or equal to k
+                characters, then reverse the first k characters and leave the
+                other as original.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: s = "abcdefg", k = 2<br />
+                    Output: "bacdfeg"
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: s = "abcd", k = 2<br />
+                    Output: "bacd"
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                1 &lt;= s.length &lt;= 10^4 <br />
+                s consists of only lowercase English letters.
+                <br />1 &lt;= k &lt;= 10^4
               </>
             }
-            tc="n"
-            sc="n"
+            tc="n^2"
+            sc="1"
             codes={{
               Javascript: {
                 code: ``,
