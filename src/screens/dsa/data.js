944dc1dd85ed19cceb1a6a9f65231a85357be068
saiashish9 <saiashish3760@gmail.com>
2022-01-29 19:16:31 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -66978,27 +66978,57 @@ Window position                Median
         content: (
           <Comp
             title="Q535. Encode and Decode TinyURL (Q450)"
-            content1={<></>}
+            content1={
+              <>
+                Note: This is a companion problem to the System Design problem:
+                Design TinyURL.
+                <br /> TinyURL is a URL shortening service where you enter a URL
+                such as https://leetcode.com/problems/design-tinyurl and it
+                returns a short URL such as http://tinyurl.com/4e9iAk. Design a
+                class to encode a URL and decode a tiny URL.
+                <br />
+                There is no restriction on how your encode/decode algorithm
+                should work. You just need to ensure that a URL can be encoded
+                to a tiny URL and the tiny URL can be decoded to the original
+                URL.
+                <br />
+                Implement the Solution class:
+                <br />
+                Solution() Initializes the object of the system.
+                <br /> String encode(String longUrl) Returns a tiny URL for the
+                given longUrl.
+                <br /> String decode(String shortUrl) Returns the original long
+                URL for the given shortUrl. It is guaranteed that the given
+                shortUrl was encoded by the same object.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: url = "https://leetcode.com/problems/design-tinyurl"
+                    <br /> Output:
+                    "https://leetcode.com/problems/design-tinyurl"
+                    <br />
+                    Explanation:
+                    <br /> Solution obj = new Solution();
+                    <br /> string tiny = obj.encode(url); // returns the encoded
+                    tiny url.
+                    <br /> string ans = obj.decode(tiny); // returns the
+                    original url after deconding it.
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                1 &lt;= url.length &lt;= 10^4 <br />
+                url is guranteed to be a valid URL.
               </>
             }
             tc="n"
-            sc="n"
+            sc="h"
             codes={{
               Javascript: {
                 code: ``,
