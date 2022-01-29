49832696bb2c29ae7cece89844f4e649d7fe6841
saiashish9 <saiashish3760@gmail.com>
2022-01-30 04:34:30 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -68022,8 +68022,33 @@ Window position                Median
             sc="1"
             codes={{
               Javascript: {
-                code: ``,
-                output: ``,
+                code: `
+                // 2
+                class Solution {
+                  public int checkRecord(int n) {
+                    final long kMod = (long) 1e9 + 7;
+                    long[][] dp = new long[2][3];
+                    dp[0][0] = 1;
+                
+                    while (n-- > 0) {
+                      long[][] prev = Arrays.stream(dp)
+                                          .map((long[] A) -> A.clone())
+                                          .toArray((int length) -> new long[length][]);
+                
+                      dp[0][0] = (prev[0][0] + prev[0][1] + prev[0][2]) % kMod;
+                      dp[0][1] = prev[0][0];
+                      dp[0][2] = prev[0][1];
+                      dp[1][0] = (prev[0][0] + prev[0][1] + prev[0][2] +
+                                  prev[1][0] + prev[1][1] + prev[1][2]) % kMod;
+                      dp[1][1] = prev[1][0];
+                      dp[1][2] = prev[1][1];
+                    }
+                
+                    return (int) ((dp[0][0] + dp[0][1] + dp[0][2] + dp[1][0] + dp[1][1] + dp[1][2]) % kMod);
+                  }
+                }
+                `,
+                output: `8`,
               },
             }}
           />
