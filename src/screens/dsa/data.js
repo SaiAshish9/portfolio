21cb994d1d1b8581aed0ed335b7345dc9b840a94
saiashish9 <saiashish3760@gmail.com>
2022-01-30 23:00:42 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -70749,26 +70749,58 @@ class Node {
         ),
       },
       q490: {
-        title: "Q (Q490)",
+        title: "Q599. Minimum Index Sum of Two Lists (Q490)",
         content: (
           <Comp
-            content1={<></>}
+            content1={
+              <>
+                Suppose Andy and Doris want to choose a restaurant for dinner,
+                and they both have a list of favorite restaurants represented by
+                strings.
+                <br />
+                You need to help them find out their common interest with the
+                least list index sum. If there is a choice tie between answers,
+                output all of them with no order requirement. You could assume
+                there always exists an answer.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: list1 = ["Shogun","Tapioca Express","Burger
+                    King","KFC"], list2 = ["Piatti","The Grill at Torrey
+                    Pines","Hungry Hunter Steakhouse","Shogun"]
+                    <br /> Output: ["Shogun"]
+                    <br />
+                    Explanation: The only restaurant they both like is "Shogun".
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: list1 = ["Shogun","Tapioca Express","Burger
+                    King","KFC"], list2 = ["KFC","Shogun","Burger King"]
+                    <br /> Output: ["Shogun"]
+                    <br />
+                    Explanation: The restaurant they both like and have the
+                    least index sum is "Shogun" with index sum 1 (0+1).
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                1 &lt;= list1.length, list2.length &lt;= 1000 <br />
+                1 &lt;= list1[i].length, list2[i].length &lt;= 30
+                <br />
+                list1[i] and list2[i] consist of spaces ' ' and English letters.
+                <br />
+                All the stings of list1 are unique.
+                <br />
+                All the stings of list2 are unique.
               </>
             }
             tc="n"
