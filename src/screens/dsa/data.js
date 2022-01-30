43912080d217d0f814e353364d47a41f1ce6ba39
saiashish9 <saiashish3760@gmail.com>
2022-01-30 20:22:30 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -69938,23 +69938,53 @@ class Node {
         title: "Q (Q482)",
         content: (
           <Comp
-            content1={<></>}
+            content1={
+              <>
+                Given the root of an n-ary tree, return the postorder traversal
+                of its nodes' values.
+                <br />
+                Nary-Tree input serialization is represented in their level
+                order traversal. Each group of children is separated by the null
+                value (See examples)
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                img: Leetcode559,
+
+                content: (
+                  <>
+                    Input: root = [1,null,3,2,4,null,5,6]
+                    <br />
+                    Output: [5,6,3,2,4,1]
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: root =
+                    [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+                    <br />
+                    Output: [2,6,14,11,7,3,12,8,4,13,9,10,5,1]
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
+            constraints={
+              <>
+                The number of nodes in the tree is in the range [0, 10^4].
+                <br />
+                0 &lt;= Node.val &lt;= 10^4
+                <br />
+                The height of the n-ary tree is less than or equal to 1000.
+              </>
+            }
             fp={
               <>
-                <b>Follow up :</b>
+                <b>Follow up :</b> Recursive solution is trivial, could you do
+                it iteratively?
               </>
             }
             tc="n"
