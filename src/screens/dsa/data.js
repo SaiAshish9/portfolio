74a96c98893da61c8093854084cee395271a187b
saiashish9 <saiashish3760@gmail.com>
2022-02-01 15:02:16 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -72143,6 +72143,7 @@ console.log(tree2str(t))`,
         title: "Q628. Maximum Product of Three Numbers (Q504)",
         content: (
           <Comp
+            title="Q628. Maximum Product of Three Numbers (Q504)"
             content1={
               <>
                 Given an integer array nums, find three numbers whose product is
@@ -72190,8 +72191,39 @@ console.log(tree2str(t))`,
             sc="1"
             codes={{
               Javascript: {
-                code: ``,
-                output: ``,
+                code: `/**
+                * @param {number[]} nums
+                * @return {number}
+                */
+               var maximumProduct = function(nums) {
+                 let min1 = Number.MAX_SAFE_INTEGER; 
+                 let min2 = Number.MAX_SAFE_INTEGER; 
+                 let max1 = Number.MIN_SAFE_INTEGER; 
+                 let max2 = Number.MIN_SAFE_INTEGER; 
+                 let max3 = Number.MIN_SAFE_INTEGER; 
+                 for (let num of nums) {
+                   if (num <= min1) {
+                     min2 = min1;
+                     min1 = num;
+                   } else if (num <= min2) {
+                     min2 = num;
+                   }
+                   if (num >= max1) {
+                     max3 = max2;
+                     max2 = max1;
+                     max1 = num;
+                   } else if (num >= max2) {
+                     max3 = max2;
+                     max2 = num;
+                   } else if (num >= max3) {
+                     max3 = num;
+                   }
+                 }
+                 return Math.max(min1 * min2 * max1, max1 * max2 * max3);
+               };
+               
+               console.log(maximumProduct([1,2,3],6))`,
+                output: `6`,
               },
             }}
           />
