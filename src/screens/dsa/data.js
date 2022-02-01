828fffe86b47da8e60aad93f462c4b34acb2fcb0
saiashish9 <saiashish3760@gmail.com>
2022-02-01 17:58:19 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -72408,6 +72408,7 @@ console.log(tree2str(t))`,
         title: "Q632. Smallest Range Covering Elements from K Lists (Q507)",
         content: (
           <Comp
+            title="Q632. Smallest Range Covering Elements from K Lists (Q507)"
             content1={
               <>
                 You have k lists of sorted integers in non-decreasing order.
@@ -72453,9 +72454,51 @@ console.log(tree2str(t))`,
             tc="n^2.log k"
             sc="k"
             codes={{
-              Javascript: {
-                code: ``,
-                output: ``,
+              Java: {
+                code: `// [[1,2,3],[1,2,3],[1,2,3]]
+                class T {
+                  public int i;
+                  public int j;
+                  public int num; // nums[i][j]
+                  public T(int i, int j, int num) {
+                    this.i = i;
+                    this.j = j;
+                    this.num = num;
+                  }
+                }
+                
+                class Solution {
+                  public int[] smallestRange(List<List<Integer>> nums) {
+                    PriorityQueue<T> pq = new PriorityQueue<>((a, b) -> a.num - b.num);
+                    int min = Integer.MAX_VALUE;
+                    int max = Integer.MIN_VALUE;
+                    for (int i = 0; i < nums.size(); ++i) {
+                      final int num = nums.get(i).get(0);
+                      pq.offer(new T(i, 0, num));
+                      min = Math.min(min, num);
+                      max = Math.max(max, num);
+                    }
+                    int minRange = min;
+                    int maxRange = max;
+                    while (pq.size() == nums.size()) {
+                      final int i = pq.peek().i;
+                      final int j = pq.poll().j;
+                      if (j + 1 < nums.get(i).size()) {
+                        pq.offer(new T(i, j + 1, nums.get(i).get(j + 1)));
+                        max = Math.max(max, nums.get(i).get(j + 1));
+                        min = pq.peek().num;
+                      }
+                      if (max - min < maxRange - minRange) {
+                        minRange = min;
+                        maxRange = max;
+                      }
+                    }
+                
+                    return new int[] {minRange, maxRange};
+                  }
+                }
+                `,
+                output: `[ 1, 1 ]`,
               },
             }}
           />
