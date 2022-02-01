a5f4722e26b8eb551e8181ebc82b6e44f7592a76
saiashish9 <saiashish3760@gmail.com>
2022-02-01 21:42:41 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -73373,8 +73373,25 @@ class Solution:
             sc="1"
             codes={{
               Javascript: {
-                code: ``,
-                output: ``,
+                code: `/**
+                * @param {number[]} nums
+                * @param {number} k
+                * @return {number}
+                */
+               var findMaxAverage = function(nums, k) {
+                 let sum = 0;
+                 for (let i = 0; i < k; ++i)
+                   sum += nums[i];
+                 let res = sum;
+                 for (let i = k; i < nums.length; ++i) {
+                   sum += nums[i] - nums[i - k];
+                   res = Math.max(res, sum);
+                 }
+                 return res / k;
+               };
+               
+               console.log(findMaxAverage([1,12,-5,-6,50,3],4))`,
+                output: `12.75`,
               },
             }}
           />
