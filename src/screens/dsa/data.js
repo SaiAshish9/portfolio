64e5c049f797a006000ac98dc6f9c64e934d6847
saiashish9 <saiashish3760@gmail.com>
2022-02-01 22:41:05 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -73594,8 +73594,31 @@ class Solution:
             sc="1"
             codes={{
               Javascript: {
-                code: ``,
-                output: ``,
+                code: `/**
+                * @param {string} s
+                * @return {number}
+                */
+               var countSubstrings = function(s) {
+                 let res = 0;
+                 for (let i = 0; i < s.length; ++i) {
+                   res += extendPalindromes(s, i, i);
+                   res += extendPalindromes(s, i, i + 1);
+                 }
+                 return res;
+               }
+               
+               function extendPalindromes(s, l, r) {
+                 let count = 0;
+                 while (l >= 0 && r < s.length && s[l] == s[r]) {
+                   ++count;
+                   --l;
+                   ++r;
+                 }
+                 return count;
+               };
+               
+               console.log(countSubstrings("abc"))`,
+                output: `3`,
               },
             }}
           />
