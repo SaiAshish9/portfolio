319ccb1bf90aeb6916b9b7b2a1c67177d7375d85
saiashish9 <saiashish3760@gmail.com>
2022-02-01 21:33:56 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -73245,7 +73245,7 @@ class Solution:
                 deleteFront, deleteLast, getFront, getRear, isEmpty, isFull.
               </>
             }
-            tc="n"
+            tc="1"
             sc="n"
             codes={{
               Java: {
@@ -73327,27 +73327,49 @@ class Solution:
         title: "Q643. Maximum Average Subarray I (Q515)",
         content: (
           <Comp
-            content1={<></>}
+            content1={
+              <>
+                You are given an integer array nums consisting of n elements,
+                and an integer k.
+                <br />
+                Find a contiguous subarray whose length is equal to k that has
+                the maximum average value and return this value. Any answer with
+                a calculation error less than 10-5 will be accepted.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: nums = [1,12,-5,-6,50,3], k = 4<br />
+                    Output: 12.75000
+                    <br />
+                    Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 /
+                    4 = 12.75
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: nums = [5], k = 1 <br />
+                    Output: 5.00000
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                n == nums.length
+                <br />
+                1 &lt;= k &lt;= n &lt;= 105
+                <br />
+                -10^4 &lt;= nums[i] &lt;= 10^4
               </>
             }
             tc="n"
-            sc="n"
+            sc="1"
             codes={{
               Javascript: {
                 code: ``,
