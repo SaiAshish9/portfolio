aec6e5edef494bd68962e452eaa674db9fc68540
saiashish9 <saiashish3760@gmail.com>
2022-02-01 19:08:38 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -72570,31 +72570,141 @@ console.log(tree2str(t))`,
         title: "Q636. Exclusive Time of Functions (Q509)",
         content: (
           <Comp
-            content1={<></>}
+            content1={
+              <>
+                On a single-threaded CPU, we execute a program containing n
+                functions. Each function has a unique ID between 0 and n-1.
+                <br />
+                Function calls are stored in a call stack: when a function call
+                starts, its ID is pushed onto the stack, and when a function
+                call ends, its ID is popped off the stack. The function whose ID
+                is at the top of the stack is the current function being
+                executed. Each time a function starts or ends, we write a log
+                with the ID, whether it started or ended, and the timestamp.
+                <br />
+                You are given a list logs, where logs[i] represents the ith log
+                message formatted as a string{" "}
+                {'"{function_id}:{"start" | "end"}:{timestamp}"'}. For example,
+                "0:start:3" means a function call with function ID 0 started at
+                the beginning of timestamp 3, and "1:end:2" means a function
+                call with function ID 1 ended at the end of timestamp 2. Note
+                that a function can be called multiple times, possibly
+                recursively.
+                <br />
+                A function's exclusive time is the sum of execution times for
+                all function calls in the program. For example, if a function is
+                called twice, one call executing for 2 time units and another
+                call executing for 1 time unit, the exclusive time is 2 + 1 = 3.
+                <br />
+                Return the exclusive time of each function in an array, where
+                the value at the ith index represents the exclusive time for the
+                function with ID i.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: n = 2, logs =
+                    ["0:start:0","1:start:2","1:end:5","0:end:6"]
+                    <br /> Output: [3,4]
+                    <br /> Explanation:
+                    <br /> Function 0 starts at the beginning of time 0, then it
+                    executes 2 for units of time and reaches the end of time 1.
+                    <br /> Function 1 starts at the beginning of time 2,
+                    executes for 4 units of time, and ends at the end of time 5.
+                    <br /> Function 0 resumes execution at the beginning of time
+                    6 and executes for 1 unit of time.
+                    <br /> So function 0 spends 2 + 1 = 3 units of total time
+                    executing, and function 1 spends 4 units of total time
+                    executing.
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: n = 1, logs =
+                    ["0:start:0","0:start:2","0:end:5","0:start:6","0:end:6","0:end:7"]
+                    <br /> Output: [8]
+                    <br /> Explanation:
+                    <br /> Function 0 starts at the beginning of time 0,
+                    executes for 2 units of time, and recursively calls itself.
+                    <br /> Function 0 (recursive call) starts at the beginning
+                    of time 2 and executes for 4 units of time.
+                    <br /> Function 0 (initial call) resumes execution then
+                    immediately calls itself again.
+                    <br /> Function 0 (2nd recursive call) starts at the
+                    beginning of time 6 and executes for 1 unit of time.
+                    <br /> Function 0 (initial call) resumes execution at the
+                    beginning of time 7 and executes for 1 unit of time.
+                    <br /> So function 0 spends 2 + 4 + 1 + 1 = 8 units of total
+                    time executing.
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: n = 2, logs =
+                    ["0:start:0","0:start:2","0:end:5","1:start:6","1:end:6","0:end:7"]
+                    <br /> Output: [7,1]
+                    <br /> Explanation:
+                    <br /> Function 0 starts at the beginning of time 0,
+                    executes for 2 units of time, and recursively calls itself.
+                    <br /> Function 0 (recursive call) starts at the beginning
+                    of time 2 and executes for 4 units of time.
+                    <br /> Function 0 (initial call) resumes execution then
+                    immediately calls function 1.
+                    <br /> Function 1 starts at the beginning of time 6,
+                    executes 1 unit of time, and ends at the end of time 6.
+                    <br /> Function 0 resumes execution at the beginning of time
+                    6 and executes for 2 units of time.
+                    <br /> So function 0 spends 2 + 4 + 1 = 7 units of total
+                    time executing, and function 1 spends 1 unit of total time
+                    executing.
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                1 &lt;= n &lt;= 100 <br />
+                1 &lt;= logs.length &lt;= 500 <br />
+                0 &lt;= function_id &lt; n <br />
+                0 &lt;= timestamp &lt;= 10^9 <br />
+                No two start events will happen at the same timestamp. <br />
+                No two end events will happen at the same timestamp. <br />
+                Each function has an "end" log for each "start" log.
               </>
             }
             tc="n"
             sc="n"
             codes={{
-              Javascript: {
-                code: ``,
-                output: ``,
+              Python: {
+                code: `
+// 2
+class Solution:
+  def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:        
+      def convertToArray(log: str):
+          tmp = log.split(':')
+          return [int(tmp[0]), tmp[1], int(tmp[2])]
+      logs = list(map(convertToArray, logs))        
+      stack = deque()
+      functions = [0] * n
+      for log in logs:
+          if log[1] == "start":
+              stack.append(log)
+          else:
+              startLog = stack.pop()
+              execTime = log[2] - startLog[2] + 1
+              functions[log[0]] += execTime
+              if len(stack):
+                  top = stack[-1]
+                  functions[top[0]] -= execTime
+      return functions`,
+                output: `[3,4]`,
               },
             }}
           />
