f94d823e51a2391b94a3d7c26262e7e1509c92e6
saiashish9 <saiashish3760@gmail.com>
2022-02-01 21:27:59 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -73171,23 +73171,77 @@ class Solution:
         title: "Q641. Design Circular Deque (Q514)",
         content: (
           <Comp
-            content1={<></>}
+            content1={
+              <>
+                Design your implementation of the circular double-ended queue
+                (deque).
+                <br />
+                Implement the MyCircularDeque class:
+                <br />
+                MyCircularDeque(int k) Initializes the deque with a maximum size
+                of k.
+                <br />
+                boolean insertFront() Adds an item at the front of Deque.
+                Returns true if the operation is successful, or false otherwise.
+                <br />
+                boolean insertLast() Adds an item at the rear of Deque. Returns
+                true if the operation is successful, or false otherwise.
+                <br />
+                boolean deleteFront() Deletes an item from the front of Deque.
+                Returns true if the operation is successful, or false otherwise.
+                <br />
+                boolean deleteLast() Deletes an item from the rear of Deque.
+                Returns true if the operation is successful, or false otherwise.
+                <br />
+                int getFront() Returns the front item from the Deque. Returns -1
+                if the deque is empty.
+                <br />
+                int getRear() Returns the last item from Deque. Returns -1 if
+                the deque is empty.
+                <br />
+                boolean isEmpty() Returns true if the deque is empty, or false
+                otherwise.
+                <br />
+                boolean isFull() Returns true if the deque is full, or false
+                otherwise.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input
+                    <br /> ["MyCircularDeque", "insertLast", "insertLast",
+                    "insertFront", "insertFront", "getRear", "isFull",
+                    "deleteLast", "insertFront", "getFront"]
+                    <br /> [[3], [1], [2], [3], [4], [], [], [], [4], []]
+                    <br /> Output
+                    <br /> [null, true, true, true, false, 2, true, true, true,
+                    4]
+                    <br /> Explanation
+                    <br /> MyCircularDeque myCircularDeque = new
+                    MyCircularDeque(3);
+                    <br /> myCircularDeque.insertLast(1); // return True
+                    <br /> myCircularDeque.insertLast(2); // return True
+                    <br /> myCircularDeque.insertFront(3); // return True
+                    <br /> myCircularDeque.insertFront(4); // return False, the
+                    queue is full.
+                    <br /> myCircularDeque.getRear(); // return 2
+                    <br /> myCircularDeque.isFull(); // return True
+                    <br /> myCircularDeque.deleteLast(); // return True
+                    <br /> myCircularDeque.insertFront(4); // return True
+                    <br /> myCircularDeque.getFront(); // return 4
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                1 &lt;= k &lt;= 1000 <br />
+                0 &lt;= value &lt;= 1000 <br />
+                At most 2000 calls will be made to insertFront, insertLast,
+                deleteFront, deleteLast, getFront, getRear, isEmpty, isFull.
               </>
             }
             tc="n"
