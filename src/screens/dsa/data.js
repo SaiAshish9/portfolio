8e6406b251de3ecc37f056e3228cb0038c74ad5d
saiashish9 <saiashish3760@gmail.com>
2022-02-02 20:16:30 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -75387,25 +75387,46 @@ class Solution:
         title: "Q667. Beautiful Arrangement II (Q533)",
         content: (
           <Comp
-            content1={<></>}
+            content1={
+              <>
+                Given two integers n and k, construct a list answer that
+                contains n different positive integers ranging from 1 to n and
+                obeys the following requirement:
+                <br />
+                Suppose this list is answer = [a1, a2, a3, ... , an], then the
+                list [|a1 - a2|, |a2 - a3|, |a3 - a4|, ... , |an-1 - an|] has
+                exactly k distinct integers.
+                <br />
+                Return the list answer. If there multiple valid answers, return
+                any of them.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: n = 3, k = 1 <br />
+                    Output: [1,2,3] <br />
+                    Explanation: The [1,2,3] has three different positive
+                    integers ranging from 1 to 3, and the [1,1] has exactly 1
+                    distinct integer: 1
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: n = 3, k = 1 <br />
+                    Output: [1,2,3] <br />
+                    Explanation: The [1,2,3] has three different positive
+                    integers ranging from 1 to 3, and the [1,1] has exactly 1
+                    distinct integer: 1
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
-              <>
-                <b>Follow up :</b>
-              </>
-            }
+            constraints={<>1 &lt;= k &lt; n &lt;= 10^4</>}
             tc="n"
             sc="n"
             codes={{
