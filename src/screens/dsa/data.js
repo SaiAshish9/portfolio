37429b97f8d3288bed600298819b4c69aa62b34f
saiashish9 <saiashish3760@gmail.com>
2022-02-03 01:05:00 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -78009,23 +78009,51 @@ class Solution:
         content: (
           <Comp
             title="Q697. Degree of an Array (Q560)"
-            content1={<></>}
+            content1={
+              <>
+                Given a non-empty array of non-negative integers nums, the
+                degree of this array is defined as the maximum frequency of any
+                one of its elements.
+                <br />
+                Your task is to find the smallest possible length of a
+                (contiguous) subarray of nums, that has the same degree as nums.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: nums = [1,2,2,3,1] Output: 2<br />
+                    Explanation: <br />
+                    The input array has a degree of 2 because both elements 1
+                    and 2 appear twice. Of the subarrays that have the same
+                    degree:
+                    <br /> [1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2,
+                    2], [2, 2, 3], [2, 2]
+                    <br /> The shortest length is 2. So return 2.
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: nums = [1,2,2,3,1,4,2]
+                    <br />
+                    Output: 6<br />
+                    Explanation: <br />
+                    The degree is 3 because the element 2 is repeated 3 times.
+                    So [2,2,3,1,4,2] is the shortest subarray, therefore
+                    returning 6.
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                nums.length will be between 1 and 50,000.
+                <br />
+                nums[i] will be an integer between 0 and 49,999.
               </>
             }
             tc="n"
