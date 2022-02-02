c34ec353ad41fdb6671437697b0f74f6ec36fe35
saiashish9 <saiashish3760@gmail.com>
2022-02-02 22:40:23 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -76960,6 +76960,7 @@ class Solution:
         title: "Q685. Redundant Connection II (Q549)",
         content: (
           <Comp
+            title="Q685. Redundant Connection II (Q549)"
             content1={<></>}
             content2={null}
             examples={[
@@ -76982,9 +76983,71 @@ class Solution:
             tc="n"
             sc="n"
             codes={{
-              Javascript: {
-                code: ``,
-                output: ``,
+              Java: {
+                code: `
+                // [[1,2],[1,3],[2,3]]
+                class UF {
+                  public UF(int n) {
+                    id = new int[n];
+                    for (int i = 0; i < n; ++i)
+                      id[i] = i;
+                  }
+                
+                  public boolean union(int u, int v) {
+                    final int i = find(u);
+                    final int j = find(v);
+                    if (i == j)
+                      return false;
+                    id[i] = j;
+                    return true;
+                  }
+                
+                  private int[] id;
+                
+                  private int find(int u) {
+                    return id[u] == u ? u : (id[u] = find(id[u]));
+                  }
+                }
+                
+                class Solution {
+                  public int[] findRedundantDirectedConnection(int[][] edges) {
+                    int[] ids = new int[edges.length + 1];
+                    int nodeWithTwoParents = 0;
+                
+                    for (int[] edge : edges) {
+                      final int v = edge[1];
+                      if (++ids[v] == 2) {
+                        nodeWithTwoParents = v;
+                        break;
+                      }
+                    }
+                
+                    if (nodeWithTwoParents == 0)
+                      return findRedundantDirectedConnection(edges, -1);
+                
+                    for (int i = edges.length - 1; i >= 0; --i)
+                      if (edges[i][1] == nodeWithTwoParents)
+                        if (findRedundantDirectedConnection(edges, i).length == 0)
+                          return edges[i];
+                
+                    throw new IllegalArgumentException();
+                  }
+                
+                  private int[] findRedundantDirectedConnection(int[][] edges, int skippedEdgeIndex) {
+                    UF uf = new UF(edges.length + 1);
+                
+                    for (int i = 0; i < edges.length; ++i) {
+                      if (i == skippedEdgeIndex)
+                        continue;
+                      if (!uf.union(edges[i][0], edges[i][1]))
+                        return edges[i];
+                    }
+                
+                    return new int[] {};
+                  }
+                }
+                `,
+                output: `[2,3]`,
               },
             }}
           />
