5b84d5c470b2606531bf9fc3df75b28b19e634a8
saiashish9 <saiashish3760@gmail.com>
2022-02-03 02:22:59 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -78768,26 +78768,69 @@ class Solution:
         title: "Q706. Design HashMap (Q568)",
         content: (
           <Comp
-            content1={<></>}
+            content1={
+              <>
+                Design a HashMap without using any built-in hash table
+                libraries.
+                <br />
+                Implement the MyHashMap class:
+                <br />
+                MyHashMap() initializes the object with an empty map.
+                <br />
+                void put(int key, int value) inserts a (key, value) pair into
+                the HashMap. If the key already exists in the map, update the
+                corresponding value.
+                <br /> int get(int key) returns the value to which the specified
+                key is mapped, or -1 if this map contains no mapping for the
+                key.
+                <br /> void remove(key) removes the key and its corresponding
+                value if the map contains the mapping for the key.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input
+                    <br />
+                    ["MyHashMap", "put", "put", "get", "get", "put", "get",
+                    "remove", "get"] [[], [1, 1], [2, 2], [1], [3], [2, 1], [2],
+                    [2], [2]]
+                    <br />
+                    Output
+                    <br />
+                    [null, null, null, 1, -1, null, 1, null, -1]
+                    <br />
+                    Explanation MyHashMap myHashMap = new MyHashMap();
+                    <br />
+                    myHashMap.put(1, 1); // The map is now [[1,1]]
+                    <br />
+                    myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+                    <br />
+                    myHashMap.get(1); // return 1, The map is now [[1,1], [2,2]]
+                    <br />
+                    <br /> myHashMap.get(3); // return -1 (i.e., not found), The
+                    map is now [[1,1], [2,2]]
+                    <br /> myHashMap.put(2, 1); // The map is now [[1,1], [2,1]]
+                    (i.e., update the existing value)
+                    <br /> myHashMap.get(2); // return 1, The map is now [[1,1],
+                    [2,1]]
+                    <br /> myHashMap.remove(2); // remove the mapping for 2, The
+                    map is now [[1,1]]
+                    <br /> myHashMap.get(2); // return -1 (i.e., not found), The
+                    map is now [[1,1]]
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                0 &lt;= key, value &lt;= 10^6 <br />
+                At most 104 calls will be made to put, get, and remove.
               </>
             }
-            tc="n"
+            tc="1"
             sc="n"
             codes={{
               Javascript: {
