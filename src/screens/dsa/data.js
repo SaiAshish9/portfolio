368472a03a1dec6fdb2ed596c927a9f11ad76252
saiashish9 <saiashish3760@gmail.com>
2022-02-02 22:53:09 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -77160,23 +77160,41 @@ class Solution:
         title: "Q687. Longest Univalue Path (Q551)",
         content: (
           <Comp
-            content1={<></>}
+            title="Q687. Longest Univalue Path (Q551)"
+            content1={
+              <>
+                Given the root of a binary tree, return the length of the
+                longest path, where each node in the path has the same value.
+                This path may or may not pass through the root.
+                <br />
+                The length of the path between two nodes is represented by the
+                number of edges between them.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: root = [5,4,5,1,1,5] <br />
+                    Output: 2
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: root = [1,4,5,4,4,5] <br />
+                    Output: 2
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                The number of nodes in the tree is in the range [0, 104].
+                <br /> -1000 &lt;= Node.val &lt;= 1000
+                <br /> The depth of the tree will not exceed 1000.
               </>
             }
             tc="n"
