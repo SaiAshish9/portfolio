39dab587defa953c5e4bb9ded11c23f5d1bcf2a4
saiashish9 <saiashish3760@gmail.com>
2022-02-02 22:09:00 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -76507,8 +76507,38 @@ class Solution:
             sc="1"
             codes={{
               Javascript: {
-                code: ``,
-                output: ``,
+                code: `/**
+                * @param {string} s
+                * @return {boolean}
+                */
+               var checkValidString = function(s) {
+                 let low = 0;  
+                 let high = 0;
+               
+                 for (let c of s) {
+                     switch (c) {
+                       case '(':
+                         ++low;
+                         ++high;
+                         break;
+                       case ')':
+                         low = Math.max(0, --low);
+                         --high;
+                         break;
+                       case '*':
+                         low = Math.max(0, --low);
+                         ++high;
+                         break;
+                     }
+                     if (high < 0)
+                       return false;
+                   }
+               
+                   return low == 0;
+               };
+               
+               console.log(checkValidString("()"))`,
+                output: `true`,
               },
             }}
           />
@@ -76518,31 +76548,99 @@ class Solution:
         title: "Q679. 24 Game (Q545)",
         content: (
           <Comp
-            content1={<></>}
+            title="Q679. 24 Game (Q545)"
+            content1={
+              <>
+                You are given an integer array cards of length 4. You have four
+                cards, each containing a number in the range [1, 9]. You should
+                arrange the numbers on these cards in a mathematical expression
+                using the operators ['+', '-', '*', '/'] and the parentheses '('
+                and ')' to get the value 24.
+                <br />
+                You are restricted with the following rules:
+                <br />
+                The division operator '/' represents real division, not integer
+                division.
+                <br /> For example, 4 / (1 - 2 / 3) = 4 / (1 / 3) = 12.
+                <br /> Every operation done is between two numbers. In
+                particular, we cannot use '-' as a unary operator.
+                <br /> For example, if cards = [1, 1, 1, 1], the expression "-1
+                - 1 - 1 - 1" is not allowed.
+                <br /> You cannot concatenate numbers together
+                <br /> For example, if cards = [1, 2, 1, 2], the expression "12
+                + 12" is not valid.
+                <br /> Return true if you can get such expression that evaluates
+                to 24, and false otherwise.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: cards = [4,1,8,7]
+                    <br />
+                    Output: true
+                    <br />
+                    Explanation: (8-4) * (7-1) = 24
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: cards = [1,2,1,2]
+                    <br />
+                    Output: false
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            fp={
+            constraints={
               <>
-                <b>Follow up :</b>
+                cards.length == 4<br />1 &lt;= cards[i] &lt;= 9
               </>
             }
-            tc="n"
-            sc="n"
+            tc="2^n"
+            sc="2^n"
             codes={{
-              Javascript: {
-                code: ``,
-                output: ``,
+              Java: {
+                code: `class Solution {
+                  public boolean judgePoint24(int[] nums) {
+                    List<Double> doubleNums = new ArrayList<>();
+                
+                    for (final int num : nums)
+                      doubleNums.add((double) num);
+                
+                    return dfs(doubleNums);
+                  }
+                
+                  private boolean dfs(List<Double> nums) {
+                    if (nums.size() == 1)
+                      return Math.abs(nums.get(0) - 24.0) < 0.001;
+                
+                    for (int i = 0; i < nums.size(); ++i)
+                      for (int j = i + 1; j < nums.size(); ++j)
+                        for (final double num : generate(nums.get(i), nums.get(j))) {
+                          List<Double> nextRound = new ArrayList<>(Arrays.asList(num));
+                          for (int k = 0; k < nums.size(); ++k) {
+                            if (k == i || k == j)
+                              continue;
+                            nextRound.add(nums.get(k));
+                          }
+                          if (dfs(nextRound))
+                            return true;
+                        }
+                
+                    return false;
+                  }
+                
+                  private double[] generate(double a, double b) {
+                    return new double[] {a * b, a / b, b / a, a + b, a - b, b - a};
+                  }
+                }
+                `,
+                output: `true`,
               },
             }}
           />
