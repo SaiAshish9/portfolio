cd929a456baa7b04d68f7bb035a5eac4156b86d9
saiashish9 <saiashish3760@gmail.com>
2022-02-05 14:52:56 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -79914,21 +79914,69 @@ class Solution:
         content: (
           <Comp
             title="Q721. Accounts Merge (Q580)"
-            content1={<></>}
+            content1={
+              <>
+                Given a list of accounts where each element accounts[i] is a
+                list of strings, where the first element accounts[i][0] is a
+                name, and the rest of the elements are emails representing
+                emails of the account.
+                <br />
+                Now, we would like to merge these accounts. Two accounts
+                definitely belong to the same person if there is some common
+                email to both accounts. Note that even if two accounts have the
+                same name, they may belong to different people as people could
+                have the same name. A person can have any number of accounts
+                initially, but all of their accounts definitely have the same
+                name.
+                <br />
+                After merging the accounts, return the accounts in the following
+                format: the first element of each account is the name, and the
+                rest of the elements are emails in sorted order. The accounts
+                themselves can be returned in any order.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: accounts =
+                    [["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
+                    <br /> Output:
+                    [["John","john00@mail.com","john_newyork@mail.com","johnsmith@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
+                    Explanation: <br />
+                    The first and second John's are the same person as they have
+                    the common email "johnsmith@mail.com". The third John and
+                    Mary are different people as none of their email addresses
+                    are used by other accounts. We could return these lists in
+                    any order, for example the answer [['Mary',
+                    'mary@mail.com'], ['John', 'johnnybravo@mail.com'], ['John',
+                    'john00@mail.com', 'john_newyork@mail.com',
+                    'johnsmith@mail.com']] would still be accepted.
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: accounts =
+                    [["Gabe","Gabe0@m.co","Gabe3@m.co","Gabe1@m.co"],["Kevin","Kevin3@m.co","Kevin5@m.co","Kevin0@m.co"],["Ethan","Ethan5@m.co","Ethan4@m.co","Ethan0@m.co"],["Hanzo","Hanzo3@m.co","Hanzo1@m.co","Hanzo0@m.co"],["Fern","Fern5@m.co","Fern1@m.co","Fern0@m.co"]]
+                    <br /> Output:
+                    [["Ethan","Ethan0@m.co","Ethan4@m.co","Ethan5@m.co"],["Gabe","Gabe0@m.co","Gabe1@m.co","Gabe3@m.co"],["Hanzo","Hanzo0@m.co","Hanzo1@m.co","Hanzo3@m.co"],["Kevin","Kevin0@m.co","Kevin3@m.co","Kevin5@m.co"],["Fern","Fern0@m.co","Fern1@m.co","Fern5@m.co"]]
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            tc="n"
+            constraints={
+              <>
+                1 &lt;= accounts.length &lt;= 1000 <br />
+                2 &lt;= accounts[i].length &lt;= 10 <br />
+                1 &lt;= accounts[i][j] &lt;= 30 <br />
+                accounts[i][0] consists of English letters. <br />
+                accounts[i][j] (for j &gt; 0) is a valid email.
+              </>
+            }
+            tc="n^2"
             sc="n"
             codes={{
               Javascript: {
