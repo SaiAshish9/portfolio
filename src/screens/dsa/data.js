0182190e48514d32fa88cb150b940725e2548c21
saiashish9 <saiashish3760@gmail.com>
2022-02-05 15:18:07 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -80210,7 +80210,23 @@ a = b + c;
         content: (
           <Comp
             title="Q724. Find Pivot Index (Q582)"
-            content1={<></>}
+            content1={
+              <>
+                Given an array of integers nums, calculate the pivot index of
+                this array.
+                <br />
+                The pivot index is the index where the sum of all the numbers
+                strictly to the left of the index is equal to the sum of all the
+                numbers strictly to the index's right.
+                <br />
+                If the index is on the left edge of the array, then the left sum
+                is 0 because there are no elements to the left. This also
+                applies to the right edge of the array.
+                <br />
+                Return the leftmost pivot index. If no such index exists, return
+                -1.
+              </>
+            }
             content2={null}
             examples={[
               {
