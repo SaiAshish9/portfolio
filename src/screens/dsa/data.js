59ab1d56d50b2241c81431d8502c3b56f750c01a
saiashish9 <saiashish3760@gmail.com>
2022-02-05 14:22:27 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -79383,26 +79383,66 @@ class Solution:
         content: (
           <Comp
             title="Q714. Best Time to Buy and Sell Stock with Transaction Fee (Q574)"
-            content1={<></>}
+            content1={
+              <>
+                You are given an array prices where prices[i] is the price of a
+                given stock on the ith day, and an integer fee representing a
+                transaction fee.
+                <br />
+                Find the maximum profit you can achieve. You may complete as
+                many transactions as you like, but you need to pay the
+                transaction fee for each transaction.
+                <br />
+                Note: You may not engage in multiple transactions simultaneously
+                (i.e., you must sell the stock before you buy again).
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: <>Input: prices = [1,3,2,8,4,9], fee = 2  <br />
+                Output: 8  <br />
+                Explanation: The maximum profit can be achieved by:  <br />
+                - Buying at prices[0] = 1  <br />
+                - Selling at prices[3] = 8  <br />
+                - Buying at prices[4] = 4  <br />
+                - Selling at prices[5] = 9  <br />
+                The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) = 8.</>,
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: <>Input: prices = [1,3,7,5,10,3], fee = 3  <br />
+                Output: 6</>,
               },
             ]}
-            constraints={<></>}
+            constraints={
+              <>
+                1 &lt;= prices.length &lt;= 5 * 10^4
+                <br />
+                1 &lt;= prices[i] &lt; 5 * 10^4
+                <br />0 &lt;= fee &lt; 5 * 10^4
+              </>
+            }
             tc="n"
-            sc="n"
+            sc="1"
             codes={{
               Javascript: {
-                code: ``,
-                output: ``,
+                code: `/**
+                * @param {number[]} prices
+                * @param {number} fee
+                * @return {number}
+                */
+               var maxProfit = function(prices, fee) {
+                 let sell = 0;
+                 let hold = Number.MIN_SAFE_INTEGER;
+                 for (let price of prices) {
+                   sell = Math.max(sell, hold + price);
+                   hold = Math.max(hold, sell - price - fee);
+                 }
+                 return sell;
+               };
+               
+               console.log(maxProfit([1,3,7,5,10,3],3))`,
+                output: `6`,
               },
             }}
           />
