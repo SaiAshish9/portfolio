8509bd56700cd0c585b7548200e93e8f9d8c06bd
saiashish9 <saiashish3760@gmail.com>
2022-02-08 13:03:31 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -81550,7 +81550,39 @@ rm -rf pods podfile podfile.txt
             <Span>Code:</Span>
             <pre>
               {`
-              
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    var player: AVAudioPlayer?
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+//        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else {return}
+//
+//        do{
+//            try AVAudioSession.sharedInstance().setCategory(.playback,mode: .default)
+//            try AVAudioSession.sharedInstance().setActive(true)
+//            player = try AVAudioPlayer(contentsOf:url,fileTypeHint: AVFileType.mp3.rawValue)
+//            guard let player = player else { return }
+//            player.play()
+//        }catch let error{
+//            print(error.localizedDescription)
+//        }
+        playSound(soundName:sender.currentTitle!);
+    }
+    
+    func playSound(soundName:String){
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
+    
+}
               `}
             </pre>
           </>
