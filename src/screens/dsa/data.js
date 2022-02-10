7b259a6c3d8efd8f001525afc26e6d512dbf7aeb
saiashish9 <saiashish3760@gmail.com>
2022-02-10 18:21:15 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -84884,6 +84884,7 @@ index.gohtml
             <Span>
               <b>TCP Servers</b>
             </Span>
+            <Span>Read from connection</Span>
             <pre>
               {`
 package main
@@ -84912,9 +84913,209 @@ func main() {
 	}
 }
 go run main.go
-telnet http 8080              
+telnet http 8080
+telnet is a program which works on tcp               
               `}
             </pre>
+            <Span>Write to connection with deadline</Span>
+            <pre>{`
+package main
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handle(conn)
+	}
+}
+func handle(conn net.Conn) {
+  err := conn.SetDeadline(time.Now().Add(10 * time.Second))
+	if err != nil {
+		log.Fatalln("CONN TIMEOUT")
+	}
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+    fmt.Fprintf(conn, "I heard you say: %s\\n", ln)
+	}
+	defer conn.Close()
+	fmt.Println("Code got here.")
+}            
+            `}</pre>
+            <Span>Dial</Span>
+            <pre>{`
+package main
+import (
+	"fmt"
+	"log"
+	"net"
+)
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer conn.Close()
+	fmt.Fprintln(conn, "I dialed you.")
+}
+            `}</pre>
+            <Span>TCP Server For HTTP</Span>
+            <pre>{`
+package main
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer li.Close()
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err.Error())
+			continue
+		}
+		go handle(conn)
+	}
+}
+func handle(conn net.Conn) {
+	defer conn.Close()
+	request(conn)
+}
+func request(conn net.Conn) {
+	i := 0
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if i == 0 {
+			mux(conn, ln)
+		}
+		if ln == "" {
+			// headers are done
+			break
+		}
+		i++
+	}
+}
+func mux(conn net.Conn, ln string) {
+	m := strings.Fields(ln)[0] 
+	u := strings.Fields(ln)[1] 
+	fmt.Println("***METHOD", m)
+  fmt.Println("***URI", u)
+	// multiplexer
+	if m == "GET" && u == "/" {
+		index(conn)
+	}
+	if m == "GET" && u == "/about" {
+		about(conn)
+	}
+	if m == "GET" && u == "/contact" {
+		contact(conn)
+	}
+	if m == "GET" && u == "/apply" {
+		apply(conn)
+	}
+	if m == "POST" && u == "/apply" {
+		applyProcess(conn)
+	}
+}
+func index(conn net.Conn) {
+	body := \`<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body>
+	<strong>INDEX</strong><br>
+	<a href="/">index</a><br>
+	<a href="/about">about</a><br>
+	<a href="/contact">contact</a><br>
+	<a href="/apply">apply</a><br>
+	</body></html>\`
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\\r\\n")
+	fmt.Fprintf(conn, "Content-Length: %d\\r\\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\\r\\n")
+	fmt.Fprint(conn, "\\r\\n")
+	fmt.Fprint(conn, body)
+}
+func about(conn net.Conn) {
+	body := \`<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body>
+	<strong>ABOUT</strong><br>
+	<a href="/">index</a><br>
+	<a href="/about">about</a><br>
+	<a href="/contact">contact</a><br>
+	<a href="/apply">apply</a><br>
+	</body></html>\`
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\\r\\n")
+	fmt.Fprintf(conn, "Content-Length: %d\\r\\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\\r\\n")
+	fmt.Fprint(conn, "\\r\\n")
+	fmt.Fprint(conn, body)
+}
+func contact(conn net.Conn) {
+	body := \`<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body>
+	<strong>CONTACT</strong><br>
+	<a href="/">index</a><br>
+	<a href="/about">about</a><br>
+	<a href="/contact">contact</a><br>
+	<a href="/apply">apply</a><br>
+	</body></html>\`
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\\r\\n")
+	fmt.Fprintf(conn, "Content-Length: %d\\r\\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\\r\\n")
+	fmt.Fprint(conn, "\\r\\n")
+	fmt.Fprint(conn, body)
+}
+func apply(conn net.Conn) {
+	body := \`<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body>
+	<strong>APPLY</strong><br>
+	<a href="/">index</a><br>
+	<a href="/about">about</a><br>
+	<a href="/contact">contact</a><br>
+	<a href="/apply">apply</a><br>
+	<form method="POST" action="/apply">
+	<input type="submit" value="apply">
+	</form>
+	</body></html>\`
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\\r\\n")
+	fmt.Fprintf(conn, "Content-Length: %d\\r\\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\\r\\n")
+	fmt.Fprint(conn, "\\r\\n")
+	fmt.Fprint(conn, body)
+}
+func applyProcess(conn net.Conn) {
+	body := \`<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body>
+	<strong>APPLY PROCESS</strong><br>
+	<a href="/">index</a><br>
+	<a href="/about">about</a><br>
+	<a href="/contact">contact</a><br>
+	<a href="/apply">apply</a><br>
+	</body></html>\`
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\\r\\n")
+	fmt.Fprintf(conn, "Content-Length: %d\\r\\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\\r\\n")
+	fmt.Fprint(conn, "\\r\\n")
+	fmt.Fprint(conn, body)
+}
+            `}</pre>
+            <Span>The Dial function connects to a server:</Span>
+            <Span>The Listen function creates servers:</Span>
             <Span>
               <b>WebRTC</b>
             </Span>
