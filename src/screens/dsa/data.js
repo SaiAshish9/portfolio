5cf920faa51b9d9b70f187c95abdd9b263ad4f3b
saiashish9 <saiashish3760@gmail.com>
2022-02-10 18:36:49 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -85174,7 +85174,24 @@ func main() {
             `}</pre>
             <Span>Net Http ServerMux</Span>
             <pre>{`
-            
+package main
+import (
+  "io"
+  "net/http"
+)
+type hotdog int
+func (m hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+  switch req.URL.Path {
+  case "/dog":
+    io.WriteString(w, "doggy doggy doggy")
+  case "/cat":
+    io.WriteString(w, "kitty kitty kitty")
+  }
+}
+func main() {
+  var d hotdog
+  http.ListenAndServe(":8080", d)
+}            
             `}</pre>
             <Span>
               <b>WebRTC</b>
