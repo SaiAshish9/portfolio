4f0bf17711a1fccc19b510d2e2a57ec390d56f98
saiashish9 <saiashish3760@gmail.com>
2022-02-10 16:31:08 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -84846,6 +84846,36 @@ file 2:
 Here is my polar bear template
 {{end}}
               `}</pre>
+            <Span>
+              <b>Html Templates</b>
+            </Span>
+            <pre>{`
+package main
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+// html templates escapes un-safe characters avoid cross site scripting
+// \`<script>alert('hi')</script>\`
+//  in text templates, fn will be exceuted
+//  in html templates , additional characters will be added in b/w
+var tpl *template.Template
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public"))))
+	http.ListenAndServe(":8080", nil)
+}
+func index(res http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(res, "index.gohtml", nil)
+	if err != nil {
+		log.Fatalln("template didn't execute: ", err)
+	}
+}                       
+            `}</pre>
             <Span>Refer to https://github.dev/SaiAshish9/go-templates</Span>
             <Span>
               <b>WebRTC</b>
