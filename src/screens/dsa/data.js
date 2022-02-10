c1c16d03e2b26fa2440ccf832f53ef37d6796dfd
saiashish9 <saiashish3760@gmail.com>
2022-02-10 14:40:50 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -84504,8 +84504,57 @@ ops: 50000
               shared resource, such as [Shared data objects, shared resources,
               shared memory].
             </Span>
+            <Span>
+              Container holds a map of counters; since we want to update it
+              concurrently from multiple goroutines, we add a Mutex to
+              synchronize access. Note that mutexes must not be copied, so if
+              this struct is passed around, it should be done by pointer.Lock
+              the mutex before accessing counters; unlock it at the end of the
+              function using a defer statement.
+            </Span>
+            <pre>
+              {`
+package main
+import (
+	"fmt"
+	"sync"
+)
+type Container struct {
+	mu       sync.Mutex
+	counters map[string]int
+}
+func (c *Container) inc(name string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.counters[name]++
+}
+func main() {
+	c := Container{
+		counters: map[string]int{"a": 0, "b": 0},
+	}
+
+	var wg sync.WaitGroup
+	doIncrement := func(name string, n int) {
+		for i := 0; i < n; i++ {
+			c.inc(name)
+		}
+		wg.Done()
+	}
+	wg.Add(3)
+	go doIncrement("a", 10000)
+	go doIncrement("a", 10000)
+	go doIncrement("b", 10000)
+	wg.Wait()
+	fmt.Println(c.counters)
+  // O/P:
+  // map[a:20000 b:10000]
+}`}
+            </pre>
             <Span>Closure</Span>
-            <Span></Span>
+            <Span>
+              For more complex state we can use a mutex to safely access data
+              across multiple goroutines.
+            </Span>
             <Span>Panic</Span>
             <Span>
               Occurs when program cannot continue at all cannot obtain TCP port
