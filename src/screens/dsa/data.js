2ba38376b9f25428139013e99a6537e68817f4e7
saiashish9 <saiashish3760@gmail.com>
2022-02-10 20:03:54 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -85310,6 +85310,55 @@ func HandleError(w http.ResponseWriter, err error) {
 }            
             `}</pre>
             <Span>
+              <b>Serving Files:</b>
+            </Span>
+            <pre>{`
+package main
+import (
+	"io"
+	"net/http"
+	"os"
+)
+func main() {
+	http.HandleFunc("/", dog)
+	http.HandleFunc("/toby.jpg", dogPic)
+	http.ListenAndServe(":8080", nil)
+}
+func dog(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, \`
+	<img src="/toby.jpg">
+	\`)
+}
+func dogPic(w http.ResponseWriter, req *http.Request) {
+	f, err := os.Open("toby.jpg")
+	if err != nil {
+		http.Error(w, "file not found", 404)
+		return
+	}
+	defer f.Close()
+	// io.Copy(w, f)
+  // fi, err := f.Stat()
+	// if err != nil {
+	// 	http.Error(w, "file not found", 404)
+	// 	return
+	// }
+	// http.ServeContent(w, req, f.Name(), fi.ModTime(), f)
+}
+func main() {
+	http.HandleFunc("/", dog)
+	http.HandleFunc("/toby.jpg", dogPic)
+	http.ListenAndServe(":8080", nil)
+}
+func dog(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, \`<img src="toby.jpg">\`)
+}
+func dogPic(w http.ResponseWriter, req *http.Request) {
+	http.ServeFile(w, req, "toby.jpg")
+}
+            `}</pre>
+            <Span>
               <b>WebRTC</b>
             </Span>
             <Span>https://webrtc.org</Span>
