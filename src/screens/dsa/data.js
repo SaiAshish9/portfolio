045eb67fefa98f4722705afa56ee5450fe53a4b7
saiashish9 <saiashish3760@gmail.com>
2022-02-10 22:23:18 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -85664,14 +85664,273 @@ func set(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(w, "in chrome go to: dev tools / application / cookies")
 }
 func read(w http.ResponseWriter, req *http.Request) {
-	c, err := req.Cookie("my-cookie")
+	c1, err := req.Cookie("my-cookie")
 	if err != nil {
-		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		log.Println(err)
+	} else {
+		fmt.Fprintln(w, "YOUR COOKIE #1:", c1)
+	}
+	c2, err := req.Cookie("general")
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Fprintln(w, "YOUR COOKIE #2:", c2)
+	}
+	c3, err := req.Cookie("specific")
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Fprintln(w, "YOUR COOKIE #3:", c3)
+	}
+}
+func expire(w http.ResponseWriter, req *http.Request) {
+	c, err := req.Cookie("session")
+	if err != nil {
+		http.Redirect(w, req, "/set", http.StatusSeeOther)
 		return
 	}
-	fmt.Fprintln(w, "YOUR COOKIE:", c)
+	c.MaxAge = -1 // delete cookie
+	http.SetCookie(w, c)
+	http.Redirect(w, req, "/", http.StatusSeeOther)
 }
             `}</pre>
+            <Span>
+              <b>Sessions, UUID, Middlewares And Bycrypt</b>
+            </Span>
+            <pre>
+              {`
+main.go
+
+package main
+
+import (
+	"github.com/satori/go.uuid"
+	"golang.org/x/crypto/bcrypt"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+type user struct {
+	UserName string
+	Password []byte
+	First    string
+	Last     string
+	Role     string
+}
+
+type session struct {
+	un           string
+	lastActivity time.Time
+}
+
+var tpl *template.Template
+var dbUsers = map[string]user{}      
+var dbSessions = map[string]session{} 
+var dbSessionsCleaned time.Time
+
+const sessionLength int = 30
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+	dbSessionsCleaned = time.Now()
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/bar", bar)
+	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", authorized(logout))
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	u := getUser(w, req)
+	showSessions()
+	tpl.ExecuteTemplate(w, "index.gohtml", u)
+}
+
+func bar(w http.ResponseWriter, req *http.Request) {
+	u := getUser(w, req)
+	if !alreadyLoggedIn(w, req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+	if u.Role != "007" {
+		http.Error(w, "You must be 007 to enter the bar", http.StatusForbidden)
+		return
+	}
+	showSessions() // for demonstration purposes
+	tpl.ExecuteTemplate(w, "bar.gohtml", u)
+}
+
+func signup(w http.ResponseWriter, req *http.Request) {
+	if alreadyLoggedIn(w, req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+	var u user
+	if req.Method == http.MethodPost {
+		un := req.FormValue("username")
+		p := req.FormValue("password")
+		f := req.FormValue("firstname")
+		l := req.FormValue("lastname")
+		r := req.FormValue("role")
+		if _, ok := dbUsers[un]; ok {
+			http.Error(w, "Username already taken", http.StatusForbidden)
+			return
+		}
+		sID, _ := uuid.NewV4()
+		c := &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		c.MaxAge = sessionLength
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = session{un, time.Now()}
+		bs, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.MinCost)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		u = user{un, bs, f, l, r}
+		dbUsers[un] = u
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+	showSessions() // for demonstration purposes
+	tpl.ExecuteTemplate(w, "signup.gohtml", u)
+}
+
+func login(w http.ResponseWriter, req *http.Request) {
+	if alreadyLoggedIn(w, req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+	var u user
+	if req.Method == http.MethodPost {
+		un := req.FormValue("username")
+		p := req.FormValue("password")
+		u, ok := dbUsers[un]
+		if !ok {
+			http.Error(w, "Username and/or password do not match", http.StatusForbidden)
+			return
+		}
+		err := bcrypt.CompareHashAndPassword(u.Password, []byte(p))
+		if err != nil {
+			http.Error(w, "Username and/or password do not match", http.StatusForbidden)
+			return
+		}
+		sID, _ := uuid.NewV4()
+		c := &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		c.MaxAge = sessionLength
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = session{un, time.Now()}
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+	showSessions() 
+	tpl.ExecuteTemplate(w, "login.gohtml", u)
+}
+
+func logout(w http.ResponseWriter, req *http.Request) {
+	c, _ := req.Cookie("session")
+	delete(dbSessions, c.Value)
+	// remove the cookie
+	c = &http.Cookie{
+		Name:   "session",
+		Value:  "",
+		MaxAge: -1,
+	}
+	http.SetCookie(w, c)
+	if time.Now().Sub(dbSessionsCleaned) > (time.Second * 30) {
+		go cleanSessions()
+	}
+	http.Redirect(w, req, "/login", http.StatusSeeOther)
+}
+
+func authorized(h http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !alreadyLoggedIn(w, r) {
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+session.go
+
+package main
+
+import (
+	"fmt"
+	"github.com/satori/go.uuid"
+	"net/http"
+	"time"
+)
+
+func getUser(w http.ResponseWriter, req *http.Request) user {
+	c, err := req.Cookie("session")
+	if err != nil {
+		sID, _ := uuid.NewV4()
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+
+	}
+	c.MaxAge = sessionLength
+	http.SetCookie(w, c)
+	var u user
+	if s, ok := dbSessions[c.Value]; ok {
+		s.lastActivity = time.Now()
+		dbSessions[c.Value] = s
+		u = dbUsers[s.un]
+	}
+	return u
+}
+func alreadyLoggedIn(w http.ResponseWriter, req *http.Request) bool {
+	c, err := req.Cookie("session")
+	if err != nil {
+		return false
+	}
+	s, ok := dbSessions[c.Value]
+	if ok {
+		s.lastActivity = time.Now()
+		dbSessions[c.Value] = s
+	}
+	_, ok = dbUsers[s.un]
+	c.MaxAge = sessionLength
+	http.SetCookie(w, c)
+	return ok
+}
+func cleanSessions() {
+	fmt.Println("BEFORE CLEAN") 
+	showSessions()              
+	for k, v := range dbSessions {
+		if time.Now().Sub(v.lastActivity) > (time.Second * 30) {
+			delete(dbSessions, k)
+		}
+	}
+	dbSessionsCleaned = time.Now()
+	fmt.Println("AFTER CLEAN") 
+	showSessions()            
+}
+func showSessions() {
+	fmt.Println("********")
+	for k, v := range dbSessions {
+		fmt.Println(k, v.un)
+	}
+	fmt.Println("")
+}
+              `}
+            </pre>
             <Span>
               <b>WebRTC</b>
             </Span>
