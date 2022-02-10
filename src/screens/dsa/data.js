c3c89a11bdadd552cbdb782858f6c5cb6f7cc79f
saiashish9 <saiashish3760@gmail.com>
2022-02-10 22:48:52 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -86000,7 +86000,37 @@ func main() {
 server: hello handler started
 server: context canceled
 server: hello handler ended              
-              `}
+
+package main
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+func main() {
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/bar", bar)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+func foo(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	ctx = context.WithValue(ctx, "userID", 777)
+	ctx = context.WithValue(ctx, "fname", "Bond")
+	results := dbAccess(ctx)
+	fmt.Fprintln(w, results)
+}
+func dbAccess(ctx context.Context) int {
+	uid := ctx.Value("userID").(int)
+	return uid
+}
+func bar(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	log.Println(ctx)
+	fmt.Fprintln(w, ctx)
+}
+`}
             </pre>
             <Span>
               <b>WebRTC</b>
