9bf5a6af7f613cd10bbf4757ab61f0bd2784deef
saiashish9 <saiashish3760@gmail.com>
2022-02-10 17:16:53 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -84882,6 +84882,40 @@ index.gohtml
             `}</pre>
             <Span>Refer to https://github.dev/SaiAshish9/go-templates</Span>
             <Span>
+              <b>TCP Servers</b>
+            </Span>
+            <pre>
+              {`
+package main
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		io.WriteString(conn, "\\nHello from TCP server\\n")
+		fmt.Fprintln(conn, "How is your day?")
+		fmt.Fprintf(conn, "%v", "Well, I hope!")
+		conn.Close()
+	}
+}
+go run main.go
+telnet http 8080              
+              `}
+            </pre>
+            <Span>
               <b>WebRTC</b>
             </Span>
             <Span>https://webrtc.org</Span>
