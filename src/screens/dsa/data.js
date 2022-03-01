bc68b15cd4cdb3ff720254e27ea4c8650c5c8131
saiashish9 <saiashish3760@gmail.com>
2022-03-01 13:36:29 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -80962,7 +80962,24 @@ a = b + c;
         content: (
           <Comp
             title="Q732. My Calendar III (Q589)"
-            content1={<></>}
+            content1={
+              <>
+                A k-booking happens when k events have some non-empty
+                intersection (i.e., there is some time that is common to all k
+                events.)
+                <br />
+                You are given some events [start, end), after each given event,
+                return an integer k representing the maximum k-booking between
+                all the previous events.
+                <br />
+                Implement the MyCalendarThree class:
+                <br />
+                MyCalendarThree() Initializes the object. <br />
+                int book(int start, int end) Returns an integer k representing
+                the largest integer such that there exists a k-booking in the
+                calendar.
+              </>
+            }
             content2={null}
             examples={[
               {
