aab686e48ef4913a9fff143c293a279316e89b4f
saiashish9 <saiashish3760@gmail.com>
2022-03-01 17:43:07 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -81258,20 +81258,101 @@ a = b + c;
         content: (
           <Comp
             title="Q736. Parse Lisp Expression (Q592)"
-            content1={<></>}
+            content1={
+              <>
+                You are given a string expression representing a Lisp-like
+                expression to return the integer value of.
+                <br />
+                The syntax for these expressions is given as follows.
+                <br />
+                An expression is either an integer, let expression, add
+                expression, mult expression, or an assigned variable.
+                Expressions always evaluate to a single integer.
+                <br />
+                (An integer could be positive or negative.)
+                <br />
+                A let expression takes the form "(let v1 e1 v2 e2 ... vn en
+                expr)", where let is always the string "let", then there are one
+                or more pairs of alternating variables and expressions, meaning
+                that the first variable v1 is assigned the value of the
+                expression e1, the second variable v2 is assigned the value of
+                the expression e2, and so on sequentially; and then the value of
+                this let expression is the value of the expression expr.
+                <br />
+                An add expression takes the form "(add e1 e2)" where add is
+                always the string "add", there are always two expressions e1, e2
+                and the result is the addition of the evaluation of e1 and the
+                evaluation of e2.
+                <br />
+                A mult expression takes the form "(mult e1 e2)" where mult is
+                always the string "mult", there are always two expressions e1,
+                e2 and the result is the multiplication of the evaluation of e1
+                and the evaluation of e2.
+                <br />
+                For this question, we will use a smaller subset of variable
+                names. A variable starts with a lowercase letter, then zero or
+                more lowercase letters or digits. Additionally, for your
+                convenience, the names "add", "let", and "mult" are protected
+                and will never be used as variable names.
+                <br />
+                Finally, there is the concept of scope. When an expression of a
+                variable name is evaluated, within the context of that
+                evaluation, the innermost scope (in terms of parentheses) is
+                checked first for the value of that variable, and then outer
+                scopes are checked sequentially. It is guaranteed that every
+                expression is legal. Please see the examples for more details on
+                the scope.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: expression = "(let x 2 (mult x (let x 3 y 4 (add x
+                    y))))"
+                    <br /> Output: 14 <br />
+                    Explanation: In the expression (add x y), when checking for
+                    the value of the variable x, we check from the innermost
+                    scope to the outermost in the context of the variable we are
+                    trying to evaluate. Since x = 3 is found first, the value of
+                    x is 3.
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: expression = "(let x 3 x 2 x)"
+                    <br /> Output: 2 <br />
+                    Explanation: Assignment in let statements is processed
+                    sequentially.
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: expression = "(let x 1 y 2 x (add x y) (add x y))"
+                    <br /> Output: 5 <br />
+                    Explanation: The first (add x y) evaluates as 3, and is
+                    assigned to x. The second (add x y) evaluates as 3+2 = 5.
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
+            constraints={
+              <>
+                1 &lt;= expression.length &lt;= 2000 <br />
+                There are no leading or trailing spaces in expression. <br />
+                All tokens are separated by a single space in expression. <br />
+                The answer and all intermediate calculations of that answer are
+                guaranteed to fit in a 32-bit integer.
+                <br />
+                The expression is guaranteed to be legal and evaluate to an
+                integer.
+              </>
+            }
             tc="n"
             sc="n"
             codes={{
