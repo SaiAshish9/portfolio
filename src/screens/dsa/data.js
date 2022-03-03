3c63484ad6809aecf1401ac463b868f85064c37a
saiashish9 <saiashish3760@gmail.com>
2022-03-04 00:07:45 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -82142,8 +82142,19 @@ a = b + c;
             sc="1"
             codes={{
               Javascript: {
-                code: ``,
-                output: ``,
+                code: `/**
+                * @param {number[]} cost
+                * @return {number}
+                */
+               var minCostClimbingStairs = function(cost) {
+                   const n = cost.length;
+                   for (let i = 2; i < n; ++i)
+                     cost[i] += Math.min(cost[i - 1], cost[i - 2]);
+                   return Math.min(cost[n - 1], cost[n - 2]); 
+               };
+               
+               console.log(minCostClimbingStairs([1,100,1,1,1,100,1,1,100,1]))`,
+                output: `6`,
               },
             }}
           />
