ff005805b3512c53546d5db3f7612bbb4e77f084
saiashish9 <saiashish3760@gmail.com>
2022-03-04 14:33:50 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -82769,20 +82769,59 @@ a = b + c;
         content: (
           <Comp
             title="Q754. Reach a Number (Q606)"
-            content1={<></>}
+            content1={
+              <>
+                You are standing at position 0 on an infinite number line. There
+                is a destination at position target.
+                <br />
+                You can make some number of moves numMoves so that:
+                <br />
+                On each move, you can either go left or right. <br />
+                During the ith move (starting from i == 1 to i == numMoves), you
+                take i steps in the chosen direction.
+                <br />
+                Given the integer target, return the minimum number of moves
+                required (i.e., the minimum numMoves) to reach the destination.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: target = 2<br />
+                    Output: 3<br />
+                    Explanation:
+                    <br />
+                    On the 1st move, we step from 0 to 1 (1 step).
+                    <br />
+                    On the 2nd move, we step from 1 to -1 (2 steps).
+                    <br />
+                    On the 3rd move, we step from -1 to 2 (3 steps).
+                  </>
+                ),
               },
               {
-                content: <></>,
-              },
-              {
-                content: <></>,
+                content: (
+                  <>
+                    Input: target = 3<br />
+                    Output: 2<br />
+                    Explanation:
+                    <br />
+                    On the 1st move, we step from 0 to 1 (1 step).
+                    <br />
+                    On the 2nd move, we step from 1 to 3 (2 steps).
+                    <br />
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
+            constraints={
+              <>
+                -10^9 &lt;= target &lt;= 10^9 <br />
+                target != 0
+              </>
+            }
             tc="n^0.5"
             sc="1"
             codes={{
