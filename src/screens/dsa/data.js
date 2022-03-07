e7656818816e70afaf33ec4017d2ae1503e18e04
saiashish9 <saiashish3760@gmail.com>
2022-03-07 15:34:15 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -83665,9 +83665,43 @@ a = b + c;
             tc="n"
             sc="1"
             codes={{
-              Javascript: {
-                code: ``,
-                output: ``,
+              Java: {
+                code: `
+                // Input: s = "aab"
+                class Solution {
+                  public String reorganizeString(String S) {
+                    final int n = S.length();
+                    int[] count = new int[128];
+                    int maxCount = 0;
+                    char maxChar = '@';
+                    for (final char c : S.toCharArray())
+                      ++count[c];
+                    for (char c = 'a'; c <= 'z'; ++c)
+                      if (count[c] > maxCount) {
+                        maxCount = count[c];
+                        maxChar = c;
+                      }
+                
+                    if (maxCount > (n + 1) / 2)
+                      return "";
+                    char[] ans = new char[n];
+                    while (count[maxChar]-- > 0)
+                      fillIn(ans, maxChar);
+                    for (char c = 'a'; c <= 'z'; ++c)
+                      while (count[c]-- > 0)
+                        fillIn(ans, c);
+                
+                    return new String(ans);
+                  }
+                  private int i = 0;
+                  private void fillIn(char[] ans, char c) {
+                    ans[i] = c;
+                    i += 2;
+                    if (i >= ans.length) 
+                      i = 1;
+                  }
+                }`,
+                output: `aba`,
               },
             }}
           />
