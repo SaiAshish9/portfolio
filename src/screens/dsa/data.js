d3f8939840f1a55b88a29410aa49313b78ed3c70
saiashish9 <saiashish3760@gmail.com>
2022-03-07 15:58:35 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -83762,8 +83762,27 @@ a = b + c;
             sc="n"
             codes={{
               Javascript: {
-                code: ``,
-                output: ``,
+                code: `/**
+                * @param {number[]} arr
+                * @return {number}
+                */
+               var maxChunksToSorted = function(arr) {
+                const n = arr.length;
+                let res = 0;
+                const maxL = Array(n).fill(0); 
+                const minR = Array(n).fill(0); 
+                for (let i = 0; i < n; ++i)
+                     maxL[i] = i == 0 ? arr[i] : Math.max(arr[i], maxL[i - 1]);
+                for (let i = n - 1; i >= 0; --i)
+                     minR[i] = i == n - 1 ? arr[i] : Math.min(arr[i], minR[i + 1]);
+                for (let i = 0; i + 1 < n; ++i)
+                 if (maxL[i] <= minR[i + 1])
+                   ++res;
+                return res + 1;    
+               };
+               
+               console.log(maxChunksToSorted([5,4,3,2,1]))`,
+                output: `1`,
               },
             }}
           />
