94c2cbc72ae1fe5aa1b68fe5d2c429b33f8b8cb2
saiashish9 <saiashish3760@gmail.com>
2022-03-07 12:30:43 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -83103,7 +83103,28 @@ a = b + c;
         title: "Q762.  (Q610)",
         content: (
           <Comp
-            content1={<></>}
+            content1={
+              <>
+                {" "}
+                Special binary strings are binary strings with the following two
+                properties:
+                <br />
+                The number of 0's is equal to the number of 1's.
+                <br />
+                Every prefix of the binary string has at least as many 1's as
+                0's.
+                <br />
+                You are given a special binary string s.
+                <br />
+                A move consists of choosing two consecutive, non-empty, special
+                substrings of s, and swapping them. Two strings are consecutive
+                if the last character of the first string is exactly one index
+                before the first character of the second string.
+                <br />
+                Return the lexicographically largest resulting string possible
+                after applying the mentioned operations on the string.
+              </>
+            }
             content2={null}
             examples={[
               {
