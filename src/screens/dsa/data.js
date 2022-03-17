e3d465095e7e60b6437a373c845881425322a67f
saiashish9 <saiashish3760@gmail.com>
2022-03-17 15:20:41 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -93405,9 +93405,28 @@ a = b + c;
             tc="n.log n"
             sc="n"
             codes={{
-              Javascript: {
-                code: ``,
-                output: ``,
+              Java: {
+                code: `
+                // Input: nums1 = [12,24,8,32], nums2 = [13,25,32,11]
+                class Solution {
+                  public int[] advantageCount(int[] A, int[] B) {
+                    TreeMap<Integer, Integer> map = new TreeMap<>();              
+                    for (int a : A)
+                      map.put(a, map.getOrDefault(a, 0) + 1);
+                    for (int i = 0; i < B.length; i++) {
+                      Integer key = map.higherKey(B[i]);
+                      if (key == null)
+                        key = map.firstKey();
+                      map.put(key, map.get(key) - 1);
+                      if (map.get(key) == 0)
+                        map.remove(key);
+                      A[i] = key;
+                    }
+                    return A;
+                  }
+                }
+                `,
+                output: `[24,32,8,12]`,
               },
             }}
           />
