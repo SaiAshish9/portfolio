b77b05643e6cf9ee1bc9560f58f6867b652be216
saiashish9 <saiashish3760@gmail.com>
2022-03-17 16:24:09 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -93729,7 +93729,27 @@ a = b + c;
         content: (
           <Comp
             title="Q874. Walking Robot Simulation (Q718)"
-            content1={<></>}
+            content1={
+              <>
+                A robot on an infinite XY-plane starts at point (0, 0) facing
+                north. The robot can receive a sequence of these three possible
+                types of commands:
+                <br />
+                -2: Turn left 90 degrees.
+                <br />
+                -1: Turn right 90 degrees.
+                <br />
+                1 &lt;= k &lt;= 9: Move forward k units, one unit at a time.
+                <br />
+                Some of the grid squares are obstacles. The ith obstacle is at
+                grid point obstacles[i] = (xi, yi). If the robot runs into an
+                obstacle, then it will instead stay in its current location and
+                move on to the next command.
+                <br />
+                Return the maximum Euclidean distance that the robot ever gets
+                from the origin squared (i.e. if the distance is 5, return 25).
+              </>
+            }
             content2={null}
             examples={[
               {
