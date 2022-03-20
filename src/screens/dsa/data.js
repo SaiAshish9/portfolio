0cc68c2eef7b379f8fc7d065675cc3b1c4a25cf8
saiashish9 <saiashish3760@gmail.com>
2022-03-20 17:02:49 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -102594,26 +102594,81 @@ a = b + c;
         ),
       },
       q811: {
-        title: "Q (Q811)",
+        title: "Q967. Numbers With Same Consecutive Differences (Q811)",
         content: (
           <Comp
-            content1={<></>}
+            title="Q967. Numbers With Same Consecutive Differences (Q811)"
+            content1={
+              <>
+                Return all non-negative integers of length n such that the
+                absolute difference between every two consecutive digits is k.
+                <br />
+                Note that every number in the answer must not have leading
+                zeros. For example, 01 has one leading zero and is invalid.
+                <br />
+                You may return the answer in any order.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: n = 3, k = 7 <br />
+                    Output: [181,292,707,818,929] <br />
+                    Explanation: Note that 070 is not a valid number, because it
+                    has leading zeroes.
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: n = 2, k = 1 <br />
+                    Output: [10,12,21,23,32,34,43,45,54,56,65,67,76,78,87,89,98]
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            tc="n"
-            sc="n"
+            constraints={
+              <>
+                2 &lt;= n &lt;= 9<br />0 &lt;= k &lt;= 9
+              </>
+            }
+            tc="n.2^n"
+            sc="2^n"
             codes={{
               Java: {
-                code: ``,
-                output: ``,
+                code: `// Input: n = 3, k = 7
+                class Solution {
+                  public int[] numsSameConsecDiff(int n, int k) {
+                    if (n == 1)
+                      return new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+                    List<Integer> ans = new ArrayList<>();
+                    if (k == 0) {
+                      for (char c = '1'; c <= '9'; ++c) {
+                        final String s = String.valueOf(c).repeat(n);
+                        ans.add(Integer.parseInt(s));
+                      }
+                      return ans.stream().mapToInt(i -> i).toArray();
+                    }
+                    for (int num = 1; num <= 9; ++num)
+                      dfs(n - 1, k, num, ans);
+                
+                    return ans.stream().mapToInt(i -> i).toArray();
+                  }
+                  private void dfs(int n, int k, int num, List<Integer> ans) {
+                    if (n == 0) {
+                      ans.add(num);
+                      return;
+                    }
+                    final int lastDigit = num % 10;
+                    for (final int nextDigit : new int[] {lastDigit - k, lastDigit + k})
+                      if (0 <= nextDigit && nextDigit <= 9)
+                        dfs(n - 1, k, num * 10 + nextDigit, ans);
+                  }
+                }`,
+                output: `[181,292,707,818,929]`,
               },
             }}
           />
