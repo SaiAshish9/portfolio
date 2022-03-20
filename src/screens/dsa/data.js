ea0feb276a847ee39baae5e8b4bdf900e385db6e
saiashish9 <saiashish3760@gmail.com>
2022-03-20 15:51:51 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -101900,21 +101900,71 @@ a = b + c;
         content: (
           <Comp
             title="Q960. Delete Columns to Make Sorted III (Q804)"
-            content1={<></>}
+            content1={
+              <>
+                You are given an array of n strings strs, all of the same
+                length.
+                <br />
+                We may choose any deletion indices, and we delete all the
+                characters in those indices for each string.
+                <br />
+                For example, if we have strs = ["abcdef","uvwxyz"] and deletion
+                indices {"{0, 2, 3}"}, then the final array after deletions is
+                ["bef", "vyz"].
+                <br />
+                Suppose we chose a set of deletion indices answer such that
+                after deletions, the final array has every string (row) in
+                lexicographic order. (i.e., (strs[0][0] &lt;= strs[0][1] &lt;=
+                ... &lt;= strs[0][strs[0].length - 1]), and (strs[1][0] &lt;=
+                strs[1][1] &lt;= ... &lt;= strs[1][strs[1].length - 1]), and so
+                on). Return the minimum possible value of answer.length.
+              </>
+            }
             content2={null}
             examples={[
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: strs = ["babca","bbazb"] <br />
+                    Output: 3 <br />
+                    Explanation: After deleting columns 0, 1, and 4, the final
+                    array is strs = ["bc", "az"]. Both these rows are
+                    individually in lexicographic order (ie. strs[0][0] &lt;=
+                    strs[0][1] and strs[1][0] &lt;= strs[1][1]). Note that
+                    strs[0] &gt; strs[1] - the array strs is not necessarily in
+                    lexicographic order.
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: strs = ["edcba"] <br />
+                    Output: 4 <br />
+                    Explanation: If we delete less than 4 columns, the only row
+                    will not be lexicographically sorted.
+                  </>
+                ),
               },
               {
-                content: <></>,
+                content: (
+                  <>
+                    Input: strs = ["ghi","def","abc"] <br />
+                    Output: 0 <br />
+                    Explanation: All rows are already lexicographically sorted.
+                  </>
+                ),
               },
             ]}
-            constraints={<></>}
-            tc="n"
+            constraints={
+              <>
+                n == strs.length <br />
+                1 &lt;= n &lt;= 100 <br />
+                1 &lt;= strs[i].length &lt;= 100 <br />
+                strs[i] consists of lowercase English letters.
+              </>
+            }
+            tc="n^3"
             sc="n"
             codes={{
               Javascript: {
