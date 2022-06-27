1ab86372a922fea41447d48126e977c1cdea944c
saiashish9 <saiashish3760@gmail.com>
2022-06-27 20:21:33 +0530

    updated design patterns

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -106765,10 +106765,304 @@ a = b + c;
   systemDesign: {
     title: "System Design",
     types: {},
+    content: (
+      <>
+        <b>System Design </b>
+        is the process of designing the architecture, components, and interfaces
+        for a system so that it meets the end-user requirements. <br />
+        <Span>
+          The HLD changes the client or business requirement into a high-level
+          solution. The LLD changes the high-level solution to a comprehensive
+          solution.
+        </Span>
+        <b>High Level Design</b>
+        <Span>
+          The high‐level design includes such things as decisions about what
+          platform to use (such as desktop, laptop, tablet, or phone), what data
+          design to use (such as direct access, 2‐tier, or 3‐tier), and
+          interfaces with other systems (such as external purchasing systems).
+        </Span>
+        <Span>
+          High Level Design in short HLD is the general system design means it
+          refers to the overall system design. It describes the overall
+          description/architecture of the application. It includes the
+          description of system architecture, data base design, brief
+          description on systems, services, platforms and relationship among
+          modules. It is also known as macro level/system design. It is created
+          by solution architect. It converts the Business/client requirement
+          into High Level Solution. It is created first means before Low Level
+          Design.
+        </Span>
+        <b>Low Level Design</b>
+        <Span>
+          Low-level design (LLD) is a component-level design process that
+          follows a step-by-step refinement process. This process can be used
+          for designing data structures, required software architecture, source
+          code and ultimately, performance algorithms.
+        </Span>
+        <Span>
+          Low Level Design in short LLD is like detailing HLD means it refers to
+          component-level design process. It describes detailed description of
+          each and every module means it includes actual logic for every system
+          component and it goes deep into each modules specification. It is also
+          known as micro level/detailed design. It is created by designers and
+          developers. It converts the High Level Solution into Detailed
+          solution. It is created second means after High Level Design.
+        </Span>
+        <br />
+        <table>
+          <thead>
+            <th>S.No.</th>
+            <th>HIGH LEVEL DESIGN</th>
+            <th>LOW LEVEL DESIGN</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>01.</td>
+              <td>
+                High Level Design is the general system design means it refers
+                to the overall system design.
+              </td>
+              <td>
+                Low Level Design is like detailing HLD means it refers to
+                component-level design process.
+              </td>
+            </tr>
+            <tr>
+              <td>02.</td>
+              <td>High Level Design in short called as HLD. </td>
+              <td>Low Level Design in short called as LLD.</td>
+            </tr>
+            <tr>
+              <td>03.</td>
+              <td>It is also known as macro level/system design.</td>
+              <td> It is also known as micro level/detailed design.</td>
+            </tr>
+            <tr>
+              <td>04.</td>
+              <td>
+                It describes the overall description/architecture of the
+                application.
+              </td>
+              <td>
+                It describes detailed description of each and every module.
+              </td>
+            </tr>
+            <tr>
+              <td>05.</td>
+              <td>
+                High Level Design expresses the brief functionality of each
+                module.
+              </td>
+              <td>
+                Low Level Design expresses details functional logic of the
+                module.
+              </td>
+            </tr>
+            <tr>
+              <td>06.</td>
+              <td>It is created by solution architect.</td>
+              <td>It is created by designers and developers.</td>
+            </tr>
+            <tr>
+              <td>07.</td>
+              <td>
+                Here in High Level Design the participants are design team,
+                review team, and client team.
+              </td>
+              <td>
+                Here in Low Level Design participants are design team, Operation
+                Teams, and Implementers.
+              </td>
+            </tr>
+            <tr>
+              <td>08.</td>
+              <td>It is created first means before Low Level Design.</td>
+              <td> It is created second means after High Level Design.</td>
+            </tr>
+            <tr>
+              <td>09.</td>
+              <td>
+                In HLD the input criteria is Software Requirement Specification
+                (SRS).
+              </td>
+              <td>
+                In LLD the input criteria is reviewed High Level Design (HLD).
+              </td>
+            </tr>
+            <tr>
+              <td>10.</td>
+              <td>
+                High Level Solution converts the Business/client requirement
+                into High Level Solution.
+              </td>
+              <td>
+                Low Level Design converts the High Level Solution into Detailed
+                solution.
+              </td>
+            </tr>
+            <tr>
+              <td>11.</td>
+              <td>
+                In HLD the output criteria is data base design, functional
+                design and review record.
+              </td>
+              <td>
+                n LLD the output criteria is program specification and unit test
+                plan.
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </>
+    ),
   },
   designPatterns: {
     title: "Design Patterns",
     types: {},
+    content: (
+      <>
+        <b>Design patterns </b>
+        are used to represent some of the best practices adapted by experienced
+        object-oriented software developers. A design pattern systematically
+        names, motivates, and explains a general design that addresses a
+        recurring design problem in object-oriented systems. It describes the
+        problem, the solution, when to apply the solution, and its consequences.
+        It also gives implementation hints and examples.
+        <br />
+        <br />
+        <Span>
+          A design pattern provides a general reusable solution for the common
+          problems that occur in software design. The pattern typically shows
+          relationships and interactions between classes or objects. The idea is
+          to speed up the development process by providing well-tested, proven
+          development/design paradigms. Design patterns are programming language
+          independent strategies for solving a common problem. That means a
+          design pattern represents an idea, not a particular implementation. By
+          using design patterns, you can make your code more flexible, reusable,
+          and maintainable.
+        </Span>
+        <Span>
+          It’s not mandatory to always implement design patterns in your
+          project. Design patterns are not meant for project development. Design
+          patterns are meant for common problem-solving. Whenever there is a
+          need, you have to implement a suitable pattern to avoid such problems
+          in the future. To find out which pattern to use, you just have to try
+          to understand the design patterns and their purposes. Only by doing
+          that, you will be able to pick the right one.
+        </Span>
+        <b>Goal: </b>
+        <br />
+        <br />
+        <Span>
+          Understand the purpose and usage of each design pattern in order to
+          pick and implement the correct pattern as needed.
+        </Span>
+        <b>Example: </b>
+        <br />
+        <br />
+        <Span>
+          In many real-world situations, we want to create only one instance of
+          a class. For example, there can be only one active president of a
+          country at any given time. This pattern is called a Singleton pattern.
+          Other software examples could be a single DB connection shared by
+          multiple objects as creating a separate DB connection for every object
+          is costly. Similarly, there can be a single configuration manager or
+          error manager in an application that handles all problems instead of
+          creating multiple managers.
+        </Span>
+        <Span>
+          <b>Types of Design Patterns</b>
+        </Span>
+        <Span>There are mainly three types of design patterns: </Span>
+        <Span>
+          <b>1. Creational</b>
+        </Span>
+        <Span>
+          These design patterns are all about class instantiation or object
+          creation. These patterns can be further categorized into
+          Class-creational patterns and object-creational patterns. While
+          class-creation patterns use inheritance effectively in the
+          instantiation process, object-creation patterns use delegation
+          effectively to get the job done. Creational design patterns are the
+          Factory Method, Abstract Factory, Builder, Singleton, Object Pool, and
+          Prototype.
+        </Span>
+        <Span>Use case of creational design pattern-</Span>
+        <Span>
+          1. Suppose a developer wants to create a simple DBConnection class to
+          connect to a database and wants to access the database at multiple
+          locations from code, generally what the developer will do is create an
+          instance of DBConnection class and use it for doing database
+          operations wherever required. This results in creating multiple
+          connections from the database as each instance of DBConnection class
+          will have a separate connection to the database. In order to deal with
+          it, we create DBConnection class as a singleton class, so that only
+          one instance of DBConnection is created and a single connection is
+          established. Because we can manage DB Connection via one instance, we
+          can control load balance, unnecessary connections, etc.
+        </Span>
+        <Span>
+          2. Suppose you want to create multiple instances of a similar kind and
+          want to achieve loose coupling then you can go for Factory pattern. A
+          class implementing factory design pattern works as a bridge between
+          multiple classes. Consider an example of using multiple database
+          servers like SQL Server and Oracle. If you are developing an
+          application using SQL Server database as back end, but in the future
+          need to change the database to the oracle, you will need to modify all
+          your code, so as factory design patterns maintain loose coupling and
+          easy implementation, we should go for the factory design pattern in
+          order to achieve loose coupling and the creation of a similar kind of
+          object.
+        </Span>
+        <Span>
+          <b>2. Structural</b>
+        </Span>
+        <Span>
+          These design patterns are about organizing different classes and
+          objects to form larger structures and provide new functionality.
+          Structural design patterns are Adapter, Bridge, Composite, Decorator,
+          Facade, Flyweight, Private Class Data, and Proxy.
+        </Span>
+        <Span>
+          <b>Use Case Of Structural Design Pattern-</b>
+        </Span>
+        <Span>
+          1. When 2 interfaces are not compatible with each other and want to
+          establish a relationship between them through an adapter it’s called
+          an adapter design pattern. The adapter pattern converts the interface
+          of a class into another interface or class that the client expects,
+          i.e adapter lets classes work together that could not otherwise
+          because of incompatibility. so in these types of incompatible
+          scenarios, we can go for the adapter pattern.
+        </Span>
+        <Span>
+          <b>3. Behavioral</b>
+        </Span>
+        <Span>
+          Behavioral patterns are about identifying common communication
+          patterns between objects and realizing these patterns. Behavioral
+          patterns are Chain of responsibility, Command, Interpreter, Iterator,
+          Mediator, Memento, Null Object, Observer, State, Strategy, Template
+          method, Visitor
+        </Span>
+        <Span>
+          <b>Use Case Of Behavioral Design Pattern-</b>
+        </Span>
+        <Span>
+          1. The template pattern defines the skeleton of an algorithm in an
+          operation deferring some steps to sub-classes. The template method
+          lets subclasses redefine certain steps of an algorithm without
+          changing the algorithm structure. For example, in your project, you
+          want the behavior of the module to be able to extend, such that we can
+          make the module behave in new and different ways as the requirements
+          of the application change, or to meet the needs of new applications.
+          However, no one is allowed to make source code changes to it, i.e you
+          can add but can’t modify the structure in those scenarios a developer
+          can approach template design pattern.
+        </Span>
+      </>
+    ),
   },
   notes: {
     title: "Notes",
