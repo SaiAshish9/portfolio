23c2af02d8c140c22c5eb7a72996c2471a4fd49f
saiashish9 <saiashish3760@gmail.com>
2022-07-04 23:14:36 +0530

    k

--- src/screens/dsa/data.js
+++ src/screens/dsa/data.js
@@ -113191,14 +113191,219 @@ const generatePDF = (dispatch) => async ({ child, file, type }) => {
       },
       Regex: {
         title: "Regex",
-        content: <>
-        
-        </>,
+        content: (
+          <>
+            <Span>
+              A regular expression (sometimes called a rational expression) is a
+              sequence of characters that define a search pattern, mainly for
+              use in pattern matching with strings, or string matching, i.e.
+              “find and replace”-like operations.
+            </Span>
+            <Span>
+              Regular expressions are a generalized way to match patterns with
+              sequences of characters. It is used in every programming language
+              like C++, Java and Python.
+            </Span>
+            <Span>
+              What is a regular expression and what makes it so important? Regex
+              are used in Google analytics in URL matching in supporting search
+              and replace in most popular editors like Sublime, Notepad++,
+              Brackets, Google Docs and Microsoft word.
+            </Span>
+            <Span>
+              Example : Regular expression for an email address :
+              <pre>
+                {`
+              ^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{(2, 5)})$                
+                `}
+              </pre>
+            </Span>
+            <Span>
+              The above regular expression can be used for checking if a given
+              set of characters is an email address or not.
+            </Span>
+            <Span>How to write regular expression?</Span>
+            <Span>Repeaters : * , + and {} :</Span>
+            <Span>
+              These symbols act as repeaters and tell the computer that the
+              preceding character is to be used for more than just one time.
+            </Span>
+            <Span>The asterisk symbol ( * ):</Span>
+            <Span>
+              It tells the computer to match the preceding character (or set of
+              characters) for 0 or more times (upto infinite).
+            </Span>
+            <pre>
+              {`
+              Example : The regular expression ab*c will give ac, abc, abbc, 
+              abbbc….ans so on 
+              `}
+            </pre>
+            <Span>The Plus symbol ( + ):</Span>
+            <Span>
+              It tells the computer to repeat the preceding character (or set of
+              characters) for atleast one or more times(upto infinite).
+            </Span>
+            <pre>
+              {`
+              Example : The regular expression ab+c will give abc, abbc,
+              abbc, … and so on.
+              `}
+            </pre>
+            <Span>The curly braces {"{…}"}</Span>
+            <Span>
+              It tells the computer to repeat the preceding character (or set of
+              characters) for as many times as the value inside this bracket.
+            </Span>
+            <pre>
+              {`
+              Example : {2} means that the preceding character is to be repeated 2 
+              times, {min,} means the preceding character is matches min or  more 
+              times. {min,max} means that the preceding character is repeated at
+              least min & at most max times.
+              `}
+            </pre>
+            <Span>Wildcard – ( . )</Span>
+            <Span>
+              The dot symbol can take place of any other symbol, that is why it
+              is called the wildcard character.
+            </Span>
+            <pre>
+              {`
+              Example : 
+              The Regular expression .* will tell the computer that any character
+              can be used any number of times.
+              `}
+            </pre>
+            <Span>Optional character – ( ? )</Span>
+            <Span>
+              This symbol tells the computer that the preceding character may or
+              may not be present in the string to be matched.
+            </Span>
+            <pre>
+              {`
+                Example:
+                We may write the format for document file as – “docx?”
+                The ‘?’ tells the computer that x may or may not be 
+                present in the name of file format. 
+                `}
+            </pre>
+            <Span>The caret ( ^ ) symbol:</Span>
+            <Span>
+              Setting position for match :tells the computer that the match must
+              start at the beginning of the string or line.
+            </Span>
+            <pre>
+              {`
+              Example : ^\d{3} will match with patterns like "901" in "901-333-".
+              `}
+            </pre>
+            <Span>The dollar ( $ ) symbol</Span>
+            <Span>
+              It tells the computer that the match must occur at the end of the
+              string or before \n at the end of the line or string.
+            </Span>
+            <pre>{`
+            Example : -\d{3}$  will match with patterns like "-333" in "-901-333".
+            `}</pre>
+            <Span>Character Classes</Span>
+            <Span>
+              A character class matches any one of a set of characters. It is
+              used to match the most basic element of a language like a letter,
+              a digit, space, a symbol etc.
+            </Span>
+            <Span>
+              /s : matches any whitespace characters such as space and tab{" "}
+              <br />
+              /S : matches any non-whitespace characters <br />
+              /d : matches any digit character <br />
+              /D : matches any non-digit characters <br />
+              /w : matches any word character (basically alpha-numeric) <br />
+              /W : matches any non-word character <br />
+              /b : matches any word boundary (this would include spaces, dashes,
+              commas, semi-colons, etc)
+            </Span>
+            <Span>{"[set_of_characters]"}</Span>
+            <Span>
+              Matches any single character in set_of_characters. By default, the
+              match is case-sensitive.
+            </Span>
+            <pre>
+              {`
+Example : [abc] will match characters a,b and c in any string.              
+              `}
+            </pre>
+            <Span>{"[^set_of_characters]"}</Span>
+            <pre>
+              {`
+Negation: Matches any single character that is not in set_of_characters. By default, the match is case sensitive.              
+              `}
+            </pre>
+            <Span>{"[first-last]"}</Span>
+            <Span>
+              Character range: Matches any single character in the range from
+              first to last.
+            </Span>
+            <pre>
+              {`
+              Example : [a-zA-z] will match any character from a to z or A to Z.
+              `}
+            </pre>
+            <Span>The Escape Symbol : \</Span>
+            <Span>
+              If you want to match for the actual ‘+’, ‘.’ etc characters, add a
+              backslash( \ ) before that character. This will tell the computer
+              to treat the following character as a search character and
+              consider it for matching pattern.
+            </Span>
+            <pre>
+              {`
+              Example : \d+[\+-x\*]\d+ will match patterns like "2+2"
+              and "3*9" in "(2+2) * 3*9".
+              `}
+            </pre>
+            <Span>Grouping Characters ( )</Span>
+            <Span>
+              A set of different symbols of a regular expression can be grouped
+              together to act as a single unit and behave as a block, for this,
+              you need to wrap the regular expression in the parenthesis( ).
+            </Span>
+            <pre>
+              {`
+              Example : ([A-Z]\w+) contains two different elements of the regular 
+              expression combined together. This expression will match any pattern 
+              containing uppercase letter followed by any character.
+              `}
+            </pre>
+            <Span>Vertical Bar ( | )</Span>
+            <Span>
+              Matches any one element separated by the vertical bar (|)
+              character.
+            </Span>
+            <pre>{`
+            Example :  th(e|is|at) will match words - the, this and that.
+            `}</pre>
+            <Span>\number :</Span>
+            <Span>
+              Backreference: allows a previously matched
+              sub-expression(expression captured or enclosed within circular
+              brackets ) to be identified subsequently in the same regular
+              expression. \n means that group enclosed within the n-th bracket
+              will be repeated at current position.
+            </Span>
+            <pre>
+              {`
+              Example : ([a-z])\1 will match “ee” in Geek because the character 
+              at second position is same as character at position 1 of the match.
+              `}
+            </pre>
+            <Span>Comment : (?# comment) </Span>
+          </>
+        ),
       },
       MySQL: {
         title: "MYSQL",
-        content: <>
-        </>,
+        content: <></>,
       },
     },
     content: (
