62d11e8e5c96b317d150411ff32cc61cf4b1ef96
saiashish9 <saiashish3760@gmail.com>
2025-07-26 12:17:01 +0530

    k

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import AppInit from "./App";
 
 import { ThemeProvider } from "styled-components";
@@ -12,6 +11,7 @@ import GlobalStyles from "global/styles";
 import { StoreProvider, useStore } from "store";
 
 import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 
 // https://api.countapi.xyz/create?namespace=saiashish9&key=portfolio&value=0
 // https://api.countapi.xyz/get/saiashish9/portfolio
@@ -29,13 +29,15 @@ function App() {
   );
 }
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
   <StoreProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StoreProvider>,
-  document.getElementById("root")
+  </StoreProvider>
 );
 
 // https://www.npmjs.com/package/react-router-i18n
