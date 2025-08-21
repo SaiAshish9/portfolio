713d812432122a75b6c5d9cfceb85806aaeccb48
saiashish9 <saiashish3760@gmail.com>
2025-08-21 19:09:47 +0530

    k

--- src/screens/home/components/info/index.js
+++ src/screens/home/components/info/index.js
@@ -117,20 +117,20 @@ const settings = {
   slidesToShow: 4,
   slidesToScroll: 1,
   swipeToSlide: true,
-  nextArrow: <NextArrow curr={4} />,
+  nextArrow: <NextArrow curr={6} />,
   prevArrow: <PrevArrow />,
 };
 
 const mSettings = {
   ...settings,
   slidesToShow: 3,
-  nextArrow: <NextArrow curr={5} />,
+  nextArrow: <NextArrow curr={2} />,
 };
 
 const sSettings = {
   ...settings,
   slidesToShow: 2,
-  nextArrow: <NextArrow curr={1} />,
+  nextArrow: <NextArrow curr={3} />,
 };
 
 const Info = () => {
